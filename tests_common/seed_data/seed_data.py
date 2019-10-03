8ace4089d6457a295b4d2efa91c39df81fea04a5
BenAndrew-Kainos <bena@kainos.com>
2019-10-03 12:28:56 +0100

    SeedOrg

--- tests_common/seed_data/seed_data.py
+++ tests_common/seed_data/seed_data.py
@@ -3,6 +3,8 @@ from shared.seed_data.request_data import create_request_data
 from shared.seed_data.make_requests import make_request
 from shared.seed_data.seed_data_classes.seed_good import SeedGood
 from shared.seed_data.seed_data_classes.seed_user import SeedUser
+from shared.seed_data.seed_data_classes.seed_organisation import SeedOrganisation
+
 
 
 class SeedData:
@@ -24,11 +26,13 @@ class SeedData:
             gov_user=gov_user
         )
         self.seed_user = SeedUser(self.base_url, self.gov_headers, self.export_headers, self.request_data, self.context)
-        self.seed_good = SeedGood(self.base_url, self.gov_headers, self.export_headers, self.request_data, self.context)
-
         self.seed_user.auth_gov_user()
-        self.setup_org()
         self.seed_user.auth_export_user()
+
+        self.seed_good = SeedGood(self.base_url, self.gov_headers, self.export_headers, self.request_data, self.context)
+        self.seed_org = SeedOrganisation(self.base_url, self.gov_headers, self.export_headers, self.request_data,
+                                         self.context)
+        self.seed_org.setup_org()
         self.seed_good.add_good()
 
     def log(self, text):
@@ -38,22 +42,8 @@ class SeedData:
         self.log(name + ': ' + str(value))
         self.context[name] = value
 
-    def setup_org(self):
-        organisation = self.find_org_by_name(self.request_data['organisation']['name'])
-        if not organisation:
-            organisation = self.add_org('organisation')
-        org_id = organisation['id']
-        self.add_to_context('org_id', org_id)
-        self.add_to_context('first_name',  self.request_data['organisation']['user']['first_name'])
-        self.add_to_context('last_name',  self.request_data['organisation']['user']['last_name'])
-        self.add_to_context('primary_site_id', self.get_org_primary_site_id(org_id))
-        self.add_to_context('org_name', self.request_data['organisation']['name'])
-
     def setup_org_for_switching_organisations(self):
-        organisation = self.find_org_by_name(self.request_data['organisation_for_switching_organisations']['name'])
-        if not organisation:
-            self.add_org('organisation_for_switching_organisations')
-        self.add_to_context('org_name_for_switching_organisations', self.request_data['organisation_for_switching_organisations']['name'])
+        self.seed_org.setup_org_for_switching_organisations()
 
     def add_clc_query(self):
         self.log("Adding clc query: ...")
