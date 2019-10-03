6d6764f1c42454c4b6bca5c292de0288feede784
BenAndrew-Kainos <bena@kainos.com>
2019-10-03 15:09:39 +0100

    SeedClc

--- tests_common/seed_data/seed_data.py
+++ tests_common/seed_data/seed_data.py
@@ -45,33 +45,6 @@ class SeedData:
     def setup_org_for_switching_organisations(self):
         self.seed_org.setup_org_for_switching_organisations()
 
-    def add_clc_query(self):
-        self.log("Adding clc query: ...")
-        item = self.seed_good.post_good('clc_good')
-        data = {
-            'not_sure_details_details': 'something',
-            'not_sure_details_control_code': 'ML1a',
-            'good_id': item['id']
-        }
-        case_id = make_request("POST", base_url=self.base_url, url='/queries/control-list-classifications/',
-                               headers=self.export_headers, body=data).json()['case_id']
-        self.add_to_context('case_id', case_id)
-
-    def add_clc_good(self):
-        self.log('Adding clc good: ...')
-        data = self.request_data['clc_good']
-        response = make_request('POST', base_url=self.base_url, url='/goods/', headers=self.export_headers, body=data)
-        item = response.json()['good']
-        self.add_to_context('clc_good_id', item['id'])
-        self.seed_good.add_good_document(item['id'])
-        data = {'good_id': self.context['clc_good_id'],
-                'not_sure_details_control_code': 'ML1a',
-                'not_sure_details_details': 'b'}
-        response = make_request('POST', base_url=self.base_url, url='/queries/control-list-classifications/', headers=self.export_headers,
-                                     body=data)
-        response_data = response.json()
-        self.add_ecju_query(response_data['case_id'])
-
     def add_eua_query(self):
         self.log("Adding end user advisory: ...")
         data = self.request_data['end_user_advisory']
