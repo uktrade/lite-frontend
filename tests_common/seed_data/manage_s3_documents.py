bd5f3208af733931bd38dd9e52b687e64e6ee26a
Eugene Fahy <e.fahy@kainos.com>
2019-10-17 12:07:52 +0100

    Cleaned up the test function to upload test S3 files

--- tests_common/seed_data/manage_s3_documents.py
+++ tests_common/seed_data/manage_s3_documents.py
@@ -1,8 +1,5 @@
 import os
-import uuid
 import boto3
-from datetime import datetime
-from django.utils import timezone
 
 from conf.settings import env
 
@@ -11,7 +8,8 @@ def upload_test_document_to_aws():
     bucket_name = env('AWS_STORAGE_BUCKET_NAME')
     s3 = boto3.client('s3', aws_access_key_id=env('AWS_ACCESS_KEY_ID'),
                       aws_secret_access_key=env('AWS_SECRET_ACCESS_KEY'))
-    # s3_key = datetime.strftime(timezone.now(), '%Y-%m-%d-%H-%M-%S--') + str(uuid.uuid4()) + '--lite-test-doc.jpg'
+    # Use a fully static S3 key so that only one file will ever exist in the S3 bucket and
+    # we don't end up filling the bucket with lots of files that don't get deleted
     s3_key = 'lite-test-doc.jpg'
 
     file_to_upload_abs_path = \
