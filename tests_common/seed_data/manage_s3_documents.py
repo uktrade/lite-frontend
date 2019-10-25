f5c693b22b90f165b09a07a4562529eddb9d0a18
Andy Taylor <a.taylor@kainos.com>
2019-10-25 11:38:19 +0100

    Update manage_s3_documents to use api endpoint

--- tests_common/seed_data/manage_s3_documents.py
+++ tests_common/seed_data/manage_s3_documents.py
@@ -1,19 +1,10 @@
-import os
-import boto3
-
-from conf.settings import env
+from ..seed_data.make_requests import make_request
 
 
-def upload_test_document_to_aws():
-    bucket_name = env('AWS_STORAGE_BUCKET_NAME')
-    s3 = boto3.client('s3', aws_access_key_id=env('AWS_ACCESS_KEY_ID'),
-                      aws_secret_access_key=env('AWS_SECRET_ACCESS_KEY'))
-    # Use a fully static S3 key so that only one file will ever exist in the S3 bucket and
-    # we don't end up filling the bucket with lots of files that don't get deleted
-    s3_key = 'lite-test-doc.jpg'
+def upload_test_document_to_aws(base_url):
+    response = make_request("GET", base_url=base_url, url='/static/upload-document-for-tests/', headers=None)
 
-    file_to_upload_abs_path = \
-        os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_files', 'cat.jpg'))
-
-    s3.upload_file(file_to_upload_abs_path, bucket_name, s3_key)
-    return s3_key
+    if response.status_code == 200:
+        return response.json()['s3_key']
+    else:
+        raise Exception(response.json()['errors'])
