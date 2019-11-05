1d26f4fe6cd48b8e156284cd857e9fd917f60d6a
Mark Spiers <marksp@kainos.com>
2019-11-05 16:49:24 +0000

    more strings

--- lite_content/lite_exporter_frontend/constants.py
+++ lite_content/lite_exporter_frontend/constants.py
@@ -7,9 +7,15 @@ APPLICATION_GOODS_ADD_APPLICATION_DETAILS = "Details for good on application"
 APPLICATION_GOODS_ADD_DOCUMENT_DESCRIPTION = "To finish creating the good, you must attach a document." \
                                              "\n\nWarning: Do not upload any document which is above " \
                                              "'official-sensitive' level\n\nMaximum size: 100MB per file"
+APPLICATION_GOODS_ADD_DOCUMENT_MISSING = "A document is required."
+APPLICATION_GOODS_ADD_PREEXISTING_TITLE = 'Add a pre-existing good to your application'
 
-APPLICATION_GOODS_CONTROL_CODE_REQUIRED_DESCRIPTION = 'If you don\'t know, please use' + PERMISSION_FINDER_LINK +  \
-            'to find the appropriate ' 'code before adding the good to the application. You may need to create a good '\
-            'from the goods list if you are still unsure'
+APPLICATION_GOODS_CONTROL_CODE_REQUIRED_DESCRIPTION = "If you don't know, please use" + PERMISSION_FINDER_LINK +  \
+            'to find the appropriate code before adding the good to the application. You may need to create a good '\
+            'from the goods list if you are still unsure.'
 
-GOODS_CREATE_CONTROL_CODE_REQUIRED_DESC = 'If you don\'t know you can use' + PERMISSION_FINDER_LINK
+GOODS_CREATE_CONTROL_CODE_REQUIRED_DESC = "If you don't know you can use" + PERMISSION_FINDER_LINK
+GOODS_CREATE_CONTROL_CODE_NO = "No"
+GOODS_CREATE_CONTROL_CODE_YES = "Yes"
+GOODS_CREATE_CONTROL_CODE_UNSURE = "I don't know"
+GOODS_CREATE_TITLE = "Add a good"
