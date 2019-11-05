f9754382ded72c9839196d5675d7cca6d02c7111
Mark Spiers <marksp@kainos.com>
2019-11-05 16:26:42 +0000

    added new constants

--- lite_content/lite_exporter_frontend/constants.py
+++ lite_content/lite_exporter_frontend/constants.py
@@ -1,14 +1,15 @@
 from conf.settings import env
 
-APPLICATION_GOODS_ADD_BACK = "Back to goods"
-APPLICATION_GOODS_ADD_DOCUMENT_DESCRIPTION = "To finish creating the good, you must attach a document." \
-                                               "\n\nWarning: Do not upload any document which is above " \
-                                               "'official-sensitive' level\n\nMaximum size: 100MB per file"
+PERMISSION_FINDER_LINK = '<a class="govuk-link" href="' + env('PERMISSIONS_FINDER_URL') + '">Permissions Finder</a>'
 
-APPLICATION_GOODS_CONTROL_CODE_REQUIRED_DESCRIPTION = 'If you don\'t know, please use <a class="govuk-link" href="' + env(
-            'PERMISSIONS_FINDER_URL') + '">Permissions Finder</a> to find the appropriate ' \
-            'code before adding the good to the application. You may need to create a good ' \
+APPLICATION_GOODS_ADD_BACK = "Back to goods"
+APPLICATION_GOODS_ADD_APPLICATION_DETAILS = "Details for good on application"
+APPLICATION_GOODS_ADD_DOCUMENT_DESCRIPTION = "To finish creating the good, you must attach a document." \
+                                             "\n\nWarning: Do not upload any document which is above " \
+                                             "'official-sensitive' level\n\nMaximum size: 100MB per file"
+
+APPLICATION_GOODS_CONTROL_CODE_REQUIRED_DESCRIPTION = 'If you don\'t know, please use' + PERMISSION_FINDER_LINK +  \
+            'to find the appropriate ' 'code before adding the good to the application. You may need to create a good '\
             'from the goods list if you are still unsure'
 
-GOODS_CREATE_CONTROL_CODE_REQUIRED_DESC = 'If you don\'t know you can use <a class="govuk-link" href="' + env(
-            'PERMISSIONS_FINDER_URL') + '">Permissions Finder</a>.'
+GOODS_CREATE_CONTROL_CODE_REQUIRED_DESC = 'If you don\'t know you can use' + PERMISSION_FINDER_LINK
