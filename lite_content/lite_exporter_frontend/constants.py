19e5bdfde08ec3e7d45afba9135d60d51bf689fb
Mark Spiers <marksp@kainos.com>
2019-11-05 17:09:38 +0000

    more

--- lite_content/lite_exporter_frontend/constants.py
+++ lite_content/lite_exporter_frontend/constants.py
@@ -2,6 +2,8 @@ from conf.settings import env
 
 PERMISSION_FINDER_LINK = '<a class="govuk-link" href="' + env('PERMISSIONS_FINDER_URL') + '">Permissions Finder</a>'
 
+APPLICATION_GOODS_TITLE = "Goods"
+APPLICATION_GOODS_ADD_NEW = "Add a new good"
 APPLICATION_GOODS_ADD_BACK = "Back to goods"
 APPLICATION_GOODS_ADD_APPLICATION_DETAILS = "Details for good on application"
 APPLICATION_GOODS_ADD_DOCUMENT_DESCRIPTION = "To finish creating the good, you must attach a document." \
@@ -10,11 +12,11 @@ APPLICATION_GOODS_ADD_DOCUMENT_DESCRIPTION = "To finish creating the good, you m
 APPLICATION_GOODS_ADD_DOCUMENT_MISSING = "A document is required."
 APPLICATION_GOODS_ADD_PREEXISTING_TITLE = 'Add a pre-existing good to your application'
 
-APPLICATION_GOODS_CONTROL_CODE_REQUIRED_DESCRIPTION = "If you don't know, please use" + PERMISSION_FINDER_LINK +  \
-            'to find the appropriate code before adding the good to the application. You may need to create a good '\
+APPLICATION_GOODS_CONTROL_CODE_REQUIRED_DESCRIPTION = "If you don't know, please use " + PERMISSION_FINDER_LINK +  \
+            ' to find the appropriate code before adding the good to the application. You may need to create a good '\
             'from the goods list if you are still unsure.'
 
-GOODS_CREATE_CONTROL_CODE_REQUIRED_DESC = "If you don't know you can use" + PERMISSION_FINDER_LINK
+GOODS_CREATE_CONTROL_CODE_REQUIRED_DESC = "If you don't know you can use " + PERMISSION_FINDER_LINK
 GOODS_CREATE_CONTROL_CODE_NO = "No"
 GOODS_CREATE_CONTROL_CODE_YES = "Yes"
 GOODS_CREATE_CONTROL_CODE_UNSURE = "I don't know"
