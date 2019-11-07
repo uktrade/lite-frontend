18e5a10cf05d7817c99a87f730a11c8cee63d6ce
Mark Spiers <marksp@kainos.com>
2019-11-07 14:20:34 +0000

    internal constants added

---
+++ lite_content/lite_internal_frontend/constants.py
@@ -0,0 +1,12 @@
+CASES_ASSIGN_USERS = 'Assign Users'
+
+CASE_CHANGES = "See what has changed"
+
+CASE_DOCUMENT_BUTTON = "Attached Documents"
+CASE_ECJU_BUTTON = "ECJU Queries"
+CASE_MOVE_CASE_BUTTON = "Move Case"
+CASE_CHANGE_STATUS_BUTTON = "Change Status"
+CASE_RECORD_DECISION_BUTTON = "Record Decision"
+CASE_VIEW_ADVICE_BUTTON = "View Advice"
+CASE_GOODS_EDIT_FLAGS = "Edit goods flags"
+CASE_GOODS_REVIEW = "Review Goods"
