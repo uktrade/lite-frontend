eec56f239d7d6f3ef97ff71b9c29538886921d3f
Jan Faracik <j.faracik@kainos.com>
2019-11-18 10:08:21 +0000

    Improved components

---
+++ shared_assets/lite-frontend/javascripts/select-buttons.js
@@ -0,0 +1,34 @@
+$( '.lite-button-checkbox' ).click(function() {
+	$table = $(this).closest('table');
+
+	if ($table.find('input:checkbox:checked').length == $table.find('input:checkbox').length) {
+		$table.find('input:checkbox').prop('checked', false);
+	} else {
+		$table.find('input:checkbox').prop('checked', true);
+	}
+
+	setSelectButtonsState();
+});
+
+$( 'input:checkbox' ).change(function() {
+	setSelectButtonsState();
+});
+
+function setSelectButtonsState() {
+	$( 'table' ).each(function(i, obj) {
+		$(obj).find('.lite-button-checkbox').attr("class", "lite-button-checkbox");
+
+		if ($(obj).find('input:checkbox:checked').length == $(obj).find('input:checkbox').length) {
+			$(obj).find('.lite-button-checkbox').addClass( 'lite-button-checkbox--checked' );
+		} else if ($(obj).find('input:checkbox:checked').length != 0) {
+			$(obj).find('.lite-button-checkbox').addClass( 'lite-button-checkbox--indeterminate' );
+		}
+
+		// Force Webkit to repaint the button
+		// DON'T REMOVE!
+		$(obj).find('.lite-button-checkbox').css('display', 'none').height();
+		$(obj).find('.lite-button-checkbox').css('display', 'block');
+	});
+}
+
+setSelectButtonsState();
