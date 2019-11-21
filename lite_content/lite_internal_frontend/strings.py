bd97fdc67363b52e95d416ef3a72abec9d84b171
Jan Faracik <j.faracik@kainos.com>
2019-11-21 15:20:47 +0000

    Strings for internal

--- lite_content/lite_internal_frontend/strings.py
+++ lite_content/lite_internal_frontend/strings.py
@@ -1,6 +1,7 @@
 # Buttons
 CONTINUE = "Continue"
 SAVE = "Save"
+NOT_APPLICABLE = "N/A"
 
 CASES_ASSIGN_USERS = "Assign Users"
 
@@ -33,3 +34,43 @@ CHOOSE_TEMPLATE_TITLE = "Select a template"
 CHOOSE_TEMPLATE_BUTTON = CONTINUE
 PREVIEW_DOCUMENT_TITLE = "Preview"
 PREVIEW_DOCUMENT_BUTTON = SAVE
+
+# Organisation
+ORGANISATION_CREATION_SUCCESS = "The organisation was created successfully"
+ORGANISATION_SET_FLAGS = "Set flags on this organisation"
+ORGANISATION_EDIT_FLAGS = "Edit organisation flags"
+
+# HMRC Organisation
+HMRC_ORGANISATION_CREATION_SUCCESS = "The HMRC organisation was created successfully"
+
+# Flags
+SET_CASE_FLAGS = "Set case flags"
+EDIT_CASE_FLAGS = "Edit case flags"
+
+# Case
+CASE_GOODS = "Goods"
+CASE_GOODS_LOCATION = "Goods location"
+CASE_ENTITIES_INVOLVED = "Entities involved"
+CASE_ENTITIES_ACTIVITY = "Activity"
+
+CASE_INFO_ORGANISATION = "Organisation"
+CASE_INFO_STATUS = "Status"
+CASE_INFO_ACTIVITY = "Activity"
+CASE_INFO_SUBMITTED_AT = "Submitted at"
+CASE_INFO_REFERENCE_NUMBER = "Reference number"
+CASE_INFO_EXPORT_TYPE = "Export type"
+CASE_INFO_USAGE = "Usage"
+CASE_INFO_LAST_UPDATED = "Last updated"
+
+# Good
+GOOD_DESCRIPTION = "Description"
+GOOD_CONTROL_LIST_ENTRY = "Control list entry"
+GOOD_CONTROLLED = "Controlled"
+GOOD_FLAGS = "Flags"
+
+# Supporting documentation
+SUPPORTING_DOCUMENTATION_TITLE = "Supporting documentation"
+SUPPORTING_DOCUMENTATION_NAME = "Name"
+SUPPORTING_DOCUMENTATION_DESCRIPTION = "Description"
+SUPPORTING_DOCUMENTATION_DOCUMENT = "Document"
+SUPPORTING_DOCUMENTATION_NO_DOCUMENTATION = "No supporting documentation"
