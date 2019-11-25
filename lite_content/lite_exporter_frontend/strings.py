46fad50196317055ab47d302d297e2d9b06d70fd
cstobart <c.stobart@kainos.com>
2019-11-25 14:21:55 +0000

    Merge branch 'master' into LT-1400

--- lite_content/lite_exporter_frontend/strings.py
+++ lite_content/lite_exporter_frontend/strings.py
@@ -48,7 +48,19 @@ APPLICATION_TYPE = "Licence type"
 APPLICATION_EXPORT_TYPE = "Export type"
 APPLICATION_STATUS = "Status"
 APPLICATION_LAST_UPDATED_AT = "Last updated"
+APPLICATION_CREATED_AT = "Created at"
 APPLICATION_SUBMITTED_AT = "Submitted at"
+
+APPLICATION_END_USER = "End user"
+APPLICATION_ULTIMATE_END_USERS = "Ultimate end users"
+APPLICATION_CONSIGNEE = "Consignee"
+APPLICATION_THIRD_PARTIES = "Third parties"
+APPLICATION_GOODS_LOCATIONS = "Goods locations"
+APPLICATION_SUPPORTING_DOCUMENTATION = "Supporting documentation"
+APPLICATION_GOODS = "Goods"
+APPLICATION_COUNTRIES = "Countries"
+APPLICATION_ON_BEHALF_OF = "On behalf of"
+APPLICATION_OPTIONAL_NOTE = "Optional note"
 
 # Initial application questions
 WHICH_EXPORT_LICENCE_DO_YOU_WANT_TITLE = "Which export licence do you want to apply for?"
@@ -91,8 +103,21 @@ WHAT_WAS_THE_REFERENCE_CODE_DESCRIPTION = (
     "This is the reference found on the letter or email to tell you to apply for an export licence."
 )
 
+WHERE_ARE_YOUR_GOODS_GOING_TITLE = "Where are your goods going?"
+WHERE_ARE_YOUR_GOODS_GOING_SHORT_TITLE = "Set countries"
+WHERE_ARE_YOUR_GOODS_GOING_DESCRIPTION = "Select all countries that apply."
+
 # Edit application
 APPLICATION_EDIT_APPLICATION_BUTTON = "Edit application"
+
+# Delete draft
+DRAFT_DELETE_LINK = "Delete draft"
+DRAFT_DELETE_TITLE = "Are you sure you want to delete this draft?"
+DRAFT_DELETE_BACK_TEXT = BACK_TO_APPLICATION
+DRAFT_DELETE_YES_LABEL = YES
+DRAFT_DELETE_NO_LABEL = NO
+DRAFT_DELETE_SUBMIT_BUTTON = SUBMIT
+DRAFT_DELETE_ERROR = "Select a choice"
 
 # Withdraw application
 APPLICATION_WITHDRAW_ACCESS_BUTTON = "Withdraw application"
@@ -102,3 +127,16 @@ APPLICATION_WITHDRAW_YES_LABEL = YES
 APPLICATION_WITHDRAW_NO_LABEL = NO
 APPLICATION_WITHDRAW_SUBMIT_BUTTON = SUBMIT
 APPLICATION_WITHDRAW_ERROR = "Select a choice"
+
+# Where are your goods located?
+APPLICATION_WHERE_ARE_YOUR_GOODS_LOCATED_TITLE = "Where are your goods located?"
+APPLICATION_WHERE_ARE_YOUR_GOODS_LOCATED_DESCRIPTION = "You can only choose one type of location per application."
+APPLICATION_ONE_OF_MY_REGISTERED_SITES = "At one of my registered sites"
+APPLICATION_NOT_AT_MY_REGISTERED_SITES = "Other location not at my organisation's sites"
+
+APPLICATION_EXTERNAL_LOCATION_TITLE = "Do you want to add a new external location or use an existing one?"
+APPLICATION_EXTERNAL_LOCATION_NEW_LOCATION = "Add a new external location"
+APPLICATION_EXTERNAL_LOCATION_PREEXISTING_LOCATION = "Use a pre-existing external location"
+
+SELECT_SITES_TITLE = "Select which sites your goods are at"
+SELECT_SITES_BUTTON = "Select sites"
--- lite_content/lite_internal_frontend/strings.py
+++ lite_content/lite_internal_frontend/strings.py
@@ -1,3 +1,10 @@
+# Buttons
+CONTINUE = "Continue"
+SAVE = "Save"
+NOT_APPLICABLE = "N/A"
+
+QUEUE_ALL_CASES = "All cases"
+
 CASES_ASSIGN_USERS = "Assign Users"
 
 CASE_CHANGES = "See what has changed"
@@ -10,6 +17,8 @@ CASE_RECORD_DECISION_BUTTON = "Record Decision"
 CASE_VIEW_ADVICE_BUTTON = "View Advice"
 CASE_GOODS_EDIT_FLAGS = "Edit goods flags"
 CASE_GOODS_REVIEW = "Review Goods"
+CASE_GENERATE_DOCUMENT = "Generate Document"
+CASE_TEAM_ADVICE_GIVE_OR_CHANGE = "Give or change advice"
 
 USERS_LIST_PAGE_EDIT = "Edit"
 
@@ -21,3 +30,66 @@ USER_ROLE_QUESTION = "What role should this user have?"
 
 USER_DEACTIVATE = "Deactivate"
 USER_REACTIVATE = "Reactivate"
+
+# Generate Document
+CHOOSE_TEMPLATE_TITLE = "Select a template"
+CHOOSE_TEMPLATE_BUTTON = CONTINUE
+PREVIEW_DOCUMENT_TITLE = "Preview"
+PREVIEW_DOCUMENT_BUTTON = SAVE
+
+# Organisation
+ORGANISATION_CREATION_SUCCESS = "The organisation was created successfully"
+ORGANISATION_SET_FLAGS = "Set flags on this organisation"
+ORGANISATION_EDIT_FLAGS = "Edit organisation flags"
+
+# HMRC Organisation
+HMRC_ORGANISATION_CREATION_SUCCESS = "The HMRC organisation was created successfully"
+
+# Flags
+SET_CASE_FLAGS = "Set case flags"
+EDIT_CASE_FLAGS = "Edit case flags"
+
+# Case
+CASE_GOODS = "Goods"
+CASE_GOODS_LOCATION = "Goods location"
+CASE_GOODS_LOCATION_NAME = "Name"
+CASE_GOODS_LOCATION_ADDRESS = "Address"
+CASE_ENTITIES_INVOLVED = "Entities involved"
+CASE_ENTITIES_ACTIVITY = "Activity"
+CASE_PARTIES_NAME = "Name"
+CASE_PARTIES_ADDRESS = "Address"
+CASE_PARTIES_TYPE = "Type"
+CASE_PARTIES_WEBSITE = "Website"
+CASE_PARTIES_DOCUMENT = "Document"
+CASE_PARTIES_LICENSEE = "Licensee"
+
+CASE_INFO_TYPE = "Type"
+CASE_INFO_ORGANISATION = "Organisation"
+CASE_INFO_STATUS = "Status"
+CASE_INFO_ACTIVITY = "Activity"
+CASE_INFO_SUBMITTED_AT = "Submitted at"
+CASE_INFO_REFERENCE_NUMBER = "Reference number"
+CASE_INFO_EXPORT_TYPE = "Export type"
+CASE_INFO_USAGE = "Usage"
+CASE_INFO_LAST_UPDATED = "Last updated"
+
+CASE_COUNTRIES = "Countries"
+CASE_COUNTRIES_GREEN_LIST = "Green list"
+CASE_DESTINATIONS_HEADER = "Destinations"
+
+CASE_ACTIVITY_HEADING = "Activity"
+
+# Good
+GOOD_DESCRIPTION = "Description"
+GOOD_CONTROL_LIST_ENTRY = "Control list entry"
+GOOD_CONTROLLED = "Controlled"
+GOOD_FLAGS = "Flags"
+
+STANDARD_CASE_TOTAL_VALUE = "Total value:"
+
+# Supporting documentation
+SUPPORTING_DOCUMENTATION_TITLE = "Supporting documentation"
+SUPPORTING_DOCUMENTATION_NAME = "Name"
+SUPPORTING_DOCUMENTATION_DESCRIPTION = "Description"
+SUPPORTING_DOCUMENTATION_DOCUMENT = "Document"
+SUPPORTING_DOCUMENTATION_NO_DOCUMENTATION = "No supporting documentation"
