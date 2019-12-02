1123b4d707fd04bad0fa52401637b22ac38ba1f1
BenAndrew-Kainos <bena@kainos.com>
2019-12-02 14:21:15 +0000

    Add parties titles

---
+++ lite_content/lite_exporter_frontend/applications.py
@@ -0,0 +1,14 @@
+class EndUserForm:
+    TITLE = "Who is the end user of your goods?"
+
+
+class UltimateEndUserForm:
+    TITLE = "Who is the ultimate end user of your goods?"
+
+
+class ConsigneeForm:
+    TITLE = "Who will be the consignee of your goods?"
+
+
+class ThirdPartyForm:
+    TITLE = "What type of third party would you like to add?"
