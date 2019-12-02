c1823494d77a82186e345ab1c99e79b4730621e5
BenAndrew-Kainos <bena@kainos.com>
2019-12-02 15:25:07 +0000

    name form title

--- lite_content/lite_exporter_frontend/applications.py
+++ lite_content/lite_exporter_frontend/applications.py
@@ -9,16 +9,19 @@ class PartyForm:
 class EndUserForm:
     TITLE = "Who is the end user of your goods?"
     BUTTON = "Continue"
+    NAME_TITLE = "Enter the final recipient's name"
 
 
 class UltimateEndUserForm:
     TITLE = "Who is the ultimate end user of your goods?"
     BUTTON = "Continue"
+    NAME_TITLE = "Enter the final recipient's name"
 
 
 class ConsigneeForm:
     TITLE = "Who will be the consignee of your goods?"
     BUTTON = "Continue"
+    NAME_TITLE = "Enter the final recipient's name"
 
 
 class ThirdPartyForm:
@@ -32,3 +35,4 @@ class ThirdPartyForm:
         EXPORTER = "Exporter"
     TITLE = "What type of third party would you like to add?"
     BUTTON = "Continue"
+    NAME_TITLE = "Enter the final recipient's name"
