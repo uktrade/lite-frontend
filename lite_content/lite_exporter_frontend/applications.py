b95cabc4e65e3ae456db5d50abe9c101e6cc8869
BenAndrew-Kainos <bena@kainos.com>
2019-12-04 14:38:23 +0000

    Anthony's content changes

--- lite_content/lite_exporter_frontend/applications.py
+++ lite_content/lite_exporter_frontend/applications.py
@@ -1,51 +1,51 @@
 class PartyForm:
     class Options:
-        GOVERNMENT = "A Government Organisation"
-        COMMERCIAL = "A Commercial Organisation"
-        INDIVIDUAL = "An Individual"
+        GOVERNMENT = "Government organisation"
+        COMMERCIAL = "Commercial organisation"
+        INDIVIDUAL = "An individual"
         OTHER = "Other"
 
 
 class EndUserForm:
     TITLE = "Who is the end user of your goods?"
     BUTTON = "Continue"
-    NAME_FORM_TITLE = "Enter the final recipient's name"
-    WEBSITE_FORM_TITLE = "Enter the final recipient's web address (URL)"
-    ADDRESS_FORM_TITLE = "Where's the final recipient based?"
+    NAME_FORM_TITLE = "Name"
+    WEBSITE_FORM_TITLE = "Website address (optional)"
+    ADDRESS_FORM_TITLE = "Address"
     SUBMIT_BUTTON = "Save and continue"
 
 
 class UltimateEndUserForm:
     TITLE = "Who is the ultimate end user of your goods?"
     BUTTON = "Continue"
-    NAME_FORM_TITLE = "Enter the final recipient's name"
-    WEBSITE_FORM_TITLE = "Enter the final recipient's web address (URL)"
-    ADDRESS_FORM_TITLE = "Where's the final recipient based?"
+    NAME_FORM_TITLE = "Name"
+    WEBSITE_FORM_TITLE = "Website address (optional)"
+    ADDRESS_FORM_TITLE = "Address"
     SUBMIT_BUTTON = "Save and continue"
 
 
 class ConsigneeForm:
     TITLE = "Who will be the consignee of your goods?"
     BUTTON = "Continue"
-    NAME_FORM_TITLE = "Enter the final recipient's name"
-    WEBSITE_FORM_TITLE = "Enter the final recipient's web address (URL)"
-    ADDRESS_FORM_TITLE = "Where's the final recipient based?"
+    NAME_FORM_TITLE = "Name"
+    WEBSITE_FORM_TITLE = "Website address (optional)"
+    ADDRESS_FORM_TITLE = "Address"
     SUBMIT_BUTTON = "Save and continue"
 
 
 class ThirdPartyForm:
     class Options:
         AGENT = "Agent or broker"
-        ADDITIONAL_END_USER = "Additional end user"
+        ADDITIONAL_END_USER = "End user"
         INTERMEDIATE_CONSIGNEE = "Intermediate consignee"
         SUBMITTER = "Authorised submitter"
         CONSULTANT = "Consultant"
         CONTACT = "Contact"
         EXPORTER = "Exporter"
 
-    TITLE = "What type of third party would you like to add?"
-    BUTTON = "Continue"
-    NAME_FORM_TITLE = "Enter the final recipient's name"
-    WEBSITE_FORM_TITLE = "Enter the final recipient's web address (URL)"
-    ADDRESS_FORM_TITLE = "Where's the final recipient based?"
+    TITLE = "Select the type of third party"
+    BUTTON = "Save and continue"
+    NAME_FORM_TITLE = "Name"
+    WEBSITE_FORM_TITLE = "Website address (optional)"
+    ADDRESS_FORM_TITLE = "Address"
     SUBMIT_BUTTON = "Save and continue"
