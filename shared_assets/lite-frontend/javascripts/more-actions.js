032b987622759516327222b2fa3e527c25b943a6
Jan Faracik <j.faracik@kainos.com>
2019-12-12 10:42:55 +0000

    component: More Actions button

---
+++ shared_assets/lite-frontend/javascripts/more-actions.js
@@ -0,0 +1,13 @@
+var moreActionsContainer = $( '.lite-more-actions__container' )
+
+moreActionsContainer.addClass( 'lite-more-actions__container--float' ).addClass( 'lite-more-actions__container--hidden' )
+secondaryText = moreActionsContainer.hasClass( 'lite-more-actions__container--secondary' ) ? 'govuk-button--secondary' : ''
+
+// Add
+moreActionsContainer.parent().css({'position': 'relative'})
+moreActionsContainer.parent().append( '<a class="govuk-button ' + secondaryText + ' lite-more-actions__button">Actions</a>' )
+moreActionsContainer.children().removeClass('govuk-button')
+
+$('.lite-more-actions__button').click(function() {
+	$('.lite-more-actions__button').prev().toggleClass( 'lite-more-actions__container--hidden' )
+});
