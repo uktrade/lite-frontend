d60b5ffa5fe912e6fa2a4b9a535fdb7d342fa2eb
BenAndrew-Kainos <bena@kainos.com>
2019-12-16 12:52:23 +0000

    add create good form strings

--- lite_content/lite_exporter_frontend/goods.py
+++ lite_content/lite_exporter_frontend/goods.py
@@ -1,3 +1,6 @@
+from lite_content.lite_exporter_frontend.strings import PERMISSION_FINDER_LINK
+
+
 class DocumentSensitivityForm:
     TITLE = "Does your good meet the following criteria?"
     DESCRIPTION = (
@@ -13,3 +16,33 @@ class DocumentSensitivityForm:
     class Options:
         YES = "Yes"
         NO = "No"
+
+
+class CreateGoodForm:
+    TITLE = "Add a good"
+
+    class Description:
+        TITLE = "Description of good"
+        DESCRIPTION = "This can make it easier to find your good later"
+
+    class IsControlled:
+        TITLE = "Is your good controlled?"
+        YES = "Yes"
+        NO = "No"
+        UNSURE = "I don't know"
+        GET_CONTROL_CODE = "If you don't know you can use " + PERMISSION_FINDER_LINK
+
+    class ControlListEntry:
+        TITLE = "What's your good's control list entry?"
+        DESCRIPTION = (
+            "<noscript>If your good is controlled, enter its control list entry. </noscript>For example, " "ML1a. "
+        )
+
+    class Incorporated:
+        TITLE = "Is your good intended to be incorporated into an end product?"
+        DESCRIPTION = ""
+        YES = "Yes"
+        NO = "No"
+
+    class PartNumber:
+        TITLE = "Part Number"
