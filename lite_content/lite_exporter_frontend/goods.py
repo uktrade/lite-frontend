4173d82ab88b6ee1b24476f6b94aed5728a05675
Mark Spiers <marksp@kainos.com>
2019-12-17 14:55:39 +0000

    strings update

---
+++ lite_content/lite_exporter_frontend/goods.py
@@ -0,0 +1,23 @@
+class GoodsList:
+    TITLE = "Products"
+    CREATE_GOOD = "Add a product"
+
+    ORGANISATION_NO_GOODS = "Your organisation doesn't have any products."
+    FILTERED_NO_GOODS = "There are no products based on filter"
+    ORGANISATION_GOODS = "Your organisation has %s products"
+    FILTERED_GOODS = "Displaying %s products"
+
+    class Filter:
+        DESCRIPTION = "Description"
+        CONTROL_LIST_ENTRY = "Control list entry"
+        PART_NUMBER = "Part number"
+        APPLY = "Apply filters"
+        CLEAR = "Clear filters"
+        SHOW = "Show filters"
+        HIDE = "Hide filters"
+
+    class Table:
+        DESCRIPTION = "Description"
+        CONTROL_LIST_ENTRY = "Control list entry"
+        PART_NUMBER = "Part number"
+        STATUS = "Status"
