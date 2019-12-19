1999f221886396d729949b00e4bfa6bf90a3db3b
BenAndrew-Kainos <bena@kainos.com>
2019-12-19 14:05:33 +0000

    initial question strings

--- lite_content/lite_exporter_frontend/applications.py
+++ lite_content/lite_exporter_frontend/applications.py
@@ -89,3 +89,35 @@ class ApplicationsSummaryPage:
     LAST_UPDATED_AT = "Last updated"
     CREATED_AT = "Created at"
     SUBMITTED_AT = "Submitted at"
+
+
+class InitialApplicationQuestionsForms:
+    WHICH_EXPORT_LICENCE_DO_YOU_WANT_TITLE = "Select the type of licence you need"
+    WHICH_EXPORT_LICENCE_DO_YOU_WANT_DESCRIPTION = ""
+    STANDARD_LICENCE = "Standard licence"
+    STANDARD_LICENCE_DESCRIPTION = "Select a standard licence for a set quantity and set value of products."
+    OPEN_LICENCE = "Open licence"
+    OPEN_LICENCE_DESCRIPTION = (
+        "Select an open licence for multiple shipments of specific products to specific countries. "
+        "Open licences cover long term projects and repeat business."
+    )
+    HELP_WITH_CHOOSING_A_LICENCE = "Help with choosing a licence"
+    HELP_WITH_CHOOSING_A_LICENCE_CONTENT = (
+        "If you're unsure about which licence to select, read the guidance on "
+        'GOV.UK for <a class="govuk-link" target="_blank" '
+        'href="https://www.gov.uk/starting-to-export/licences">exporting and doing business '
+        'abroad<span class="govuk-visually-hidden"> (opens in a new window or tab)</span></a>.'
+    )
+    ENTER_A_REFERENCE_NAME_TITLE = "Application reference"
+    ENTER_A_REFERENCE_NAME_SHORT_TITLE = "Reference name"
+    ENTER_A_REFERENCE_NAME_DESCRIPTION = "Name and save this application so you can refer back to it when needed."
+    TEMPORARY_OR_PERMANENT_TITLE = "Select an export type"
+    TEMPORARY_OR_PERMANENT_DESCRIPTION = ""
+    TEMPORARY = "Temporary"
+    PERMANENT = "Permanent"
+    HAVE_YOU_BEEN_INFORMED_TITLE = (
+        "Have you been informed under an end use control that you need to apply for a licence?"
+    )
+    HAVE_YOU_BEEN_INFORMED_DESCRIPTION = "This could be a letter or email from HMRC or another government department."
+    WHAT_WAS_THE_REFERENCE_CODE_TITLE = "Reference number (optional)"
+    WHAT_WAS_THE_REFERENCE_CODE_DESCRIPTION = "The reference number is on the letter or email."
