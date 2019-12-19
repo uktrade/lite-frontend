b9f060a48a6744547f9426298179903d92535ac8
Jan Faracik <j.faracik@kainos.com>
2019-12-19 15:13:05 +0000

    Fix env

--- tests_common/api/client.py
+++ tests_common/api/client.py
@@ -1,22 +1,25 @@
+import os
+
 import requests
 
-from conf.settings import env
+
+LITE_API_URL = os.environ.get("LOCAL_LITE_API_URL", os.environ.get("LITE_API_URL"),)
 
 
 def get(appended_address, headers):
-    return requests.get(env("LITE_API_URL") + appended_address, headers=headers)
+    return requests.get(LITE_API_URL + appended_address, headers=headers)
 
 
 def post(appended_address, json, headers):
-    return requests.post(env("LITE_API_URL") + appended_address, json=json, headers=headers)
+    return requests.post(LITE_API_URL + appended_address, json=json, headers=headers)
 
 
 def put(appended_address, json, headers):
     if not appended_address.endswith("/"):
         appended_address = appended_address + "/"
 
-    return requests.put(env("LITE_API_URL") + appended_address, json=json, headers=headers)
+    return requests.put(LITE_API_URL + appended_address, json=json, headers=headers)
 
 
 def delete(appended_address, headers):
-    return requests.delete(env("LITE_API_URL") + appended_address, headers=headers,)
+    return requests.delete(LITE_API_URL + appended_address, headers=headers,)
