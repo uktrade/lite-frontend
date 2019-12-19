1202ce7f42735361fd6475f8657e58865bae77b5
BenAndrew-Kainos <bena@kainos.com>
2019-12-19 15:01:19 +0000

    refactor add and edit role form

--- lite_content/lite_exporter_frontend/roles.py
+++ lite_content/lite_exporter_frontend/roles.py
@@ -11,18 +11,18 @@ class ManageRolesPage:
 class AddRoleForm:
     TITLE = "Add a role"
     DESCRIPTION = "This will create a new role to use within your organisation"
-    ADD_NAME = "What do you want to call the role?"
-    ADD_PERMISSIONS = "What permissions should this role have?"
-    ADD_PERMISSIONS_DESCRIPTION = "Select all permissions that apply."
-    ADD_FORM_BACK_TO_ROLES = "Back to roles"
-    ADD_FORM_CREATE = "Create"
+    NAME = "What do you want to call the role?"
+    PERMISSIONS = "What permissions should this role have?"
+    PERMISSIONS_DESCRIPTION = "Select all permissions that apply."
+    FORM_BACK_TO_ROLES = "Back to roles"
+    FORM_CREATE = "Create"
 
 
 class EditRoleForm:
     TITLE = "Edit a role"
     DESCRIPTION = "This will change this role within your organisation"
-    EDIT_NAME = "What do you want to call the role?"
-    EDIT_PERMISSIONS = "What permissions should this role have?"
-    EDIT_PERMISSIONS_DESCRIPTION = "Select all permissions that apply."
-    EDIT_FORM_BACK_TO_ROLES = "Back to roles"
-    EDIT_FORM_CREATE = "Create"
+    NAME = "What do you want to call the role?"
+    PERMISSIONS = "What permissions should this role have?"
+    PERMISSIONS_DESCRIPTION = "Select all permissions that apply."
+    FORM_BACK_TO_ROLES = "Back to roles"
+    FORM_CREATE = "Create"
