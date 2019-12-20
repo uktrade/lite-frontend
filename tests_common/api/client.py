e5c4ce106e263cccf80691bb8c1a92bd6e9556ef
Jan Faracik <j.faracik@kainos.com>
2019-12-20 12:51:25 +0000

    Update url fetcher

--- tests_common/api/client.py
+++ tests_common/api/client.py
@@ -4,23 +4,30 @@ import requests
 
 from fixtures.env import env
 
-LITE_API_URL = env("LITE_API_URL")
+
+def _get_api_url():
+    environment = env("ENVIRONMENT").lower()
+
+    if environment == "local":
+        return env("LITE_API_URL")
+
+    return "https://lite-api-" + environment + ".london.cloudapps.digital/"
 
 
 def get(appended_address, headers):
-    return requests.get(LITE_API_URL + appended_address, headers=headers)
+    return requests.get(_get_api_url() + appended_address, headers=headers)
 
 
 def post(appended_address, json, headers):
-    return requests.post(LITE_API_URL + appended_address, json=json, headers=headers)
+    return requests.post(_get_api_url() + appended_address, json=json, headers=headers)
 
 
 def put(appended_address, json, headers):
     if not appended_address.endswith("/"):
         appended_address = appended_address + "/"
 
-    return requests.put(LITE_API_URL + appended_address, json=json, headers=headers)
+    return requests.put(_get_api_url() + appended_address, json=json, headers=headers)
 
 
 def delete(appended_address, headers):
-    return requests.delete(LITE_API_URL + appended_address, headers=headers,)
+    return requests.delete(_get_api_url() + appended_address, headers=headers,)
