fdd6bf566a8688842c4cd92fac9c4cdb639e2f67
Jan Faracik <j.faracik@kainos.com>
2019-12-20 14:55:58 +0000

    Add strings

--- lite_content/lite_exporter_frontend/sites.py
+++ lite_content/lite_exporter_frontend/sites.py
@@ -1,2 +1,5 @@
 class SitesPage:
     TAB = "Sites"
+    EDIT = "Edit "  # Intentional space to separate text and site name
+    BACK_TO = "Back to "  # Intentional space to separate text and site name
+    PRIMARY_SITE = "(Primary site)"
