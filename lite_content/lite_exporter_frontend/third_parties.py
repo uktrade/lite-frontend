c4693d218739ccf1e0b282fcfa91127b3640033c
Jan Faracik <j.faracik@kainos.com>
2020-01-03 10:27:40 +0000

    Added no third parties

--- lite_content/lite_exporter_frontend/third_parties.py
+++ lite_content/lite_exporter_frontend/third_parties.py
@@ -1,6 +1,7 @@
 class ThirdPartiesPage:
     TITLE = "Third parties"
     ADD = "Add a third party"
+    NO_RESULTS = "You haven't added any third parties to your application"
 
     class Variables:
         NAME = "Name"
