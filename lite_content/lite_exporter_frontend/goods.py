6eadf8d45687fb9a37b91c1011de70252c78609e
Jan Faracik <j.faracik@kainos.com>
2020-01-16 13:36:51 +0000

    Document

--- lite_content/lite_exporter_frontend/goods.py
+++ lite_content/lite_exporter_frontend/goods.py
@@ -192,6 +192,8 @@ class RespondToQueryForm:
 
 
 class GoodPage:
+    YOUR_QUERY_HEADING = "Your query"
+
     class Tabs:
         DETAILS = "Details"
         NOTES = "Notes"
@@ -202,6 +204,13 @@ class GoodPage:
         CASE_OFFICER = "Case officer"
         NO_ASSIGNED_CASE_OFFICER = "Not assigned"
         REFERENCE = "Reference"
+
+    class Document:
+        DOWNLOAD = generic.Document.DOWNLOAD
+        DELETE = generic.Document.DELETE
+        PROCESSING = generic.Document.PROCESSING
+        ATTACH = generic.Document.ATTACH
+        REMOVE = generic.Document.REMOVE
 
 
 class AddGoodToApplicationForm:
