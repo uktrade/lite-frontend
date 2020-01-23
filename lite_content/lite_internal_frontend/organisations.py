1e3fb40028bac48dd415348c53506bdb06e8de2f
Jan Faracik <j.faracik@kainos.com>
2020-01-23 11:16:22 +0000

    Stringssss

---
+++ lite_content/lite_internal_frontend/organisations.py
@@ -0,0 +1,12 @@
+class OrganisationsPage:
+    TITLE = "Organisations"
+    ADD_BUTTON = "Add an organisation"
+    ACTIVE_FILTER_NO_ORGANISATIONS = "There are no organisations to show with those filters"
+
+    class Table:
+        NAME = "Name"
+        TYPE = "Type"
+        EORI_NUMBER = "EORI number"
+        SIC_NUMBER = "SIC number"
+        VAT_NUMBER = "VAT number"
+        ADDRESS = "Address"
