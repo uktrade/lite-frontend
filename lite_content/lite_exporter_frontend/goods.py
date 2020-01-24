6db8fcfdd33b2b33f26dd175e50538a68d931716
Mark Spiers <marksp@kainos.com>
2020-01-24 13:32:17 +0000

    commercial sensitivity removed

--- lite_content/lite_exporter_frontend/goods.py
+++ lite_content/lite_exporter_frontend/goods.py
@@ -54,7 +54,7 @@ class DocumentSensitivityForm:
     DESCRIPTION = (
         "I have a document for my product.\nDocumentation should be specifications, datasheets, sales brochures, "
         "drawings or anything else that fully details what the product is and what it's designed to do.\n\n"
-        "The document is below OFFICIAL-SENSITIVE.\n\nThe document is not commercially sensitive."
+        "The document is below OFFICIAL-SENSITIVE."
     )
     ECJU_HELPLINE = (
         "**<noscript>If the answer is No;</noscript>**\n\nContact ECJU to arrange a more secure way to send "
