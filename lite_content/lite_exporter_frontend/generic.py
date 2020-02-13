c6162d12996763090cd81644e198ae00f091dc43
asmund <asmund.bekker@gmail.com>
2020-02-13 16:17:15 +0000

    Filters content

--- lite_content/lite_exporter_frontend/generic.py
+++ lite_content/lite_exporter_frontend/generic.py
@@ -27,3 +27,15 @@ class Document:
 
 # Links
 PERMISSION_FINDER_LINK = "[control list](" + env("PERMISSIONS_FINDER_URL") + ")"
+
+
+class Filters:
+    SHOW_FILTERS_LINK = "Show filters"
+    HIDE_FILTERS_LINK = "Hide filters"
+    APPLY_FILTERS_BUTTON = "Apply filters"
+    CLEAR_FILTERS_BUTTON = "Clear filters"
+    FILTER_BY_PREFIX = "Filter by"
+
+
+class Checkboxes:
+    SELECT_DESELECT_ALL = "Select all/Deselect all"
