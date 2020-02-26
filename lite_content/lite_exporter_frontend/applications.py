3787c04f313b70ad3a935d2de9d7778108ef3504
rorysproule1 <52039706+rorysproule1@users.noreply.github.com>
2020-02-26 16:22:30 +0000

    Details string

--- lite_content/lite_exporter_frontend/applications.py
+++ lite_content/lite_exporter_frontend/applications.py
@@ -334,7 +334,7 @@ class HMRCApplicationTaskList:
 
 class ExhibitionClearanceTaskList:
     ENTER_A_REFERENCE_NAME_SHORT_TITLE = "Reference"
-    DETAILS = "Details"
+    DETAILS = "Exhibition details"
     NEW_TITLE = "Exhibition clearance application"
     EDIT_TITLE = "Edit the application"
     GOODS = "Products"
