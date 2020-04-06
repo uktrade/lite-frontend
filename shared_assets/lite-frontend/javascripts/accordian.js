78d3a7f148a965f0a588ad05917f69e0c18fd3f9
BenAndrew-Kainos <bena@kainos.com>
2020-04-06 11:14:56 +0100

    rewrite accordian js

--- shared_assets/lite-frontend/javascripts/accordian.js
+++ shared_assets/lite-frontend/javascripts/accordian.js
@@ -1,5 +1,5 @@
-function openTableFold(element) {
-	$(element).parent().parent().find(".app-expanded-row__item, .app-expanded-row__item--invert").toggle();
-	$(element).parent().parent().toggleClass("open");
+$(".lite-accordian-table__chevron").click(function() {
+	$(this).parent().parent().find(".app-expanded-row__item, .app-expanded-row__item--invert").toggle();
+	$(this).parent().parent().toggleClass("open");
 	return false;
-}
+});
