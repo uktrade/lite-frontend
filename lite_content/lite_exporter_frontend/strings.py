a755dd6a95424a3afa2027df8055e3cba7130501
Chris Iliev <iichr@users.noreply.github.com>
2020-04-08 16:13:27 +0100

    feat: LT-2270 - Optional document upload (#179)

--- lite_content/lite_exporter_frontend/strings.py
+++ lite_content/lite_exporter_frontend/strings.py
@@ -128,7 +128,7 @@ class EndUser:
                 "You must attach:\n\n- You must attach a completed [end user undertaking"
                 "(EUU)](https://www.gov.uk/government/publications/end-user-undertaking-euu-form) or "
                 "[stockist undertaking (SU)](https://www.gov.uk/government/publications/stockist-undertaking-su-form) "
-                "completed by the end user or stockist\n\n- a letterhead as proof of the end user or stockist\n\nIf the "
+                "completed by the end user or stockist\n- a letterhead as proof of the end user or stockist\n\nIf the "
                 "end user is a government organisation, instead of an EUU or SU you can attach a government purchase "
                 "order. The purchase order must identify you as the applicant, any consignees, the products and "
                 "respective quantities.\n\nIf any of the information provided by the end user is not in English, "
