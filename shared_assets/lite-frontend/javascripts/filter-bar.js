51dc619497634d7d16a15e29d91f0e1839315169
Mark Spiers <marksp@kainos.com>
2020-06-15 11:44:22 +0100

    filter js updated to allow multiple filters on same page

--- shared_assets/lite-frontend/javascripts/filter-bar.js
+++ shared_assets/lite-frontend/javascripts/filter-bar.js
@@ -1,28 +1,29 @@
-$('#filters').hide();
-$('#show-filters-link').show();
-$('#hide-filters-link').hide();
 
-$('#filters input, #filters select').each(function() {
-	if ($(this).val() != ''
-		&& $(this).val() != 'Select'
-		&& $(this).val() != 'blank'
-		&& $(this).attr('type') != 'hidden'
-		&& ($(this).attr('type') != 'checkbox' || $(this).attr('type') == 'checkbox' && $(this).attr('checked')))
-	{
-		$('#filters').show();
-		$('#show-filters-link').hide();
-		$('#hide-filters-link').show();
-	}
+$(".lite-filter-bar").each(function() {
+	var $filters = $(this).parent();
+	$(this).find("input, select").each(function() {
+		if ($(this).val() != ''
+			&& $(this).val() != 'Select'
+			&& $(this).val() != 'blank'
+			&& $(this).attr('type') != 'hidden'
+			&& ($(this).attr('type') != 'checkbox' || $(this).attr('type') == 'checkbox' && $(this).attr('checked')))
+		{
+			$filters.show();
+			$filters.prev().find("#show-filters-link").hide();
+			$filters.prev().find("#hide-filters-link").show();
+		}
+		else {
+			$filters.hide();
+			$filters.prev().find("#show-filters-link").show();
+			$filters.prev().find("#hide-filters-link").hide();
+		}
+	});
 });
 
-$('#show-filters-link, #hide-filters-link').click(function() {
-	$('#filters').toggle();
-	if ($('#filters').is(':visible')) {
-		$('#show-filters-link').hide();
-		$('#hide-filters-link').show();
-	} else {
-		$('#show-filters-link').show();
-		$('#hide-filters-link').hide();
-	}
+$('.lite-filter-toggle-link').unbind().click(function() {
+	var $filters = $(this).parent().next();
+	$filters.toggle();
+	$(this).parent().find("> *").toggle();
+
 	return false;
 });
