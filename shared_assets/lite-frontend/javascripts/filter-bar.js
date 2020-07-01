74225f54d78feb354e1c1696690e332564477060
Jan Faracik <janf@kainos.com>
2020-07-01 16:35:56 +0100

    fix: Expanded filters now show automatically

--- shared_assets/lite-frontend/javascripts/filter-bar.js
+++ shared_assets/lite-frontend/javascripts/filter-bar.js
@@ -1,22 +1,26 @@
-$(".lite-filter-bar").each(function() {
-	var $filters = $(this).parent();
-	$filters.hide();
-	$filters.prev().find("#show-filters-link").show();
-	$filters.prev().find("#hide-filters-link").hide();
+function tryShowFilterBar() {
+	$(".lite-filter-bar").each(function() {
+		var $filters = $(this).parent();
+		$filters.hide();
+		$filters.prev().find("#show-filters-link").show();
+		$filters.prev().find("#hide-filters-link").hide();
 
-	$(this).find("input, select").each(function() {
-		if ($(this).val() != ''
-			&& $(this).val() != 'Select'
-			&& $(this).val() != 'blank'
-			&& $(this).attr('type') != 'hidden'
-			&& ($(this).attr('type') != 'checkbox' || $(this).attr('type') == 'checkbox' && $(this).attr('checked')))
-		{
-			$filters.show();
-			$filters.prev().find("#show-filters-link").hide();
-			$filters.prev().find("#hide-filters-link").show();
-		}
+		$(this).find("input, select").each(function() {
+			if ($(this).val() != ''
+				&& $(this).val() != 'Select'
+				&& $(this).val() != 'blank'
+				&& $(this).attr('type') != 'hidden'
+				&& ($(this).attr('type') != 'checkbox' || $(this).attr('type') == 'checkbox' && $(this).attr('checked')))
+			{
+				$filters.show();
+				$filters.prev().find("#show-filters-link").hide();
+				$filters.prev().find("#hide-filters-link").show();
+			}
+		});
 	});
-});
+}
+
+tryShowFilterBar();
 
 $('.lite-filter-toggle-link').unbind().click(function() {
 	var $filters = $(this).parent().next();
