c17ccb9909e0f6fac402da4fb65e89e757295308
Chris Iliev <iichr@users.noreply.github.com>
2020-07-13 09:42:22 +0100

    feat: LT-2718 - Display product details on internal (#253)

--- lite_content/lite_internal_frontend/goods.py
+++ lite_content/lite_internal_frontend/goods.py
@@ -11,3 +11,29 @@ class ReviewGoods:
 
     class Comment:
         TITLE = "Explain why you're making this decision (optional)"
+
+
+class GoodsDetailSummary:
+    CATEGORY = "Category"
+    DESCRIPTION = "Description"
+    PART_NUMBER = "Part number"
+    CLC = "Control list entries"
+    IS_GRADED = "Security graded"
+    MILITARY_USE = "Military use"
+    COMPONENT = "Component"
+    INFORMATION_SECURITY_FEATURES = "Information security features"
+    PURPOSE_SOFTWARE_TECHNOLOGY = "Purpose"
+    GRADING = "Grading"
+    ISSUING_AUTHORITY = "Issuing authority"
+    REFERENCE = "Reference"
+    DATE_OF_ISSUE = "Date of issue"
+
+    PV_GRADING_YES = "Yes"
+    PV_GRADING_NO = "No"
+
+    class FirearmDetails:
+        PRODUCT_TYPE = "Product type"
+        YEAR_OF_MANUFACTURE = "Year of manufacture"
+        CALIBRE = "Calibre"
+        COVERED_BY_THE_FIREARMS_ACT_1968 = "Covered by the Firearms Act 1968"
+        IDENTIFICATION_MARKINGS = "Identification markings"
