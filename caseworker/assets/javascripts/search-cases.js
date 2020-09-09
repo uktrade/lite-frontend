e93b31ba6fc99f2da8b17e4d4925f65207adc6e7
richtier <contact@richardtier.co.uk>
2020-09-09 12:50:08 +0100

    Imrove style of suggested field names

--- caseworker/assets/javascripts/search-cases.js
+++ caseworker/assets/javascripts/search-cases.js
@@ -48,7 +48,7 @@
             if (data.value.field == 'wildcard') {
                 source.innerHTML = '<td>' + data.value.value + '</td>'
             } else {
-                source.innerHTML = '<td class="autoCompleteResultFieldName">' + data.value.field.replace('_', ' ') + '</td><td>' + data.value.value + '<td/>'
+                source.innerHTML = '<td class="autoCompleteResultFieldName">' + data.value.field.replace(/_/g, ' ') + '</td><td>' + data.value.value + '<td/>'
             }
         },
         element: "tr"
