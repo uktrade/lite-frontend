17a03da28cdc84b22c33d085191ababdc56e079b
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2020-11-20 15:43:14 +0000

    Rerun black

--- exporter/applications/forms/common.py
+++ exporter/applications/forms/common.py
@@ -189,19 +189,12 @@ def declaration_form(application_id):
                 name="agreed_to_foi",
                 title=strings.declaration.FOI.TITLE,
                 options=[
-                    Option(
-                        key=True,
-                        value=strings.declaration.FOI.AGREE_TO_FOI,
-                    ),
+                    Option(key=True, value=strings.declaration.FOI.AGREE_TO_FOI,),
                     Option(
                         key=False,
                         value=strings.declaration.FOI.DISAGREE_TO_FOI,
                         components=[
-                            TextArea(
-                                title=strings.declaration.FOI.FOI_REASON,
-                                name="foi_reason",
-                                optional=False,
-                            ),
+                            TextArea(title=strings.declaration.FOI.FOI_REASON, name="foi_reason", optional=False,),
                         ],
                     ),
                 ],
