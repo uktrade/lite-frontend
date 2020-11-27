93bea874ae77879ac239ce01bf09d205e55a1981
richtier <contact@richardtier.co.uk>
2020-11-27 15:17:46 +0000

    Add LITE API feature flag

--- caseworker/search/services.py
+++ caseworker/search/services.py
@@ -1,8 +1,14 @@
-from core import client
 from urllib import parse
+
+from django.conf import settings
+
+from core import client
 
 
 def get_search_results(request, query_params):
+    if not settings.LITE_API_SEARCH_ENABLED:
+        return []
+
     querystring = parse.urlencode(query_params, doseq=True)
     response = client.get(request, f"/search/application/application_search/?{querystring}")
     response.raise_for_status()
@@ -10,6 +16,8 @@ def get_search_results(request, query_params):
 
 
 def get_autocomplete(request, q):
+    if not settings.LITE_API_SEARCH_ENABLED:
+        return []
     url = f"/search/application/suggest/?q={q}"
     response = client.get(request, url)
     response.raise_for_status()
