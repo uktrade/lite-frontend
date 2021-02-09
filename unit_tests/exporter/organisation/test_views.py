9a36540451682153ff7a250e57fff8e0521d8874
richtier <contact@richardtier.co.uk>
2021-02-09 15:01:01 +0000

    Add view document-on-organisation view

--- unit_tests/exporter/organisation/test_views.py
+++ unit_tests/exporter/organisation/test_views.py
@@ -54,3 +54,24 @@ def test_upload_firearm_section_five_certificate(authorized_client, requests_moc
         "reference_code": "1234",
         "document": {"name": "file.txt", "s3_key": mock.ANY, "size": 0,},
     }
+
+
+@mock.patch("exporter.organisation.views.s3_client")
+def test_download_docoument_on_organisation(mock_s3_client_class, authorized_client, requests_mock, settings):
+    mock_s3_client_class().generate_presigned_url.return_value = "/the/document/url"
+    requests_mock.get(
+        client._build_absolute_uri(f"/organisations/1/document/00acebbf-2077-4b80-8b95-37ff7f46c6d0/"),
+        json={"document": {"s3_key": "123"}},
+    )
+
+    url = reverse("organisation:document", kwargs={"pk": "00acebbf-2077-4b80-8b95-37ff7f46c6d0"})
+
+    response = authorized_client.get(url)
+
+    assert response.status_code == 302
+    assert response.url == "/the/document/url"
+
+    assert mock_s3_client_class().generate_presigned_url.call_count == 1
+    assert mock_s3_client_class().generate_presigned_url.call_args_list == [
+        mock.call("get_object", Params={"Bucket": settings.AWS_STORAGE_BUCKET_NAME, "Key": "123"}, ExpiresIn=15)
+    ]
