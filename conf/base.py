c343da8e68e66f3b5ae2e8ec3decf87df5a0046b
Ali Zaidi <alixedi@gmail.com>
2021-04-07 12:33:30 +0100

    Add middleware to check return_to is a valid, relative URL.

--- conf/base.py
+++ conf/base.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "core.middleware.UploadFailedMiddleware",
     "core.middleware.RequestsSessionMiddleware",
     "core.middleware.NoCacheMiddleware",
+    "core.middleware.ValidateReturnToMiddleware",
 ]
 
 FEATURE_CSP_MIDDLEWARE_ENABLED = env.bool("FEATURE_CSP_MIDDLEWARE_ENABLED", True)
--- unit_tests/core/test_middleware.py
+++ unit_tests/core/test_middleware.py
@@ -1,13 +1,39 @@
 from unittest import mock
 
-from django.http import HttpResponse
+import pytest
+from rest_framework import status
+from rest_framework.response import Response
 
 from core import middleware
 
 
 def test_no_cache_middleware(rf):
     request = rf.get("/")
+    get_response = mock.Mock(return_value=Response())
     instance = middleware.NoCacheMiddleware(get_response)
     response = instance(request)
-
     assert response["Cache-Control"] == "max-age=0, no-cache, no-store, must-revalidate"
+
+
+@pytest.mark.parametrize(
+    "url,response_code",
+    [
+        ("?return_to=hello", status.HTTP_200_OK),
+        ("?return_to=hello/", status.HTTP_200_OK),
+        ("?return_to=/hello", status.HTTP_200_OK),
+        ("?return_to=/hello/", status.HTTP_200_OK),
+        ("?return_to=http://example.com", status.HTTP_400_BAD_REQUEST),
+        ("?return_to=http://example.com/", status.HTTP_400_BAD_REQUEST),
+        ("?return_to=https://example.com/", status.HTTP_400_BAD_REQUEST),
+        ('?return_to=javascript:alert("hello!")', status.HTTP_400_BAD_REQUEST),
+        # Protocol-relative URL
+        ("?return_to=////example.com", status.HTTP_400_BAD_REQUEST),
+        ("?return_to=///example.com", status.HTTP_400_BAD_REQUEST),
+        ("?return_to=//example.com", status.HTTP_400_BAD_REQUEST),
+    ],
+)
+def test_validate_return_to_middleware(rf, url, response_code):
+    request = rf.get(url)
+    get_response = mock.Mock(return_value=Response())
+    response = middleware.ValidateReturnToMiddleware(get_response)(request)
+    assert response.status_code == response_code
