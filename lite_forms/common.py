26d7b54c6842b6b99023535709d4a7c42e064aad
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2021-04-12 13:16:05 +0000

    Update organisation register forms to ask for phone number and website

--- lite_forms/common.py
+++ lite_forms/common.py
@@ -6,20 +6,26 @@ def country_question(countries, prefix="address."):
     return AutocompleteInput(title="Country", name=prefix + "country", options=countries)
 
 
-def address_questions(countries, prefix="address."):
+def address_questions(countries, is_individual, prefix="address."):
+    phone_number_title = "Phone number" if is_individual else "Organisation phone number"
     return [
         TextInput(title="Building and street", accessible_description="line 1 of 2", name=prefix + "address_line_1",),
         TextInput(title="", accessible_description="line 2 of 2", name=prefix + "address_line_2",),
         TextInput(title="Town or city", name=prefix + "city"),
         TextInput(title="County or state", name=prefix + "region"),
         TextInput(title="Postcode", name=prefix + "postcode"),
+        TextInput(title=phone_number_title, name="phone_number", description="For international numbers include the country code"),
+        TextInput(title="Website", name="website"),
         conditional(countries, country_question(countries, prefix)),
     ]
 
 
-def foreign_address_questions(countries, prefix="address."):
+def foreign_address_questions(is_individual, countries, prefix="address."):
+    phone_number_title = "Phone number" if is_individual else "Organisation phone number"
     return [
         TextArea(title="Address", name=prefix + "address", classes=["govuk-input--width-20"], rows=6),
+        TextInput(title=phone_number_title, name="phone_number", description="For international numbers include the country code"),
+        TextInput(title="Website", name="website"),
         conditional(countries, country_question(countries, prefix)),
     ]
 
