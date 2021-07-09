04ceb4eda8cc36d80072fe8da49853668e1be146
Ali Zaidi <alixedi@gmail.com>
2021-07-09 11:46:32 +0100

    Add function to validate expiry date.

--- exporter/core/validators.py
+++ exporter/core/validators.py
@@ -1,5 +1,9 @@
 from http import HTTPStatus
+from datetime import date
+from django.utils import timezone
+from dateutil.relativedelta import relativedelta
 
+from exporter.applications.helpers.date_fields import format_date
 from lite_content.lite_exporter_frontend.core import RegisterAnOrganisation
 
 
@@ -16,3 +20,22 @@ def validate_register_organisation_triage(_, json):
         return {"errors": errors}, HTTPStatus.BAD_REQUEST
 
     return json, HTTPStatus.OK
+
+
+def validate_expiry_date(request, field_name):
+    """ Validate that the section certificate expiry date is
+    not in the past or > 5y in the future.
+    """
+    iso_date = format_date(request.POST, field_name)
+    # Absence of ISO date is dealt with by the API
+    if not iso_date:
+        return ["Enter the certificate expiry date and include a day, month and year"]
+
+    expiry_date = date.fromisoformat(iso_date)
+    today = timezone.now().date()
+    # Date of expiry has to be in the future
+    if expiry_date < today:
+        return ["Expiry date must be in the future"]
+    else:
+        if relativedelta(expiry_date, today).years >= 5:
+            return ["Expiry date is too far in the future"]
