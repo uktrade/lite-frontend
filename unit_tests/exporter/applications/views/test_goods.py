011bd960e986962f3124895b902fc0e486e9e4c3
Ali Zaidi <alixedi@gmail.com>
2021-07-09 11:49:12 +0100

    Add tests for goods views.

--- unit_tests/exporter/applications/views/test_goods.py
+++ unit_tests/exporter/applications/views/test_goods.py
@@ -1,3 +1,4 @@
+import pytest
 from exporter.applications.views import goods
 
 
@@ -29,3 +30,27 @@ def test_is_firearm_certificate_needed_not_section_five_selected():
     )
 
     assert actual is True
+
+
+@pytest.mark.parametrize(
+    "expiry_date,error",
+    (
+        ("2020-01-01", ["Expiry date must be in the future"]),  # Past
+        ("2050-01-01", ["Expiry date is too far in the future"]),  # Too far in the future
+    ),
+)
+def test_upload_firearm_registered_dealer_certificate_error(authorized_client, expiry_date, error):
+    # How do I write this?
+    pass
+
+
+@pytest.mark.parametrize(
+    "expiry_date,error",
+    (
+        ("2020-01-01", ["Expiry date must be in the future"]),  # Past
+        ("2050-01-01", ["Expiry date is too far in the future"]),  # Too far in the future
+    ),
+)
+def test_upload_firearm_section_five_certificate_error(authorized_client, expiry_date, error):
+    # How do I write this?
+    pass
