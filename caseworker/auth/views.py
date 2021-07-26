5f6a093cab4ff2592a9118a68bcf5e54eff16456
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2021-07-26 16:46:20 +0100

    Refactor

--- caseworker/auth/views.py
+++ caseworker/auth/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import redirect
 from django.views.generic.base import View
 
-from caseworker.auth.services import authenticate_gov_user
 from django.conf import settings
+
+from caseworker.auth.services import authenticate_gov_user
+from caseworker.auth.helpers import save_user_info_to_session
+
 from lite_content.lite_internal_frontend import strings
 from lite_forms.generators import error_page
+
 from core.auth import views as auth_views
 
 
@@ -13,13 +17,7 @@ class AuthCallbackView(auth_views.AbstractAuthCallbackView, View):
         return authenticate_gov_user(self.request, self.user_profile)
 
     def handle_success(self, data, status_code):
-        self.request.session["first_name"] = self.user_profile["first_name"]
-        self.request.session["last_name"] = self.user_profile["last_name"]
-        self.request.session["default_queue"] = data["default_queue"]
-        self.request.session["user_token"] = data["token"]
-        self.request.session["lite_api_user_id"] = data["lite_api_user_id"]
-        self.request.session["email"] = self.user_profile["email"]
-        self.request.session.save()
+        save_user_info_to_session(self.request.session, data, self.user_profile)
         return redirect(settings.LOGIN_REDIRECT_URL)
 
     def handle_failure(self, data, status_code):
