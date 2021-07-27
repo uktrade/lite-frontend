850e2347ab7a3944b1606c7620469ab47e1411d9
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2021-07-27 14:27:36 +0100

    Fix mock, test other error

--- unit_tests/caseworker/auth/test_mixins.py
+++ unit_tests/caseworker/auth/test_mixins.py
@@ -10,9 +10,11 @@ def test_login_redirect(client):
     assert response.url == "/auth/login/?next=/"
 
 
-@mock.patch("caseworker.auth.services.authenticate_gov_user")
-def test_login_redirect_no_gov_user(mock_auth_gov_user, client, rf):
-    mock_auth_gov_user.return_value = {}, 403
+@mock.patch("core.client.post")
+def test_login_redirect_no_gov_user(mock_post, client, rf):
+    data = mock.MagicMock()
+    data.status_code = 403
+    mock_post.return_value = data
 
     class TestView(LoginRequiredMixin, View):
         pass
@@ -32,4 +34,32 @@ def test_login_redirect_no_gov_user(mock_auth_gov_user, client, rf):
     assert response.status_code == 200
 
     soup = BeautifulSoup(response.content, "html.parser")
-    assert "User not found" in soup
+    assert "User not found" in soup.h1.string
+    assert "You are not registered to use this system" in soup.select(".govuk-body")[0]
+
+
+@mock.patch("core.client.post")
+def test_login_redirect_some_other_error(mock_post, client, rf):
+    data = mock.MagicMock()
+    data.status_code = 500
+    mock_post.return_value = data
+
+    class TestView(LoginRequiredMixin, View):
+        pass
+
+    request = rf.get("/")
+    request.authbroker_client = mock.Mock()
+    request.authbroker_client.authorized = True
+    request.requests_session = mock.Mock()
+    request.requests_session.request = mock.Mock()
+    request.session = {
+        "first_name": "John",
+        "last_name": "Smith",
+        "user_token": "foo",
+    }
+    response = TestView.as_view()(request)
+
+    assert response.status_code == 200
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    assert "An error occurred" in soup.h1.string
