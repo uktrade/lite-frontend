0f93eb4ccb17a9a7cede202a575a0665c11ab8be
Daniel Ball <mail@danielball.com>
2021-08-03 17:08:06 +0100

    default_queue

--- tests_common/api_client/sub_helpers/gov_users.py
+++ tests_common/api_client/sub_helpers/gov_users.py
@@ -5,7 +5,9 @@ class GovUsers:
         self.request_data = request_data
 
     def put_test_user_in_admin_team(self):
-        response = self.api_client.make_request(method="GET", url=f"/teams/", headers=self.api_client.gov_headers)
+        response = self.api_client.make_request(
+            method="GET", url="/teams/?disable_pagination=True", headers=self.api_client.gov_headers
+        )
         team_list = response.json()["teams"]
         team_id = None
         for team in team_list:
@@ -15,5 +17,5 @@ class GovUsers:
             method="PUT",
             url=f"/gov-users/{self.api_client.context['gov_user_id']}/",
             headers=self.api_client.gov_headers,
-            body={"team": team_id},
+            body={"team": team_id, "default_queue": "00000000-0000-0000-0000-000000000001"},
         )
