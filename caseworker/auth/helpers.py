059bef8b682cc67378e6bc329f68fb1f0b9e7bcc
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2021-08-16 11:16:51 +0100

    Remove unused function

--- caseworker/auth/helpers.py
+++ caseworker/auth/helpers.py
@@ -6,12 +6,3 @@ def save_internal_user_info_to_session(session, data, user_profile):
     session["lite_api_user_id"] = data["lite_api_user_id"]
     session["email"] = user_profile["email"]
     session.save()
-
-
-def save_exporter_user_info_to_session(session, data, user_profile):
-    session["first_name"] = data["first_name"]
-    session["last_name"] = data["last_name"]
-    session["user_token"] = data["token"]
-    session["lite_api_user_id"] = data["lite_api_user_id"]
-    session["email"] = user_profile["email"]
-    session.save()
