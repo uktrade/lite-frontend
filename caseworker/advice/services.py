fd70cfc1be598bcacb4e30d7bc9325f28c0682bc
Ali Zaidi <alixedi@gmail.com>
2021-10-14 13:57:23 +0100

    Add post_refusal_advice function.

--- caseworker/advice/services.py
+++ caseworker/advice/services.py
@@ -20,3 +20,21 @@ def post_approval_advice(request, case, data):
     response = client.post(request, f"/cases/{case['id']}/user-advice/", json)
     response.raise_for_status()
     return response.json(), response.status_code
+
+
+def post_refusal_advice(request, case, data):
+    product_ids = [item["id"] for item in case["data"]["goods"]]
+    json = [
+        {
+            "type": "refuse",
+            "text": data["refusal_reasons"],
+            "footnote_required": False,
+            "good": product_id,
+            "denial_reasons": data["denial_reasons"],
+        }
+        for product_id in product_ids
+    ]
+
+    response = client.post(request, f"/cases/{case['id']}/user-advice/", json)
+    response.raise_for_status()
+    return response.json(), response.status_code
