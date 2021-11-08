c68f52092c7515fbaf7b8d5e23dca15212904d91
Arun Siluvery <arunk.siluvery@gmail.com>
2021-11-08 14:19:52 +0000

    Revert "Add my advice to view advice page"

--- unit_tests/caseworker/advice/views/test_view_ogd_advice.py
+++ unit_tests/caseworker/advice/views/test_view_ogd_advice.py
@@ -234,21 +234,19 @@ def test_advice_view_heading_ogd_advice_data(
                 {
                     "user": john_smith,
                     "decision": "Approve",
-                    "title": "John Smith has approved",
+                    "decision_verb": "approved",
                     "advice": [
                         {
-                            "type": "consignee",
+                            "type": "Consignee",
+                            "address": "44",
                             "licence_condition": None,
-                            "denial_reasons": None,
-                            "name": "Consignee",
                             "country": "Abu Dhabi",
                             "advice": consignee_advice1,
                         },
                         {
-                            "type": "end_user",
+                            "type": "End User",
+                            "address": "44",
                             "licence_condition": None,
-                            "denial_reasons": None,
-                            "name": "End User",
                             "country": "United Kingdom",
                             "advice": end_user_advice1,
                         },
@@ -256,14 +254,13 @@ def test_advice_view_heading_ogd_advice_data(
                 },
                 {
                     "user": john_smith,
-                    "title": "John Smith has refused",
                     "decision": "Refuse",
+                    "decision_verb": "refused",
                     "advice": [
                         {
-                            "type": "third_party",
+                            "type": "Third party",
+                            "address": "44",
                             "licence_condition": None,
-                            "denial_reasons": None,
-                            "name": "Third party",
                             "country": "United Kingdom",
                             "advice": third_party_advice1,
                         }
@@ -277,30 +274,27 @@ def test_advice_view_heading_ogd_advice_data(
                 {
                     "user": jane_doe,
                     "decision": "Approve",
-                    "title": "Jane Doe has approved",
+                    "decision_verb": "approved",
                     "advice": [
                         {
-                            "type": "consignee",
+                            "type": "Consignee",
+                            "address": "44",
                             "licence_condition": None,
-                            "name": "Consignee",
                             "country": "Abu Dhabi",
-                            "denial_reasons": None,
                             "advice": consignee_advice2,
                         },
                         {
-                            "type": "end_user",
+                            "type": "End User",
+                            "address": "44",
                             "licence_condition": None,
-                            "name": "End User",
                             "country": "United Kingdom",
-                            "denial_reasons": None,
                             "advice": end_user_advice2,
                         },
                         {
-                            "type": "third_party",
+                            "type": "Third party",
+                            "address": "44",
                             "licence_condition": None,
-                            "name": "Third party",
                             "country": "United Kingdom",
-                            "denial_reasons": None,
                             "advice": third_party_advice2,
                         },
                     ],
@@ -325,17 +319,17 @@ def test_advice_view_heading_ogd_advice_data(
     # team1 approved 2 destinations
     assert team1_advice["advice"][0]["decision"] == "Approve"
     assert len(team1_advice["advice"][0]["advice"]) == 2
-    assert team1_advice["advice"][0]["advice"][0]["type"] == "consignee"
-    assert team1_advice["advice"][0]["advice"][1]["type"] == "end_user"
+    assert team1_advice["advice"][0]["advice"][0]["type"] == "Consignee"
+    assert team1_advice["advice"][0]["advice"][1]["type"] == "End User"
 
     # team1 refused 1 destination
     assert team1_advice["advice"][1]["decision"] == "Refuse"
     assert len(team1_advice["advice"][1]["advice"]) == 1
-    assert team1_advice["advice"][1]["advice"][0]["type"] == "third_party"
+    assert team1_advice["advice"][1]["advice"][0]["type"] == "Third party"
 
     # team2 approved all three destinations
     assert team2_advice["advice"][0]["decision"] == "Approve"
     assert len(team2_advice["advice"][0]["advice"]) == 3
-    assert team2_advice["advice"][0]["advice"][0]["type"] == "consignee"
-    assert team2_advice["advice"][0]["advice"][1]["type"] == "end_user"
-    assert team2_advice["advice"][0]["advice"][2]["type"] == "third_party"
+    assert team2_advice["advice"][0]["advice"][0]["type"] == "Consignee"
+    assert team2_advice["advice"][0]["advice"][1]["type"] == "End User"
+    assert team2_advice["advice"][0]["advice"][2]["type"] == "Third party"
