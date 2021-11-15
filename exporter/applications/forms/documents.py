738d392e00c46eba08c529085b6d5aa1ed1ee28f
Mano <manossef@gmail.com>
2021-11-15 16:58:45 +0000

    Revert back attach_document_form and create end_user_attach_document_form

--- exporter/applications/forms/documents.py
+++ exporter/applications/forms/documents.py
@@ -1,11 +1,32 @@
 from django.urls import reverse_lazy
 
 from lite_content.lite_exporter_frontend.applications import DeletePartyDocumentForm
-from lite_forms.components import Form, FileUpload, TextArea, BackLink, Option, RadioButtons
+from lite_forms.components import Form, FileUpload, Label, TextArea, BackLink, Option, RadioButtons
 from lite_forms.generators import confirm_form
 
 
 def attach_document_form(application_id, strings, back_link, is_optional):
+    return Form(
+        title=strings.TITLE,
+        description=strings.DESCRIPTION,
+        questions=[
+            FileUpload(optional=is_optional),
+            TextArea(title=strings.DESCRIPTION_FIELD_TITLE, optional=True, name="description"),
+        ],
+        back_link=BackLink(strings.BACK, reverse_lazy(back_link, kwargs={"pk": application_id})),
+        footer_label=Label(
+           'Or <a id="return_to_application" href="'
+           + str(reverse_lazy("applications:task_list", kwargs={"pk": application_id}))
+           + '" class="govuk-link govuk-link--no-visited-state">'
+           + strings.SAVE_AND_RETURN_LATER
+           + "</a> "
+           + strings.ATTACH_LATER
+       ),
+       default_button_name=strings.BUTTON_TEXT,
+    )
+
+
+def end_user_attach_document_form(application_id, strings, back_link, is_optional):
     return Form(
         title=strings.TITLE,
         description=strings.DESCRIPTION,
