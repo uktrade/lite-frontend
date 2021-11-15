e05b57687af2ac2e0a4aef2609fd398bb34d5069
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2021-11-15 17:00:15 +0000

    Rerun black

--- unit_tests/exporter/applications/forms/locations/test_locations_forms.py
+++ unit_tests/exporter/applications/forms/locations/test_locations_forms.py
@@ -35,26 +35,10 @@ def test_locations_forms_validation(form_class, data, is_valid):
 @pytest.mark.parametrize(
     "url_name,valid_data,success_url_name",
     [
-        (
-            "applications:edit_location",
-            {"goods_starting_point": "GB"},
-            "applications:temporary_or_permanent",
-        ),
-        (
-            "applications:temporary_or_permanent",
-            {"export_type": "temporary"},
-            "applications:temporary_export_details",
-        ),
-        (
-            "applications:temporary_or_permanent",
-            {"export_type": "permanent"},
-            "applications:route_of_goods",
-        ),
-        (
-            "applications:route_of_goods",
-            {"is_shipped_waybill_or_lading": True},
-            "applications:goods_recipients",
-        ),
+        ("applications:edit_location", {"goods_starting_point": "GB"}, "applications:temporary_or_permanent",),
+        ("applications:temporary_or_permanent", {"export_type": "temporary"}, "applications:temporary_export_details",),
+        ("applications:temporary_or_permanent", {"export_type": "permanent"}, "applications:route_of_goods",),
+        ("applications:route_of_goods", {"is_shipped_waybill_or_lading": True}, "applications:goods_recipients",),
         (
             "applications:goods_recipients",
             {"goods_recipients": "direct_to_end_user"},
@@ -72,7 +56,9 @@ def test_form_view_redirects(
     mock_put_application,
     mock_put_application_route_of_goods,
 ):
-    response = authorized_client.post(reverse(url_name, kwargs={"pk": data_standard_case["case"]["id"]}), data=valid_data)
+    response = authorized_client.post(
+        reverse(url_name, kwargs={"pk": data_standard_case["case"]["id"]}), data=valid_data
+    )
     assert response.status_code == 302
     assert response.url == reverse(success_url_name, kwargs={"pk": data_standard_case["case"]["id"]})
 
@@ -80,38 +66,16 @@ def test_form_view_redirects(
 @pytest.mark.parametrize(
     "url_name,back_link_url",
     [
-        (
-            "applications:edit_location",
-            "applications:task_list",
-        ),
-        (
-            "applications:temporary_or_permanent",
-            "applications:edit_location",
-        ),
-        (
-            "applications:temporary_export_details",
-            "applications:location",
-        ),
-        (
-            "applications:route_of_goods",
-            "applications:temporary_or_permanent",
-        ),
-        (
-            "applications:goods_recipients",
-            "applications:route_of_goods",
-        ),
-        (
-            "applications:locations_summary",
-            "applications:goods_recipients",
-        ),
-    ]
+        ("applications:edit_location", "applications:task_list",),
+        ("applications:temporary_or_permanent", "applications:edit_location",),
+        ("applications:temporary_export_details", "applications:location",),
+        ("applications:route_of_goods", "applications:temporary_or_permanent",),
+        ("applications:goods_recipients", "applications:route_of_goods",),
+        ("applications:locations_summary", "applications:goods_recipients",),
+    ],
 )
 def test_form_view_back_link_urls(
-    url_name,
-    back_link_url,
-    authorized_client,
-    data_standard_case,
-    mock_get_application,
+    url_name, back_link_url, authorized_client, data_standard_case, mock_get_application,
 ):
     response = authorized_client.get(reverse(url_name, kwargs={"pk": data_standard_case["case"]["id"]}))
     assert response.status_code == 200
