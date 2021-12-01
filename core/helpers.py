6709b111fe58582458f19372b12d24c46870db90
Mano <manossef@gmail.com>
2021-12-01 16:28:23 +0000

    Remove cached_property and cache since Django ships with it

--- core/helpers.py
+++ core/helpers.py
@@ -1,5 +1,4 @@
 from urllib.parse import urlencode
-from functools import lru_cache
 
 
 def dummy_quote(string, safe="", encoding=None, errors=None):
@@ -29,23 +28,3 @@ def parse_boolean(value):
         else:
             return False
     return value
-
-
-cache = lru_cache  # TODO: Remove this once we hit python3.8
-
-
-class cached_property:
-    """
-    SRC: https://stackoverflow.com/questions/4037481/caching-class-attributes-in-python
-
-    TODO: Remove this once we hit python3.8
-    """
-
-    def __init__(self, factory):
-        self._attr_name = factory.__name__
-        self._factory = factory
-
-    def __get__(self, instance, owner):
-        attr = self._factory(instance)
-        setattr(instance, self._attr_name, attr)
-        return attr
