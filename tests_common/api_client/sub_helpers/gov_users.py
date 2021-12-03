2a1241a1ee32d23c79a51afc65d04f6b13e8c268
Nawaz <nawaz.ahmed@digital.trade.gov.uk>
2021-12-03 14:50:41 +0000

    refactored to paramatise method

--- tests_common/api_client/sub_helpers/gov_users.py
+++ tests_common/api_client/sub_helpers/gov_users.py
@@ -4,30 +4,14 @@ class GovUsers:
         self.api_client = api_client
         self.request_data = request_data
 
-    def put_test_user_in_admin_team(self):
+    def put_test_user_in_team(self, team_name):
         response = self.api_client.make_request(
             method="GET", url="/teams/?disable_pagination=True", headers=self.api_client.gov_headers
         )
         team_list = response.json()["teams"]
         team_id = None
         for team in team_list:
-            if team["name"] == "Admin":
-                team_id = team["id"]
-        self.api_client.make_request(
-            method="PUT",
-            url=f"/gov-users/{self.api_client.context['gov_user_id']}/",
-            headers=self.api_client.gov_headers,
-            body={"team": team_id, "default_queue": "00000000-0000-0000-0000-000000000001"},
-        )
-
-    def put_test_user_in_licensing_unit_team(self):
-        response = self.api_client.make_request(
-            method="GET", url="/teams/?disable_pagination=True", headers=self.api_client.gov_headers
-        )
-        team_list = response.json()["teams"]
-        team_id = None
-        for team in team_list:
-            if team["name"] == "Licensing Unit":
+            if team["name"] == team_name:
                 team_id = team["id"]
         self.api_client.make_request(
             method="PUT",
