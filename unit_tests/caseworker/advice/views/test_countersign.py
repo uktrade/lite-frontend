85cabb795eb9d66d00a9b29ab88e57ffceb1679a
Ali Zaidi <alixedi@gmail.com>
2021-12-24 11:23:42 +0000

    Update tests.

--- unit_tests/caseworker/advice/views/test_countersign.py
+++ unit_tests/caseworker/advice/views/test_countersign.py
@@ -56,7 +56,6 @@ def test_countersign_approve_all_put(
     }
 
     for index, item in enumerate(advice_to_countersign.keys()):
-        data[f"form-{index}-agree_with_recommendation"] = ["yes"]
         data[f"form-{index}-approval_reasons"] = [f"reason{index}"]
         requests_mock.get(client._build_absolute_uri(f"/users/{item}/team-queues/"), json={"queues": []})
 
@@ -79,86 +78,3 @@ def test_countersign_approve_all_put(
             "countersign_comments": "reason0",
         },
     ]
-
-    history = [item for item in requests_mock.request_history if case_queues_url in item.url]
-    assert len(history) == 1
-    history = history.pop()
-    assert history.method == "PUT"
-    assert set(history.json()["queues"]) == set(
-        ["1b926457-5c9e-4916-8497-51886e51863a", "c270b79b-370c-4c5e-b8b6-4d5210a58956"]
-    )
-
-
-def test_countersign_refuse_all_put(
-    authorized_client,
-    requests_mock,
-    data_standard_case,
-    standard_case_with_advice,
-    advice_for_countersign,
-    mock_gov_user,
-    queue_pk,
-    url,
-):
-    case_id = data_standard_case["case"]["id"]
-    user_id = mock_gov_user["user"]["id"]
-
-    # Set up advice on the case
-    data_standard_case["case"]["data"]["goods"] = standard_case_with_advice["data"]["goods"]
-    data_standard_case["case"]["advice"] = advice_for_countersign
-
-    # Setup mock API requests
-    countersign_advice_url = client._build_absolute_uri(f"/cases/{case_id}/countersign-advice/")
-    requests_mock.put(countersign_advice_url, json={})
-
-    case_queues_url = client._build_absolute_uri(f"/cases/{case_id}/queues/")
-    requests_mock.put(case_queues_url, json={})
-
-    requests_mock.get(client._build_absolute_uri(f"/cases/{case_id}"), json=data_standard_case)
-    requests_mock.get(client._build_absolute_uri(f"/gov_users/{user_id}"), json=mock_gov_user)
-    requests_mock.get(client._build_absolute_uri(f"/users/{user_id}/"), json={})
-
-    advice_to_countersign = services.get_advice_to_countersign(advice_for_countersign, mock_gov_user["user"])
-
-    # Fill up the formset - starting with management form
-    data = {
-        "form-TOTAL_FORMS": [f"{len(advice_to_countersign)}"],
-        "form-INITIAL_FORMS": ["0"],
-        "form-MIN_NUM_FORMS": [f"{len(advice_to_countersign)}"],
-        "form-MAX_NUM_FORMS": [f"{len(advice_to_countersign)}"],
-        "submit": ["Submit"],
-    }
-    for index, item in enumerate(advice_to_countersign):
-        data[f"form-{index}-agree_with_recommendation"] = ["no"]
-        data[f"form-{index}-refusal_reasons"] = [f"reason{index}"]
-        data[f"form-{index}-queue_to_return"] = [queue_pk]
-        requests_mock.get(
-            client._build_absolute_uri(f"/users/{item}/team-queues/"), json={"queues": [(queue_pk, "Queue")]}
-        )
-
-    response = authorized_client.post(url, data=data)
-    assert response.status_code == 302
-
-    history = [item for item in requests_mock.request_history if countersign_advice_url in item.url]
-    assert len(history) == 1
-    history = history.pop()
-    assert history.method == "PUT"
-    assert history.json() == [
-        {
-            "id": "b32d7dfa-a90d-4b37-adac-db231d4b83be",
-            "countersigned_by": "2a43805b-c082-47e7-9188-c8b3e1a83cb0",
-            "countersign_comments": "reason0",
-        },
-        {
-            "id": "c9a96d84-6a6b-421d-bbbb-b12b9577d46e",
-            "countersigned_by": "2a43805b-c082-47e7-9188-c8b3e1a83cb0",
-            "countersign_comments": "reason0",
-        },
-    ]
-
-    history = [item for item in requests_mock.request_history if case_queues_url in item.url]
-    assert len(history) == 1
-    history = history.pop()
-    assert history.method == "PUT"
-    assert set(history.json()["queues"]) == set(
-        ["1b926457-5c9e-4916-8497-51886e51863a", "c270b79b-370c-4c5e-b8b6-4d5210a58956", queue_pk]
-    )
