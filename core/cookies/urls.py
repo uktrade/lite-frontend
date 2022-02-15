a97aaed13148daa3d63210065c37e36826018a8e
Ali Zaidi <alixedi@gmail.com>
2022-02-15 14:23:45 +0000

    Simplify URLs as we are not using cookies-details.

--- core/cookies/urls.py
+++ core/cookies/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "cookies"
 
 urlpatterns = [
-    path("", views.CookiesDetailsView.as_view(), name="cookies-details"),
-    path("preferences/", views.CookiesPreferencesView.as_view(), name="cookies-preferences",),
+    path("", views.CookiesPreferencesView.as_view(), name="cookies-preferences",),
+    path("info/", views.CookiesDetailsView.as_view(), name="cookies-details"),
 ]
