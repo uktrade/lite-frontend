98c1275ef04545731fce62581b926174f99619ef
Ali Zaidi <alixedi@gmail.com>
2022-03-16 13:53:22 +0000

    Add tests for case info panel.

--- unit_tests/caseworker/tau/test_home.py
+++ unit_tests/caseworker/tau/test_home.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import pytest
 
 from django.urls import reverse
@@ -16,11 +17,73 @@ def url(data_queue, data_standard_case):
     )
 
 
-def test_tau_home_auth(authorized_client, url, data_standard_case):
+def get_cells(soup, table_id):
+    return [td.text for td in soup.find(id=table_id).find_all("td")]
+
+
+def test_tau_home_auth(authorized_client, url):
     """GET /tau should return 200 with an authorised client"""
     response = authorized_client.get(url)
     assert response.status_code == 200
-    assert data_standard_case["case"]["id"] in response.context["greetings"]
+
+
+def test_case_info(authorized_client, url):
+    """GET /tau would return a case info panel"""
+    response = authorized_client.get(url)
+    assert response.status_code == 200
+
+    # Test elements of case info panel
+    soup = BeautifulSoup(response.content, "html.parser")
+    assert soup.find(id="products-title").text == "Assessing 2 products"
+    assert soup.find(id="case-details-title").text == "Case details"
+    assert soup.find(id="products-section-title").text == "2 products"
+    assert soup.find(id="destinations-section-title").text == "3 destinations"
+
+    assert get_cells(soup, "table-products-1") == [
+        "Select the type of firearm product",
+        "",
+        "Part number (optional)",
+        "44",
+        "Does the product have a government security grading or classification?",
+        "No",
+        "Is the product for military use?",
+        "No",
+        "Will the product be onward exported to any additional countries?",
+        "Yes",
+        "Quantity",
+        "444",
+        "Total value",
+        "£888.00",
+    ]
+    assert get_cells(soup, "table-products-2") == [
+        "Select the type of firearm product",
+        "",
+        "Part number (optional)",
+        "44",
+        "Does the product have a government security grading or classification?",
+        "No",
+        "Is the product for military use?",
+        "No",
+        "Will the product be onward exported to any additional countries?",
+        "Yes",
+        "Quantity",
+        "444",
+        "Total value",
+        "£888.00",
+    ]
+    assert get_cells(soup, "table-products-summary") == ["", ""]
+    assert get_cells(soup, "table-destinations") == [
+        "United Kingdom",
+        "end_user",
+        "End User",
+        "Abu Dhabi",
+        "Consignee",
+        "Consignee",
+        "United Kingdom",
+        "Third party",
+        "Third party",
+    ]
+    assert get_cells(soup, "table-end-use") == ["44"]
 
 
 def test_tau_home_noauth(client, url):
