2a8b428d21ab65bbf64c00c02aca95aefd1a0cce
Ali Zaidi <alixedi@gmail.com>
2022-03-17 14:49:58 +0000

    Change because dict isn't hashable.

--- caseworker/tau/templatetags/tau_tags.py
+++ caseworker/tau/templatetags/tau_tags.py
@@ -7,4 +7,4 @@ register = template.Library()
 @register.filter()
 def is_ultimate_end_user(destinations):
     """If the case does not have an ultimate_end_user, then the product might be exported onwards to another country"""
-    return {d for d in destinations if d.get("type") == "ultimate_end_user"} != {}
+    return any(d.get("type") == "ultimate_end_user" for d in destinations)
