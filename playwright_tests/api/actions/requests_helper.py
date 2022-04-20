e901802f1ed3a2f1f0ad3b504b194b41a842a82a
Pippo Raimondi <filipporai@gmail.com>
2022-04-20 13:04:00 +0100

    Remove VPN auth header as it is no longer needed

--- playwright_tests/api/actions/requests_helper.py
+++ playwright_tests/api/actions/requests_helper.py
@@ -6,17 +6,14 @@ from dotenv import load_dotenv
 load_dotenv("caseworker.env")
 
 api_url = os.environ.get("LITE_API_URL")
-vpn_user = os.getenv("VPN_USER")
-vpn_password = os.getenv("VPN_PASSWORD")
-
 
 def post(uri, kwargs):
-    return requests.post(f"{api_url}/{uri}", auth=(vpn_user, vpn_password), **kwargs)
+    return requests.post(f"{api_url}/{uri}", **kwargs)
 
 
 def put(uri, kwargs):
-    return requests.put(f"{api_url}/{uri}", auth=(vpn_user, vpn_password), **kwargs)
+    return requests.put(f"{api_url}/{uri}", **kwargs)
 
 
 def get(uri, kwargs):
-    return requests.get(f"{api_url}/{uri}", auth=(vpn_user, vpn_password), **kwargs)
+    return requests.get(f"{api_url}/{uri}", **kwargs)
