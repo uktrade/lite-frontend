6cedfc4744a48e0c868fe9778da4d45b98ad76a5
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-04-26 14:39:06 +0100

    Raise http404 with error message from good firearm mixins

--- exporter/applications/views/goods/add_good_firearm/views/mixins.py
+++ exporter/applications/views/goods/add_good_firearm/views/mixins.py
@@ -12,7 +12,7 @@ class ApplicationMixin:
         try:
             self.application = get_application(request, kwargs["pk"])
         except requests.exceptions.HTTPError:
-            raise Http404
+            raise Http404(f"Couldn't get application {kwargs['pk']}")
 
         return super().dispatch(request, *args, **kwargs)
 
@@ -22,7 +22,7 @@ class GoodMixin:
         try:
             self.good = get_good(request, kwargs["good_pk"], full_detail=True)[0]
         except requests.exceptions.HTTPError:
-            raise Http404
+            raise Http404(f"Couldn't get good {kwargs['good_pk']}")
 
         return super().dispatch(request, *args, **kwargs)
 
@@ -32,14 +32,14 @@ class GoodOnApplicationMixin:
         try:
             self.good_on_application = get_good_on_application(request, kwargs["good_on_application_pk"])
         except requests.exceptions.HTTPError:
-            raise Http404
+            raise Http404(f"Couldn't get good on application {kwargs['good_on_application_pk']}")
 
         good_id = self.good_on_application["good"]["id"]
 
         try:
             self.good = get_good(request, good_id, full_detail=True)[0]
         except requests.exceptions.HTTPError:
-            raise Http404
+            raise Http404(f"Couldn't get good {good_id}")
 
         return super().dispatch(request, *args, **kwargs)
 
@@ -47,6 +47,6 @@ class GoodOnApplicationMixin:
 class Product2FlagMixin:
     def dispatch(self, request, *args, **kwargs):
         if not settings.FEATURE_FLAG_PRODUCT_2_0:
-            raise Http404
+            raise Http404("Feature flag not set")
 
         return super().dispatch(request, *args, **kwargs)
