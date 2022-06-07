4fd061fa95efc031f78d5055728c80f6308623b1
Sekhar Panja <sekhar.panja@digital.trade.gov.uk>
2022-06-07 19:03:37 +0100

    fixup! UI test for removing sanctions

--- ui_tests/caseworker/step_defs/test_sanctions.py
+++ ui_tests/caseworker/step_defs/test_sanctions.py
@@ -1,5 +1,6 @@
 from pytest_bdd import given, scenarios, then, parsers, when
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 from tests_common import selectors
 from tests_common.tools.helpers import scroll_to_element_by_id
@@ -33,27 +34,28 @@ def check_sanction_highlighted(driver, end_user_name):  # noqa
 
 
 @when('I select <end_user_name> and press remove sanction match')
-def remove_sanctions_match(driver, end_user_name):
-    # scroll_to_element_by_id(driver, "table-sanction-matches")
-    #table-sanction-matches
-    driver.find_element(by=By.ID, value="'#table-sanction-matches .govuk-checkboxes__input'").click()
-    # tab.find_element(f"#table-sanction-matches {selectors.CHECKBOX}").click()
-    driver.find_element_by_id("button-remove-sanction-matches").click()
+def click_remove_sanctions_match(driver, end_user_name):  # noqa
+    checkbox_element = driver.find_element(
+        by=By.CSS_SELECTOR, value="#table-sanction-matches > tbody > tr > td:nth-child(1)"
+    )
+    checkbox_element.find_element(by=By.CSS_SELECTOR, value=f"#table-sanction-matches {selectors.CHECKBOX}").click()
+    driver.find_element(by=By.ID, value="button-remove-sanction-matches").click()
 
 
 @then(parsers.parse("I am asked to provide a reason"))
 def provide_reason_to_remove(driver):  # noqa
     comment_text = driver.find_element(
-        by=By.CSS_SELECTOR, value="#table-sanction-matches > tbody > tr > td:nth-child(4)"
+        by=By.CSS_SELECTOR, value="#id_comment"
     )
-    comment_text.send_keys('remove sanction')
+    comment_text.send_keys('test remove sanction')
     comment_text.submit()
 
 
 @then(parsers.parse("the sanction is removed from the case page"))
 def check_removal(driver):    # noqa
-    removed_message = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/text()")
-    assert removed_message is not None
-    sanctions_h2 = driver.find_element(by=By.XPATH, value="//*[contains(text(),'Sanction matches')]")
-    assert sanctions_h2 is None
-    
+    banner = driver.find_element(by=By.CLASS_NAME, value="app-snackbar__content")
+    assert "Sanction match successfully removed" in banner.text
+    try:
+        driver.find_element(by=By.XPATH, value="//*[contains(text(),'Sanction matches')]")
+    except NoSuchElementException:
+        pass
