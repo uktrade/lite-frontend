fcd3dbde996f4abdff07132d691f12e5ec50a524
Sekhar Panja <sekhar.panja@digital.trade.gov.uk>
2022-06-07 19:30:56 +0100

    fixup! UI test for removing sanctions

--- ui_tests/caseworker/step_defs/test_sanctions.py
+++ ui_tests/caseworker/step_defs/test_sanctions.py
@@ -33,7 +33,7 @@ def check_sanction_highlighted(driver, end_user_name):  # noqa
     assert name_element.text == end_user_name
 
 
-@when('I select <end_user_name> and press remove sanction match')
+@when("I select <end_user_name> and press remove sanction match")
 def click_remove_sanctions_match(driver, end_user_name):  # noqa
     checkbox_element = driver.find_element(
         by=By.CSS_SELECTOR, value="#table-sanction-matches > tbody > tr > td:nth-child(1)"
@@ -44,15 +44,13 @@ def click_remove_sanctions_match(driver, end_user_name):  # noqa
 
 @then(parsers.parse("I am asked to provide a reason"))
 def provide_reason_to_remove(driver):  # noqa
-    comment_text = driver.find_element(
-        by=By.CSS_SELECTOR, value="#id_comment"
-    )
-    comment_text.send_keys('test remove sanction')
+    comment_text = driver.find_element(by=By.CSS_SELECTOR, value="#id_comment")
+    comment_text.send_keys("test remove sanction")
     comment_text.submit()
 
 
 @then(parsers.parse("the sanction is removed from the case page"))
-def check_removal(driver):    # noqa
+def check_removal(driver):  # noqa
     banner = driver.find_element(by=By.CLASS_NAME, value="app-snackbar__content")
     assert "Sanction match successfully removed" in banner.text
     try:
