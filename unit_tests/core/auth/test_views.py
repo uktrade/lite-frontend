a7b22f0950e0f5e2032e57d2e9054084829a45da
dituser <dituser@Gs-MacBook-Pro-2.local>
2022-06-28 15:02:57 +0100

    allow disable rfa

---
+++ unit_tests/core/auth/test_views.py
@@ -0,0 +1,21 @@
+from django.urls import reverse
+from django.conf import settings
+from urllib.parse import urlsplit, parse_qs
+import pytest
+
+
+@pytest.mark.parametrize(
+    "low_security, value",
+    (
+        (True, ["['Cl']"]),
+        (False, None),
+    ),
+)
+def test_logged_in_call_back_low_security(low_security, value, client, settings):
+    settings.AUTHBROKER_LOW_SECURITY = low_security
+    url = reverse("auth:login")
+    response = client.get(url)
+    assert response.status_code == 302
+    query = urlsplit(response.url).query
+    params = parse_qs(query)
+    assert params.get("vtr") == value
