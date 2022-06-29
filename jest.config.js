9aff8ed95b09ec4b8d072bb5d18c611948963b21
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-06-29 08:44:29 +0100

    Add frontend testing infrastructure
    This installs the necessary libraries to run frontend tests via Jest
    
    This also adds the config for Jest to be able to transpile the JS and adds the necessary config for parcel to undo that config as Parcel handles its own transpilation

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    "caseworker/assets/javascripts"
+  ],
+
+  // The test environment that will be used for testing
+  testEnvironment: "jsdom",
+
+  // A map from regular expressions to paths to transformers
+  // transform: undefined,
+};
