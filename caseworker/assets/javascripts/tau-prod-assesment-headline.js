8034fe8e58892aded2dbe79891bb9d9ffe986b1d
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-06-29 13:02:26 +0100

    Use explicit element to toggle select all text instead of event element

--- caseworker/assets/javascripts/tau-prod-assesment-headline.js
+++ caseworker/assets/javascripts/tau-prod-assesment-headline.js
@@ -43,22 +43,22 @@ const addSelectAllExpandAll = (
         product.checked = true;
         product.dispatchEvent(new Event("input"));
       });
+      selectAllButton.innerText = DESELECT_ALL;
       isAllSelected = true;
 
       productsNumberChecks.number = productsNumberChecks.max;
       tauHeadline.innerText = `Assessing ${productsNumberChecks.number} products`;
       tauSecondColumn.classList.remove("tau__second-column--hide");
-      event.target.innerText = DESELECT_ALL;
     } else {
       checkboxProducts.forEach((product) => {
         product.checked = false;
         product.dispatchEvent(new Event("input"));
       });
+      selectAllButton.innerText = SELECT_ALL;
       isAllSelected = false;
 
       productsNumberChecks.number = 0;
       tauSecondColumn.classList.add("tau__second-column--hide");
-      event.target.innerText = SELECT_ALL;
     }
   });
 
