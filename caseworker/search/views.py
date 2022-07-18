5c312f4db4772dfed25a1a7e3891f3ed1307ed6a
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-07-18 12:46:59 +0100

    Retrieve search results on product search page
    This also tidies up some of the search related fixtures, moslty to stop them from being autoused

--- caseworker/search/views.py
+++ caseworker/search/views.py
@@ -4,12 +4,24 @@ from django.views.generic import TemplateView
 
 from core.auth.views import LoginRequiredMixin
 
+from .services import get_product_search_results
+
 
 class ProductSearchView(LoginRequiredMixin, TemplateView):
     template_name = "search/products.html"
 
-    def dispatch(self, *args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
         if not settings.FEATURE_FLAG_PRODUCT_SEARCH:
             raise Http404("No feature flag")
 
-        return super().dispatch(*args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["search_results"] = get_product_search_results(
+            self.request,
+            self.request.GET,
+        )
+
+        return context
