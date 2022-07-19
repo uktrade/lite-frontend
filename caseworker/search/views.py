4872a1194d21864d5fdf4c68106c4e5406b31c24
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-07-19 14:23:48 +0100

    Use product search result data on product search page

--- caseworker/search/views.py
+++ caseworker/search/views.py
@@ -16,12 +16,14 @@ class ProductSearchView(LoginRequiredMixin, TemplateView):
 
         return super().dispatch(request, *args, **kwargs)
 
+    def get_search_results(self, request, query):
+        search_results = get_product_search_results(request, query)
+
+        return search_results
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
 
-        context["search_results"] = get_product_search_results(
-            self.request,
-            self.request.GET,
-        )
+        context["search_results"] = self.get_search_results(self.request, self.request.GET)
 
         return context
