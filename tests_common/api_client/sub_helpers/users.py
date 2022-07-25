c068b6020f90e900dd320ef2eeec2de314c60efe
Sekhar Panja <sekhar.panja@digital.trade.gov.uk>
2022-07-25 18:33:57 +0100

    fix for lack exporter config in caseworker tests

--- tests_common/api_client/sub_helpers/users.py
+++ tests_common/api_client/sub_helpers/users.py
@@ -16,7 +16,7 @@ class BasicAuthenticator:
 
 def create_govuk_sso_user():
     # we should decide if we would like to create a govuk user, if so it should be here
-    first_name, last_name = os.environ.get("EXPORTER_TEST_SSO_NAME").split(" ")
+    first_name, last_name = os.environ.get("EXPORTER_TEST_SSO_NAME", "LITE Testing").split(" ")
     return {
         "email": os.environ.get("EXPORTER_TEST_SSO_EMAIL"),
         "first_name": first_name,
