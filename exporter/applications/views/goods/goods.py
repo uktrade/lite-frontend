b54a4d7721f4f199055ab39f98d8892cb74b84f4
depsiatwal <depsi.a@gmail.com>
2022-08-31 10:01:45 +0100

    fix 404 error

--- exporter/applications/views/goods/goods.py
+++ exporter/applications/views/goods/goods.py
@@ -290,8 +290,7 @@ class IsMaterialSubstanceCategory(LoginRequiredMixin, FormView):
 
     def dispatch(self, request, **kwargs):
         if not (
-            settings.FEATURE_FLAG_NON_FIREARMS_MATERIAL_ENABLED
-            and settings.FEATURE_FLAG_NON_FIREARMS_COMPONENTS_ENABLED
+            settings.FEATURE_FLAG_NON_FIREARMS_MATERIAL_ENABLED or settings.FEATURE_FLAG_NON_FIREARMS_COMPONENTS_ENABLED
         ):
             raise Http404
         return super().dispatch(request, **kwargs)
