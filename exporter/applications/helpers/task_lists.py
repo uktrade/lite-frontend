b9ad26d788d78881f5e7e0c58681074a08b40463
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-09-02 15:10:19 +0100

    Use `any` instead of checking for `True` in a list

--- exporter/applications/helpers/task_lists.py
+++ exporter/applications/helpers/task_lists.py
@@ -139,6 +139,6 @@ def get_application_task_list(request, application, errors=None):
 
     if not application_type == OPEN:
         context["goods"] = get_application_goods(request, application["id"])
-        context["ultimate_end_users_required"] = True in [good["is_good_incorporated"] for good in context["goods"]]
+        context["ultimate_end_users_required"] = any(good["is_good_incorporated"] for good in context["goods"])
 
     return render(request, "applications/task-list.html", context)
