653d1d813cb560ff9553318190d1516afe09df56
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-09-02 15:07:04 +0100

    Add tests for task list view

---
+++ unit_tests/exporter/applications/views/test_application_task_list.py
@@ -0,0 +1,110 @@
+import pytest
+
+from pytest_django.asserts import assertTemplateUsed
+
+from django.urls import reverse
+
+from exporter.core.objects import Application
+
+
+@pytest.fixture
+def application(data_standard_case):
+    return data_standard_case["case"]
+
+
+@pytest.fixture
+def task_list_url(application):
+    return reverse(
+        "applications:task_list",
+        kwargs={
+            "pk": application["id"],
+        },
+    )
+
+
+@pytest.fixture
+def mock_application_documents_get(application, requests_mock):
+    return requests_mock.get(
+        f"/applications/{application['id']}/documents/",
+        json={"documents": []},
+    )
+
+
+@pytest.fixture
+def mock_case_notes_get(application, requests_mock):
+    return requests_mock.get(
+        f"/cases/{application['id']}/case-notes/",
+        json={"case_notes": []},
+    )
+
+
+@pytest.fixture
+def mock_goods_get(application, requests_mock):
+    return requests_mock.get(
+        f"/applications/{application['id']}/goods/",
+        json={"goods": []},
+    )
+
+
+def test_application_task_list_status_code(
+    authorized_client,
+    task_list_url,
+    mock_application_get,
+    mock_application_documents_get,
+    mock_case_notes_get,
+    mock_goods_get,
+):
+    response = authorized_client.get(task_list_url)
+    assert response.status_code == 200
+
+
+def test_application_task_list_template_rendered(
+    authorized_client,
+    task_list_url,
+    mock_application_get,
+    mock_application_documents_get,
+    mock_case_notes_get,
+    mock_goods_get,
+):
+    response = authorized_client.get(task_list_url)
+    assertTemplateUsed(response, "applications/task-list.html")
+
+
+@pytest.mark.parametrize(
+    "goods, ultimate_end_users_required",
+    (
+        ([], False),
+        (
+            [{"is_good_incorporated": False}],
+            False,
+        ),
+        (
+            [{"is_good_incorporated": True}],
+            True,
+        ),
+        (
+            [
+                {"is_good_incorporated": True},
+                {"is_good_incorporated": False},
+            ],
+            True,
+        ),
+    ),
+)
+def test_application_task_list_ultimate_end_users_required(
+    goods,
+    ultimate_end_users_required,
+    authorized_client,
+    task_list_url,
+    application,
+    mock_application_get,
+    mock_application_documents_get,
+    mock_case_notes_get,
+    requests_mock,
+):
+    requests_mock.get(
+        f"/applications/{application['id']}/goods/",
+        json={"goods": goods},
+    )
+    response = authorized_client.get(task_list_url)
+    assert response.context["ultimate_end_users_required"] == ultimate_end_users_required
