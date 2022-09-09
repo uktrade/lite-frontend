4ea7c1bf3ee5e0f7d84e62492c5596ea2eabdb1c
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-09-09 15:08:53 +0100

    Add additional summary utility functions

--- core/summaries/utils.py
+++ core/summaries/utils.py
@@ -9,3 +9,15 @@ def remove_fields(summary, fields):
     filtered_summary = tuple((key, *rest) for key, *rest in summary if key not in fields)
 
     return filtered_summary
+
+
+def get_field(summary, key):
+    for field in summary:
+        if field[0] == key:
+            return field
+    raise KeyError(f"{key} not found in summary")
+
+
+def pluck_field(summary, key):
+    found_field = get_field(summary, key)
+    return found_field, remove_fields(summary, [key])
