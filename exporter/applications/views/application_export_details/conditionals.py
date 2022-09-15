55dc63c598059cf476f5eb4bdca4cf76842da48a
Gurdeep Atwal <44236490+depsiatwal@users.noreply.github.com>
2022-09-15 17:22:34 +0100

    LTD-2816-hook-up-wizard  (#907)
    * hook up wizard
    
    * fix lint
    
    * fix tests;
    
    * rename is_mod_approvall flag

--- exporter/applications/views/application_export_details/conditionals.py
+++ exporter/applications/views/application_export_details/conditionals.py
@@ -4,14 +4,14 @@ from exporter.applications.constants import SecurityClassifiedApprovalsType
 
 def is_f680_approval(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(ExportDetailsSteps.SECURITY_CLASSIFIED)
-    return SecurityClassifiedApprovalsType.F680 in cleaned_data.get("mod_security_classified_approvals", [])
+    return SecurityClassifiedApprovalsType.F680 in cleaned_data.get("security_approvals", [])
 
 
 def is_f1686_approval(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(ExportDetailsSteps.SECURITY_CLASSIFIED)
-    return SecurityClassifiedApprovalsType.F1686 in cleaned_data.get("mod_security_classified_approvals", [])
+    return SecurityClassifiedApprovalsType.F1686 in cleaned_data.get("security_approvals", [])
 
 
 def is_other_approval(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(ExportDetailsSteps.SECURITY_CLASSIFIED)
-    return SecurityClassifiedApprovalsType.OTHER in cleaned_data.get("mod_security_classified_approvals", [])
+    return SecurityClassifiedApprovalsType.OTHER in cleaned_data.get("security_approvals", [])
