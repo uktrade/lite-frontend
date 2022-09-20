ba5cfba0bde2576a01aa1df567ccc083fbc8e338
Gurdeep Atwal <44236490+depsiatwal@users.noreply.github.com>
2022-09-20 16:55:35 +0100

    LTD-2816-application-edit  (#917)
    * add summary and edit pages
    
    * fix test
    
    * add editforms and summary
    
    * fix tests
    
    * revert test file
    
    * Update test_export_details_edit_views.py

--- exporter/applications/views/application_export_details/edit_views.py
+++ exporter/applications/views/application_export_details/edit_views.py
@@ -1,14 +1,27 @@
+from datetime import datetime
+
 from django.views.generic import FormView
 from django.urls import reverse
+from django.shortcuts import redirect
 
 from core.auth.views import LoginRequiredMixin
 
 from exporter.applications.views.goods.common.mixins import ApplicationMixin
 from exporter.applications.views.goods.common.payloads import get_cleaned_data
 from exporter.applications.services import put_application
+from exporter.core.wizard.views import BaseSessionWizardView
 
-from .forms import F680ReferenceNumberForm
+from .forms import SecurityClassifiedDetailsForm, F680ReferenceNumberForm, SecurityOtherDetailsForm, F1686DetailsForm
+from .conditionals import is_f680_approval, is_f1686_approval, is_other_approval
 from .mixins import NonF680SecurityClassifiedFlagMixin
+from .payloads import get_other_details_data, get_f1686_data, ExportDetailsStepsPayloadBuilder
+from .constants import ExportDetailsSteps
+from .initial import (
+    get_initial_security_classified_details,
+    get_initial_f680_reference_number,
+    get_initial_other_security_approval_details,
+    get_initial_f1686_details,
+)
 
 
 class BaseApplicationEditView(
@@ -47,7 +60,43 @@ class BaseApplicationEditView(
         return ctx
 
 
-class EditExportDetailsF680ReferenceNumber(BaseApplicationEditView):
+class BaseApplicationEditWizardView(
+    LoginRequiredMixin,
+    ApplicationMixin,
+    BaseSessionWizardView,
+):
+    def get_success_url(self):
+        return reverse(
+            "applications:application_export_details_summary",
+            kwargs={"pk": self.application["id"]},
+        )
+
+    def get_back_link_url(self):
+        return self.get_success_url()
+
+    def get_context_data(self, form, **kwargs):
+        ctx = super().get_context_data(form, **kwargs)
+        ctx["back_link_url"] = self.get_success_url()
+        ctx["title"] = form.Layout.TITLE
+        return ctx
+
+    def get_payload(self, form_dict):
+        export_details_payload = ExportDetailsStepsPayloadBuilder().build(form_dict)
+        return export_details_payload
+
+    def edit_object(self, request, application_id, payload):
+        put_application(request, application_id, payload)
+
+    def process_forms(self, form_list, form_dict, **kwargs):
+        self.edit_object(self.request, self.application["id"], self.get_payload(form_dict))
+
+    def done(self, form_list, form_dict, **kwargs):
+        self.process_forms(form_list, form_dict, **kwargs)
+
+        return redirect(self.get_success_url())
+
+
+class EditF680ReferenceNumber(BaseApplicationEditView):
     form_class = F680ReferenceNumberForm
 
     def get_initial(self):
@@ -55,3 +104,68 @@ class EditExportDetailsF680ReferenceNumber(BaseApplicationEditView):
 
     def get_edit_payload(self, form):
         return get_cleaned_data(form)
+
+
+class EditSecurityOtherDetails(BaseApplicationEditView):
+    form_class = SecurityOtherDetailsForm
+
+    def get_initial(self):
+        return {"other_security_approval_details": self.application["other_security_approval_details"]}
+
+    def get_edit_payload(self, form):
+        return get_other_details_data(form)
+
+
+class EditF1686Details(BaseApplicationEditView):
+    form_class = F1686DetailsForm
+
+    def get_initial(self):
+        f1686_approval_date = self.application["f1686_approval_date"]
+        if f1686_approval_date:
+            f1686_approval_date = datetime.fromisoformat(self.application["f1686_approval_date"]).date()
+        return {
+            "f1686_contracting_authority": self.application["f1686_contracting_authority"],
+            "f1686_reference_number": self.application["f1686_reference_number"],
+            "f1686_approval_date": f1686_approval_date,
+            "is_f1686_approval_document_available": False,
+        }
+
+    def get_edit_payload(self, form):
+        return get_f1686_data(form)
+
+
+class EditExportDetails(
+    NonF680SecurityClassifiedFlagMixin,
+    BaseApplicationEditWizardView,
+):
+    form_list = [
+        (ExportDetailsSteps.SECURITY_CLASSIFIED, SecurityClassifiedDetailsForm),
+        (ExportDetailsSteps.F680_REFERENCE_NUMBER, F680ReferenceNumberForm),
+        (ExportDetailsSteps.F1686_DETAILS, F1686DetailsForm),
+        (ExportDetailsSteps.SECURITY_OTHER_DETAILS, SecurityOtherDetailsForm),
+    ]
+
+    condition_dict = {
+        ExportDetailsSteps.F680_REFERENCE_NUMBER: is_f680_approval,
+        ExportDetailsSteps.F1686_DETAILS: is_f1686_approval,
+        ExportDetailsSteps.SECURITY_OTHER_DETAILS: is_other_approval,
+    }
+
+    def get_form_initial(self, step):
+        initial = super().get_form_initial(step)
+        if step == ExportDetailsSteps.SECURITY_CLASSIFIED:
+            initial.update(get_initial_security_classified_details(self.application))
+        if step == ExportDetailsSteps.F680_REFERENCE_NUMBER:
+            initial.update(get_initial_f680_reference_number(self.application))
+        if step == ExportDetailsSteps.F1686_DETAILS:
+            initial.update(get_initial_f1686_details(self.application))
+        if step == ExportDetailsSteps.SECURITY_OTHER_DETAILS:
+            initial.update(get_initial_other_security_approval_details(self.application))
+
+        return initial
+
+    def get_context_data(self, form, **kwargs):
+        ctx = super().get_context_data(form, **kwargs)
+        ctx["back_link_url"] = self.get_success_url()
+        ctx["title"] = form.Layout.TITLE
+        return ctx
--- unit_tests/exporter/applications/views/application_export_details/test_export_details_edit_views.py
+++ unit_tests/exporter/applications/views/application_export_details/test_export_details_edit_views.py
@@ -1,5 +1,16 @@
+from datetime import datetime
 import pytest
+
 from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from exporter.applications.views.application_export_details.constants import ExportDetailsSteps
+from exporter.applications.views.application_export_details.forms import (
+    F680ReferenceNumberForm,
+    F1686DetailsForm,
+    SecurityOtherDetailsForm,
+    SecurityClassifiedDetailsForm,
+)
 
 
 @pytest.fixture(autouse=True)
@@ -23,6 +34,22 @@ def application_export_details_summary_url(data_standard_case):
     )
 
 
+@pytest.fixture
+def edit_export_details_url(application):
+    url = reverse(
+        "applications:edit_export_details",
+        kwargs={
+            "pk": application["id"],
+        },
+    )
+    return url
+
+
+@pytest.fixture
+def post_to_edit_export_details(post_to_step_factory, edit_export_details_url):
+    return post_to_step_factory(edit_export_details_url)
+
+
 @pytest.mark.parametrize(
     "url_name, form_data, expected",
     (
@@ -30,6 +57,27 @@ def application_export_details_summary_url(data_standard_case):
             "edit_export_details_f680_reference_number",
             {"f680_reference_number": "new ref number"},
             {"f680_reference_number": "new ref number"},
+        ),
+        (
+            "edit_export_details_security_other_details",
+            {"other_security_approval_details": "other details"},
+            {"other_security_approval_details": "other details"},
+        ),
+        (
+            "edit_export_details_f1686_details",
+            {
+                "is_f1686_approval_document_available": False,
+                "f1686_contracting_authority": "signed by the joe",
+                "f1686_reference_number": "dummy ref",
+                "f1686_approval_date_0": "02",
+                "f1686_approval_date_1": "02",
+                "f1686_approval_date_2": "2020",
+            },
+            {
+                "f1686_contracting_authority": "signed by the joe",
+                "f1686_reference_number": "dummy ref",
+                "f1686_approval_date": "2020-02-02",
+            },
         ),
     ),
 )
@@ -51,3 +99,146 @@ def test_edit_export_details_post(
     assert response.status_code == 302
     assert response.url == application_export_details_summary_url
     assert requests_mock.last_request.json() == expected
+
+
+@pytest.mark.parametrize(
+    "url_name,application_data,initial",
+    (
+        (
+            "edit_export_details_f680_reference_number",
+            {"f680_reference_number": "new ref number"},
+            {"f680_reference_number": "new ref number"},
+        ),
+        (
+            "edit_export_details_security_other_details",
+            {"other_security_approval_details": "other details"},
+            {"other_security_approval_details": "other details"},
+        ),
+        (
+            "edit_export_details_f1686_details",
+            {
+                "f1686_contracting_authority": "signed by the joe",
+                "f1686_reference_number": "dummy ref",
+                "f1686_approval_date": "2020-02-02",
+            },
+            {
+                "is_f1686_approval_document_available": False,
+                "f1686_contracting_authority": "signed by the joe",
+                "f1686_reference_number": "dummy ref",
+                "f1686_approval_date": datetime.fromisoformat("2020-02-02").date(),
+            },
+        ),
+    ),
+)
+def test_edit_export_details_initial(
+    authorized_client,
+    application,
+    url_name,
+    application_data,
+    initial,
+):
+    application.update(application_data)
+
+    url = reverse(f"applications:{url_name}", kwargs={"pk": application["id"]})
+    response = authorized_client.get(url)
+
+    assert response.status_code == 200
+    assert response.context["form"].initial == initial
+
+
+def test_edit_export_details_true(
+    authorized_client,
+    edit_export_details_url,
+    post_to_edit_export_details,
+    mock_application_put,
+    application,
+):
+    application_data = {
+        "is_mod_security_approved": True,
+        "security_approvals": ["F680", "F1686", "Other"],
+        "f680_reference_number": "dummy ref 1",
+        "f1686_contracting_authority": "dummy contracting authority 1",
+        "is_f1686_approval_document_available": False,
+        "other_security_approval_details": "other security approval details 1",
+    }
+    application.update(application_data)
+
+    response = authorized_client.get(edit_export_details_url)
+    assert response.status_code == 200
+
+    assert isinstance(response.context["form"], SecurityClassifiedDetailsForm)
+    assert response.context["form"].initial == {
+        "is_mod_security_approved": True,
+        "security_approvals": ["F680", "F1686", "Other"],
+    }
+
+    response = post_to_edit_export_details(
+        ExportDetailsSteps.SECURITY_CLASSIFIED,
+        {
+            "is_mod_security_approved": True,
+            "security_approvals": ["F680", "F1686", "Other"],
+        },
+    )
+    assert response.status_code == 200
+
+    assert isinstance(response.context["form"], F680ReferenceNumberForm)
+    assert response.context["form"].initial == {
+        "f680_reference_number": "dummy ref 1",
+    }
+
+    response = post_to_edit_export_details(
+        ExportDetailsSteps.F680_REFERENCE_NUMBER,
+        {
+            "f680_reference_number": "dummy ref 2",
+        },
+    )
+
+    assert response.status_code == 200
+
+    assert isinstance(response.context["form"], F1686DetailsForm)
+
+    assert response.context["form"].initial == {
+        "f1686_contracting_authority": "dummy contracting authority 1",
+        "is_f1686_approval_document_available": False,
+        "f1686_approval_date": None,
+        "f1686_reference_number": None,
+    }
+
+    response = post_to_edit_export_details(
+        ExportDetailsSteps.F1686_DETAILS,
+        {
+            "f1686_contracting_authority": "dummy contracting authority 2",
+            "is_f1686_approval_document_available": True,
+            "f1686_approval_document": SimpleUploadedFile("data sheet", b"This is a an approval document"),
+        },
+    )
+
+    assert response.status_code == 200
+    assert isinstance(response.context["form"], SecurityOtherDetailsForm)
+
+    assert response.context["form"].initial == {"other_security_approval_details": "other security approval details 1"}
+
+    response = post_to_edit_export_details(
+        ExportDetailsSteps.SECURITY_OTHER_DETAILS,
+        {"other_security_approval_details": "other security approval details 2"},
+    )
+
+    assert response.status_code == 302
+
+    assert mock_application_put.called_once
+    last_request = mock_application_put.last_request
+
+    assert last_request.json() == {
+        "security_approvals": ["F680", "F1686", "Other"],
+        "is_mod_security_approved": True,
+        "f680_reference_number": "dummy ref 2",
+        "f1686_contracting_authority": "dummy contracting authority 2",
+        "other_security_approval_details": "other security approval details 2",
+    }
+
+    assert response.url == reverse(
+        "applications:application_export_details_summary",
+        kwargs={
+            "pk": application["id"],
+        },
+    )
