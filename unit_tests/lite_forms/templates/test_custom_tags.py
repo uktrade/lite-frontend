9d738c9e79ee2ff2ab8c2725ee50cf343ef18aa9
depsiatwal <depsi.a@gmail.com>
2022-09-23 10:19:43 +0100

    add choice labels

--- unit_tests/lite_forms/templates/test_custom_tags.py
+++ unit_tests/lite_forms/templates/test_custom_tags.py
@@ -3,6 +3,17 @@ import pytest
 from lite_forms.templatetags import custom_tags
 
 
+@pytest.fixture()
+def dummy_choice():
+    class FakeType:
+        choices = (
+            ("T1", "label 1"),
+            ("T2", "label 2"),
+        )
+
+    return FakeType
+
+
 @pytest.mark.parametrize(
     "filename,expected",
     [
@@ -12,3 +23,16 @@ from lite_forms.templatetags import custom_tags
 )
 def test_file_type(filename, expected):
     assert expected == custom_tags.file_type(filename)
+
+
+@pytest.mark.parametrize(
+    "items, expected",
+    [
+        ([], ""),
+        (None, ""),
+        (["T1"], "label 1"),
+        (["T1", "T2"], "label 1, label 2"),
+    ],
+)
+def test_list_to_choice_labels(items, expected, dummy_choice):
+    assert expected == custom_tags.list_to_choice_labels(items, dummy_choice)
