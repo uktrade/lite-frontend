4c41115ed5f98da24d223135c80f3e5a72761ec2
depsiatwal <depsi.a@gmail.com>
2022-09-28 12:35:16 +0100

    add conditionals to edit form

--- exporter/applications/views/security_approvals/conditionals.py
+++ exporter/applications/views/security_approvals/conditionals.py
@@ -15,3 +15,28 @@ def is_f1686_approval(wizard):
 def is_other_approval(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step(SecurityApprovalSteps.SECURITY_CLASSIFIED)
     return SecurityClassifiedApprovalsType.OTHER in cleaned_data.get("security_approvals", [])
+
+
+def is_f680_approval_changed_and_selected(wizard):
+
+    cleaned_data = wizard.get_cleaned_data_for_step(SecurityApprovalSteps.SECURITY_CLASSIFIED)
+    return (
+        SecurityClassifiedApprovalsType.F680 in cleaned_data.get("security_approvals", [])
+        and not SecurityClassifiedApprovalsType.F680 in wizard.application.security_approvals
+    )
+
+
+def is_f1686_approval_changed_and_selected(wizard):
+    cleaned_data = wizard.get_cleaned_data_for_step(SecurityApprovalSteps.SECURITY_CLASSIFIED)
+    return (
+        SecurityClassifiedApprovalsType.F1686 in cleaned_data.get("security_approvals", [])
+        and not SecurityClassifiedApprovalsType.F1686 in wizard.application.security_approvals
+    )
+
+
+def is_other_approval_changed_and_selected(wizard):
+    cleaned_data = wizard.get_cleaned_data_for_step(SecurityApprovalSteps.SECURITY_CLASSIFIED)
+    return (
+        SecurityClassifiedApprovalsType.OTHER in cleaned_data.get("security_approvals", [])
+        and not SecurityClassifiedApprovalsType.OTHER in wizard.application.security_approvals
+    )
