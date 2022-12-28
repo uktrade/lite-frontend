adf8cffa6fdc0168e45dfe93a115a46ab768d623
Sekhar Panja <sekhar.panja@digital.trade.gov.uk>
2022-12-28 11:33:41 +0000

    addressing review comments

--- caseworker/assets/javascripts/regime-entries.js
+++ caseworker/assets/javascripts/regime-entries.js
@@ -5,34 +5,9 @@ export default function initRegimeEntries() {
     "regime_entries"
   );
 
-  if (!(regimeEntriesField)) return;
-
-  // adding place for "rating may need alternative CLC"
-  const regimeEntriesTokenFieldInfo = document.createElement("div");
-  regimeEntriesField.parentElement.appendChild(
-    regimeEntriesTokenFieldInfo
-  );
+  if (!regimeEntriesField) return;
 
   const regimeEntriesTokenField = progressivelyEnhanceMultipleSelectField(
     regimeEntriesField
   );
-
-  regimeEntriesTokenField.on("change", function (tokenField) {
-    const note =
-      " may need an alternative regime entry because of its destination";
-    const messages = tokenField
-      .getItems()
-      .filter(function (item) {
-        return item.name.match(/[a-zA-Z]$/) !== null;
-      })
-      .map(function (item) {
-        return "<div>" + item.name + note + "</div>";
-      });
-    if (messages.length > 0) {
-      regimeEntriesTokenFieldInfo.innerHTML =
-        "<div class='govuk-inset-text'>" + messages.join("") + "</div>";
-    } else {
-      regimeEntriesTokenFieldInfo.innerHTML = "";
-    }
-  });
 }
