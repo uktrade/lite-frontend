69f8628003d3d2af9baf6079f8f22c589a7a6870
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-12-28 12:25:44 +0000

    Update from deprecated find element methods

--- ui_tests/caseworker/step_defs/test_view_goods.py
+++ ui_tests/caseworker/step_defs/test_view_goods.py
@@ -1,14 +1,19 @@
 from pytest_bdd import scenarios, when, then
 
+from selenium.webdriver.common.by import By
+
 scenarios("../features/view_goods.feature", strict_gherkin=False)
 
 
 @when("I click the first good on the case")  # noqa
 def click_good_on_case(driver, context):
-    driver.find_element_by_link_text(context.goods[0]["good"]["name"]).click()
+    driver.find_element(
+        by=By.LINK_TEXT,
+        value=context.goods[0]["good"]["name"],
+    ).click()
 
 
 @then("I see the good details")  # noqa
 def good_details_visible(driver, context):
-    assert driver.find_element_by_tag_name("h2").text == "Product details"
-    assert driver.find_element_by_css_selector("div.govuk-caption-l").text == context.goods[0]["good"]["name"]
+    assert driver.find_element(by=By.TAG_NAME, value="h2").text == "Product details"
+    assert driver.find_element(by=By.CSS_SELECTOR, value="div.govuk-caption-l").text == context.goods[0]["good"]["name"]
