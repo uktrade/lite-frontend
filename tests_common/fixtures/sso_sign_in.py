8eb3ddde7cca85f0478f9c513e52b23ebf1c9b59
Brendan Smith <brendan@bsmitty.co.uk>
2023-01-16 14:08:55 +0000

    Skip visiting SSO login page in UI tests if we are mocking SSO

--- tests_common/fixtures/sso_sign_in.py
+++ tests_common/fixtures/sso_sign_in.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium.webdriver.common.by import By
 
 from pytest import fixture
@@ -5,10 +7,12 @@ from pytest import fixture
 
 @fixture(scope="session")
 def sso_sign_in(driver, internal_url, sso_sign_in_url, internal_info, context, api_test_client):
-    driver.get(sso_sign_in_url)
-    driver.find_element(by=By.NAME, value="username").send_keys(internal_info["email"])
-    driver.find_element(by=By.NAME, value="password").send_keys(internal_info["password"])
-    driver.find_element(by=By.CSS_SELECTOR, value="[type='submit']").click()
+    # Skip visiting the SSO signin page if our app is set to use mock SSO
+    if not os.environ.get("MOCK_SSO_ACTIVATE_ENDPOINTS") == "True":
+        driver.get(sso_sign_in_url)
+        driver.find_element(by=By.NAME, value="username").send_keys(internal_info["email"])
+        driver.find_element(by=By.NAME, value="password").send_keys(internal_info["password"])
+        driver.find_element(by=By.CSS_SELECTOR, value="[type='submit']").click()
     driver.get(internal_url)
     context.org_id = api_test_client.context["org_id"]
     context.org_name = api_test_client.context["org_name"]
