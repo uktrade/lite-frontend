f1f6aa3ab3be372b5f3b4faf5ab6b788f6c5b10a
Kevin Carrogan <kevin@carrogan.com>
2023-01-31 19:55:59 +0000

    Determine target branch based off current branch

---
+++ which_branch.py
@@ -0,0 +1,44 @@
+import os
+import re
+
+
+def get_current_branch():
+    return os.environ.get("CIRCLE_BRANCH", "dev")
+
+
+BASE_TARGET_MAP = {
+    "dev": "uat",
+    "uat": "master",
+}
+
+
+HOTFIX_BRANCH_RE = re.compile("^hotfix-(.*)-.*$")
+
+
+def is_hotfix_branch(branch):
+    return bool(HOTFIX_BRANCH_RE.match(branch))
+
+
+def get_hotfix_target(branch):
+    return HOTFIX_BRANCH_RE.match(branch).groups()[0]
+
+
+DEFAULT_BRANCH = "dev"
+
+
+def get_presumed_target(branch):
+    try:
+        return BASE_TARGET_MAP[branch]
+    except KeyError:
+        pass
+
+    if is_hotfix_branch(branch):
+        return get_hotfix_target(branch)
+
+    return DEFAULT_BRANCH
+
+
+if __name__ == "__main__":
+    current_branch = get_current_branch()
+    presumed_target = get_presumed_target(current_branch)
+    print(presumed_target)
