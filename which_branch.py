c980bc8c929a2fef3ecfc3adde2cf4434b609e04
Kevin Carrogan <kevin@carrogan.com>
2023-02-01 11:23:54 +0000

    Add tests for `which_branch.py`

--- which_branch.py
+++ which_branch.py
@@ -2,18 +2,25 @@ import os
 import re
 import sys
 
-
-def get_current_branch():
-    return os.environ.get("CIRCLE_BRANCH", "dev")
-
-
 BASE_TARGET_MAP = {
     "dev": "uat",
     "uat": "master",
 }
 
+HOTFIX_BRANCH_RE = re.compile("^hotfix-(uat|master)")
 
-HOTFIX_BRANCH_RE = re.compile("^hotfix-(.*)-.*$")
+DEFAULT_BRANCH = "dev"
+
+
+class UnknownBranch(Exception):
+    pass
+
+
+def get_current_branch():
+    try:
+        return os.environ["CIRCLE_BRANCH"]
+    except KeyError:
+        raise UnknownBranch()
 
 
 def is_hotfix_branch(branch):
@@ -22,9 +29,6 @@ def is_hotfix_branch(branch):
 
 def get_hotfix_target(branch):
     return HOTFIX_BRANCH_RE.match(branch).groups()[0]
-
-
-DEFAULT_BRANCH = "dev"
 
 
 def get_presumed_target(branch):
@@ -39,16 +43,20 @@ def get_presumed_target(branch):
     return DEFAULT_BRANCH
 
 
-def get_branch():
-    desired_branch = sys.argv[1]
+def get_branch(desired_branch):
     if desired_branch != "auto":
         return desired_branch
 
-    current_branch = get_current_branch()
+    try:
+        current_branch = get_current_branch()
+    except UnknownBranch:
+        return "dev"
+
     presumed_target = get_presumed_target(current_branch)
 
     return presumed_target
 
 
 if __name__ == "__main__":
-    print(get_branch())
+    desired_branch = sys.argv[1]
+    print(get_branch(desired_branch))
