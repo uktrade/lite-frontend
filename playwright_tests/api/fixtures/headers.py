a92351603121fcb7c11252da99c81771e8fa4463
Kevin Carrogan <kevin@carrogan.com>
2023-02-02 14:34:09 +0000

    Remove playwright
    The usage of playwright was an experiment to try to move away from Selenium but the end result was to stick with Selenium
    
    We won't be attempting to move to playright any time soon so this removes any of the remnants of that experiment

--- playwright_tests/api/fixtures/headers.py
+++
@@ -1,6 +0,0 @@
-def exporter(token, organisationId):
-    return {"ORGANISATION-ID": organisationId, "EXPORTER-USER-TOKEN": token, "Content-Type": "application/json"}
-
-
-def caseworker(token):
-    return {"GOV-USER-TOKEN": token}
--- playwright_tests/api/fixtures/route_of_goods.py
+++
@@ -1,2 +0,0 @@
-def route_of_goods():
-    return {"is_shipped_waybill_or_lading": True}
