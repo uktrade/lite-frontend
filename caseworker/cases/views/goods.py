ef93176c47ba181b8752a16d710662a74e11e7da
Anthony Roy <anthony.roy@madetech.com>
2023-02-15 15:34:03 +0000

    Remove unused ReviewGoods and related views

--- caseworker/cases/views/goods.py
+++ caseworker/cases/views/goods.py
@@ -1,43 +1,16 @@
-from django.shortcuts import redirect
-from django.urls import reverse
 from django.utils.functional import cached_property
 from django.views.generic import FormView
 
-from caseworker.cases.forms.review_goods import review_goods_form
-from caseworker.cases.helpers.advice import get_param_goods, flatten_goods_data
 from caseworker.cases.helpers.summaries import get_good_on_application_summary
 from caseworker.cases.services import (
     get_case,
-    post_review_goods,
     get_good_on_application,
     get_good_on_application_documents,
 )
-from caseworker.core.constants import Permission
-from caseworker.core.helpers import has_permission
-from caseworker.core.services import get_control_list_entries
 from caseworker.search.forms import SearchForm
 from caseworker.search.services import get_application_search_results
 from core.auth.views import LoginRequiredMixin
 from core.constants import OrganisationDocumentType
-from lite_forms.views import SingleFormView
-
-
-class ReviewGoods(LoginRequiredMixin, SingleFormView):
-    def init(self, request, **kwargs):
-        case_url = reverse("cases:case", kwargs={"queue_pk": kwargs["queue_pk"], "pk": kwargs["pk"]})
-        if not has_permission(request, Permission.REVIEW_GOODS):
-            return redirect(case_url)
-        self.object_pk = kwargs["pk"]
-
-        case = get_case(request, self.object_pk)
-        param_goods = get_param_goods(request, case)
-        control_list_entries = get_control_list_entries(request, convert_to_options=True)
-
-        self.data = flatten_goods_data(param_goods)
-        self.form = review_goods_form(control_list_entries=control_list_entries, back_url=case_url)
-        self.context = {"case": case, "goods": param_goods}
-        self.action = post_review_goods
-        self.success_url = case_url
 
 
 class GoodDetails(LoginRequiredMixin, FormView):
--- unit_tests/caseworker/cases/forms/test_review_goods.py
+++ unit_tests/caseworker/cases/forms/test_review_goods.py
@@ -1,9 +1,7 @@
 import pytest
 import requests
 
-from caseworker.cases.forms import review_goods
 from caseworker.core.services import get_control_list_entries
-from core.middleware import RequestsSessionMiddleware
 
 
 @pytest.fixture
@@ -13,47 +11,3 @@ def control_list_entries(mock_control_list_entries, rf, client):
     request.requests_session = requests.Session()
     data = get_control_list_entries(request, convert_to_options=True)
     return [(item.value, item.key) for item in data]
-
-
-def test_export_control_characteristics_form_no_clc(control_list_entries):
-    form = review_goods.ExportControlCharacteristicsForm(
-        control_list_entries_choices=control_list_entries,
-        data={
-            "control_list_entries": [],
-            "report_summary": "Foo bar",
-        },
-    )
-    assert form.is_valid() is False
-    assert form.errors["does_not_have_control_list_entries"] == [form.MESSAGE_NO_CLC_REQUIRED]
-
-
-def test_export_control_characteristics_form_mutex(control_list_entries):
-    form = review_goods.ExportControlCharacteristicsForm(
-        control_list_entries_choices=control_list_entries,
-        data={
-            "control_list_entries": ["ML1"],
-            "does_not_have_control_list_entries": True,
-            "report_summary": "Foo bar",
-        },
-    )
-    assert form.is_valid() is False
-    assert form.errors["does_not_have_control_list_entries"] == [form.MESSAGE_NO_CLC_MUTEX]
-
-
-@pytest.mark.parametrize(
-    "data",
-    [
-        {
-            "control_list_entries": ["ML1"],
-            "report_summary": "Foo bar",
-        },
-        {"control_list_entries": ["ML1"], "report_summary": "Foo bar", "is_precedent": True},
-        {"control_list_entries": ["ML1"], "report_summary": "Foo bar", "is_precedent": False},
-    ],
-)
-def test_export_control_characteristics_form_ok(control_list_entries, data):
-    form = review_goods.ExportControlCharacteristicsForm(
-        control_list_entries_choices=control_list_entries,
-        data=data,
-    )
-    assert form.is_valid() is True
