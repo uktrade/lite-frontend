7aca52d4049ec51cbcb25ae4a854d551b10de5dc
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2023-02-20 12:25:23 +0000

    LTD-3294: enable duplicate keys in pagination
    We encountered a bug in the denials search, where duplicate keys were
    being removed in the pagination as it was converting the params to a
    regular dict. This commit uses a QueryDict which allows for duplicate
    params.

--- unit_tests/lite_forms/templates/test_custom_tags.py
+++ unit_tests/lite_forms/templates/test_custom_tags.py
@@ -12,3 +12,19 @@ from lite_forms.templatetags import custom_tags
 )
 def test_file_type(filename, expected):
     assert expected == custom_tags.file_type(filename)
+
+
+@pytest.mark.parametrize(
+    "url, expected",
+    [
+        (
+            "https://www.example.com/?page=1&example_param=foo&example_param=bar",
+            "https://www.example.com/?page=2&example_param=foo&example_param=bar",
+        ),
+        ("https://www.example.com/?page=3&example_param=bar", "https://www.example.com/?page=2&example_param=bar"),
+        ("https://www.example.com/?example_param=bar", "https://www.example.com/?example_param=bar&page=2"),
+    ],
+)
+def test_pagination_params(url, expected):
+    page = 2
+    assert custom_tags.pagination_params(url, page) == expected
