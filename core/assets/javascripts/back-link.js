1b0f102043a933f677d105354fa03b3a3241227d
Brendan Smith <brendan@bsmitty.co.uk>
2023-02-20 15:37:45 +0000

    Add mechanism to override back-link JS

--- core/assets/javascripts/back-link.js
+++ core/assets/javascripts/back-link.js
@@ -1,9 +1,11 @@
-$(".govuk-back-link").on("click", function () {
-  var address = $(this).attr("href");
-  if (address != "#") {
-    window.location.href = address;
-  } else {
-    window.history.go(-1);
-  }
-  return false;
-});
+$(".govuk-back-link")
+  .not(".govuk-back-link-nojs")
+  .on("click", function () {
+    var address = $(this).attr("href");
+    if (address != "#") {
+      window.location.href = address;
+    } else {
+      window.history.go(-1);
+    }
+    return false;
+  });
