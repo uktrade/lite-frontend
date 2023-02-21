46396de31fa147410bb1478c72dd560d467d5d65
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2023-02-21 09:48:15 +0000

    LTD-3294: refactor pagination test

--- unit_tests/lite_forms/templates/test_custom_tags.py
+++ unit_tests/lite_forms/templates/test_custom_tags.py
@@ -15,16 +15,24 @@ def test_file_type(filename, expected):
 
 
 @pytest.mark.parametrize(
-    "url, expected",
+    "url, page, expected",
     [
         (
             "https://www.example.com/?page=1&example_param=foo&example_param=bar",
+            2,
             "https://www.example.com/?page=2&example_param=foo&example_param=bar",
         ),
-        ("https://www.example.com/?page=3&example_param=bar", "https://www.example.com/?page=2&example_param=bar"),
-        ("https://www.example.com/?example_param=bar", "https://www.example.com/?example_param=bar&page=2"),
+        (
+            "https://www.example.com/?page=3&example_param=bar",
+            5,
+            "https://www.example.com/?page=5&example_param=bar",
+        ),
+        (
+            "https://www.example.com/?example_param=bar",
+            "10",
+            "https://www.example.com/?example_param=bar&page=10",
+        ),
     ],
 )
-def test_pagination_params(url, expected):
-    page = 2
+def test_pagination_params(url, page, expected):
     assert custom_tags.pagination_params(url, page) == expected
