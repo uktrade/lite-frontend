5db7be7e6c74d750499ab03c5aa7ba57b98f8f9d
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2023-02-23 15:20:03 +0000

    LTD-3372: test banner appears when expected

--- unit_tests/caseworker/cases/test_permissions.py
+++ unit_tests/caseworker/cases/test_permissions.py
@@ -1,12 +1,12 @@
 import pytest
-from django.urls import reverse
+import re
+from core import client
 
-import rules
 from bs4 import BeautifulSoup
 from django.urls import reverse
 
 
-mock_gov_user_id = "2a43805b-c082-47e7-9188-c8b3e1a83cb0"
+mock_gov_user_id = "2a43805b-c082-47e7-9188-c8b3e1a83cb0"  # /PS-IGNORE
 
 
 @pytest.fixture(autouse=True)
@@ -90,3 +90,58 @@ def test_permission_move_case_forward_done_button(
         assert id_html_find_result
     else:
         assert id_html_find_result is None
+
+
+@pytest.fixture
+def mock_regime_entries_get(requests_mock, wassenaar_regime_entry):
+    regime_entries_url = client._build_absolute_uri("/static/regimes/entries/")
+    requests_mock.get(
+        re.compile(f"{regime_entries_url}ag|cwc|mtcr|nsg|wassenaar"),
+        json=[wassenaar_regime_entry],
+    )
+
+
+@pytest.mark.parametrize(
+    "view_name, is_user_case_officer, is_user_assigned",
+    (
+        ["cases:case", False, False],
+        ["cases:activities:notes-and-timeline", False, False],
+        ["cases:advice_view", False, False],
+        ["cases:tau:home", False, False],
+        ["cases:case", True, False],
+        ["cases:activities:notes-and-timeline", True, False],
+        ["cases:advice_view", True, False],
+        ["cases:tau:home", True, False],
+        ["cases:case", False, True],
+        ["cases:activities:notes-and-timeline", False, True],
+        ["cases:advice_view", False, True],
+        ["cases:tau:home", False, True],
+    ),
+)
+def test_warning_renders_when_expected(
+    authorized_client,
+    data_queue,
+    mock_gov_user,
+    mock_denial_reasons,
+    mock_control_list_entries_get,
+    mock_regime_entries_get,
+    mock_precedents_api,
+    data_standard_case,
+    view_name,
+    is_user_case_officer,
+    is_user_assigned,
+):
+    if is_user_case_officer:
+        data_standard_case["case"]["case_officer"] = mock_gov_user["user"]
+    elif is_user_assigned:
+        data_standard_case["case"]["assigned_users"] = {data_queue["name"]: [mock_gov_user["user"]]}
+    else:
+        data_standard_case["case"]["case_officer"] = None
+        data_standard_case["case"]["assigned_users"] = {}
+    url = reverse(view_name, kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"]})
+    response = authorized_client.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    if is_user_case_officer or is_user_assigned:
+        assert not soup.find(id="allocation-warning")
+    else:
+        assert soup.find(id="allocation-warning")
