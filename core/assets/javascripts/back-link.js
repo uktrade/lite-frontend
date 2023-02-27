22a2067033451b462409aaef12f21213ddb5c3bc
Brendan Smith <brendan@bsmitty.co.uk>
2023-02-27 10:36:57 +0000

    Improve back-link.js

--- core/assets/javascripts/back-link.js
+++ core/assets/javascripts/back-link.js
@@ -1,11 +1,9 @@
-$(".govuk-back-link")
-  .not(".govuk-back-link-nojs")
-  .on("click", function () {
-    var address = $(this).attr("href");
-    if (address != "#") {
-      window.location.href = address;
-    } else {
-      window.history.go(-1);
-    }
-    return false;
-  });
+$(".govuk-back-link:not(.govuk-back-link-nojs)").on("click", function () {
+  var address = $(this).attr("href");
+  if (address != "#") {
+    window.location.href = address;
+  } else {
+    window.history.go(-1);
+  }
+  return false;
+});
