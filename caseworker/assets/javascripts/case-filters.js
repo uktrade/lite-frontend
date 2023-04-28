b5e0aa50e2482a804b111cf722201de4fab8fe5c
Anthony Roy <anthony.roy@madetech.com>
2023-04-28 17:30:55 +0100

    Fix for countries

--- caseworker/assets/javascripts/case-filters.js
+++ caseworker/assets/javascripts/case-filters.js
@@ -4,27 +4,32 @@ import debounce from "lodash.debounce";
 
 import { progressivelyEnhanceMultipleSelectField } from "core/multi-select";
 
-
-const initCountryAutocompleteField = async (getDefaultValue) => {
+const initCountryAutocompleteField = async () => {
   const originalInput = document.querySelector("#id_country");
   const autocompleteContainer = document.createElement("div");
   autocompleteContainer.id = "filter_country_container";
   originalInput.parentElement.appendChild(autocompleteContainer);
   originalInput.style = "display:none";
-  const countryData = await fetch("/api/countries/")
-    .then((response) => response.json())
-    .then((results) => results["countries"]);
+  const countryData = null;
   let nameInput;
+  const getCountryData = async () => {
+    if (countryData) {
+      return countryData;
+    }
+    return await fetch("/api/countries/")
+      .then((response) => response.json())
+      .then((results) => results["countries"]);
+  };
   accessibleAutocomplete({
     element: document.querySelector("#filter_country_container"),
     id: "_id_country",
-    source: (query, populateResults) => {
+    source: async (query, populateResults) => {
       if (!query) {
         populateResults([{ id: null, name: "" }]);
         return;
       }
       populateResults(
-        countryData.filter((obj) =>
+        (await getCountryData()).filter((obj) =>
           obj.name.toLowerCase().includes(query.toLowerCase())
         )
       );
@@ -47,7 +52,9 @@ const initCountryAutocompleteField = async (getDefaultValue) => {
         originalInput.value = confirmed.id;
       }
     },
-    defaultValue: getDefaultValue(originalInput),
+    defaultValue: (await getCountryData()).filter((obj) => {
+      return obj.id === originalInput.value;
+    })[0].name,
     showNoOptionsFound: true,
     autoselect: true,
     confirmOnBlur: true,
@@ -62,8 +69,9 @@ const initRegimeEntryAutocompleteField = async (getDefaultValue) => {
   originalInput.parentElement.appendChild(autocompleteContainer);
   originalInput.style = "display:none";
   let nameInput;
-  const regimeEntriesData = await fetch("/api/regime-entries/")
-    .then((response) => response.json());
+  const regimeEntriesData = await fetch("/api/regime-entries/").then(
+    (response) => response.json()
+  );
   accessibleAutocomplete({
     element: document.querySelector("#filter_regime_entry_container"),
     id: "_id_regime_entry",
@@ -105,48 +113,46 @@ const initRegimeEntryAutocompleteField = async (getDefaultValue) => {
 };
 
 export default function initFlagsFiltersField() {
-    const flagsField = document.getElementById("flags");
-    if (!flagsField)
-        return;
+  const flagsField = document.getElementById("flags");
+  if (!flagsField) return;
 
-    const flagsTokenField = progressivelyEnhanceMultipleSelectField(
-        flagsField, (option) => {
-            return { id: option.value, name: option.label, classes: [] };
-        }
-    );
-  }
+  const flagsTokenField = progressivelyEnhanceMultipleSelectField(
+    flagsField,
+    (option) => {
+      return { id: option.value, name: option.label, classes: [] };
+    }
+  );
+}
 
 function showHideFilters() {
-    $(".case-filters").each(function () {
-        var $filters = $(this).parent();
-        $filters.hide();
-        $filters.prev().find("#show-filters-link").show();
-        $filters.prev().find("#hide-filters-link").hide();
+  $(".case-filters").each(function () {
+    var $filters = $(this).parent();
+    $filters.hide();
+    $filters.prev().find("#show-filters-link").show();
+    $filters.prev().find("#hide-filters-link").hide();
 
-        $(this)
-        .find("input, select")
-        .each(function () {
-            if (
-            $(this).val() != "" &&
-            $(this).val() != "Select" &&
-            $(this).val() != "blank" &&
-            $(this).attr("type") != "hidden" &&
-            ($(this).attr("type") != "checkbox" ||
-                ($(this).attr("type") == "checkbox" && $(this).attr("checked")))
-            ) {
-            $filters.show();
-            $filters.prev().find("#show-filters-link").hide();
-            $filters.prev().find("#hide-filters-link").show();
-            $(this).parents(".govuk-details").attr("open", "");
-            }
-        });
-    });
+    $(this)
+      .find("input, select")
+      .each(function () {
+        if (
+          $(this).val() != "" &&
+          $(this).val() != "Select" &&
+          $(this).val() != "blank" &&
+          $(this).attr("type") != "hidden" &&
+          ($(this).attr("type") != "checkbox" ||
+            ($(this).attr("type") == "checkbox" && $(this).attr("checked")))
+        ) {
+          $filters.show();
+          $filters.prev().find("#show-filters-link").hide();
+          $filters.prev().find("#hide-filters-link").show();
+          $(this).parents(".govuk-details").attr("open", "");
+        }
+      });
+  });
 }
 
 const initCaseFilters = () => {
-  initCountryAutocompleteField(
-    (originalInput) => originalInput.dataset.name || ""
-  );
+  initCountryAutocompleteField();
   initRegimeEntryAutocompleteField(
     (originalInput) => originalInput.dataset.name || ""
   );
@@ -155,15 +161,13 @@ const initCaseFilters = () => {
 
   showHideFilters();
 
-  $(".lite-filter-toggle-link")
-  .on(function () {
+  $(".lite-filter-toggle-link").on(function () {
     var $filters = $(this).parent().next();
     $filters.toggle();
     $(this).parent().find("> *").toggle();
 
     return false;
   });
-
 };
 
 initCaseFilters();
