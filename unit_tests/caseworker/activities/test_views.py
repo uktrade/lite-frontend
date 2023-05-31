09e8c94ab156abf59581a06f5400fcaf77fe93f1
Brendan Smith <brendan@bsmitty.co.uk>
2023-05-31 10:57:19 +0100

    Fix quick summary unit tests

--- unit_tests/caseworker/activities/test_views.py
+++ unit_tests/caseworker/activities/test_views.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 from pytest_django.asserts import assertTemplateUsed
 from bs4 import BeautifulSoup
 
-from django.test import override_settings
 from django.urls import reverse
 
 from core import client
@@ -37,17 +36,6 @@ def mock_get_gov_users(mock_gov_users, requests_mock):
     )
 
 
-@override_settings(FEATURE_QUICK_SUMMARY=True)
-def test_quick_summary(authorized_client, data_queue, data_standard_case):
-    url = reverse("cases:case", kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"]})
-    response = authorized_client.get(url)
-    assertTemplateUsed(response, "layouts/case.html")
-    assert response.context["tabs"][0].name == "Quick summary"
-    soup = BeautifulSoup(response.content, "html.parser")
-    assert soup.find("a", {"id": "tab-quick-summary"})
-
-
-@override_settings(FEATURE_QUICK_SUMMARY=True)
 def test_notes_and_timeline_tab(authorized_client, data_queue, data_standard_case):
     url = reverse("cases:case", kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"]})
     response = authorized_client.get(url)
--- unit_tests/caseworker/cases/views/test_quick_summary.py
+++ unit_tests/caseworker/cases/views/test_quick_summary.py
@@ -31,15 +31,12 @@ def setup(
     pass
 
 
-def test_case_summary_deactivated(authorized_client, data_queue, data_standard_case):
-    url = reverse("cases:case", kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"]})
-    response = authorized_client.get(url)
-    assertTemplateNotUsed(response, "case/tabs/quick-summary.html")
-
-
 @override_settings(FEATURE_QUICK_SUMMARY=True)
 def test_case_summary_activated(authorized_client, data_queue, data_standard_case):
-    url = reverse("cases:case", kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"]})
+    url = reverse(
+        "cases:case",
+        kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"], "tab": "quick-summary"},
+    )
     response = authorized_client.get(url)
     assertTemplateUsed(response, "case/tabs/quick-summary.html")
 
@@ -65,164 +62,84 @@ def test_case_summary_data(authorized_client, data_queue, data_standard_case):
     data_standard_case["case"]["data"]["sanction_matches"] = [{"list_name": "Russia"}]
     data_standard_case["case"]["data"]["goods"][0]["report_summary_prefix"] = {"name": "casing for"}
     data_standard_case["case"]["data"]["goods"][0]["report_summary_subject"] = {"name": "nuke"}
-    url = reverse("cases:case", kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"]})
+    url = reverse(
+        "cases:case",
+        kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"], "tab": "quick-summary"},
+    )
     response = authorized_client.get(url)
     html = BeautifulSoup(response.content, "html.parser")
 
-    status = html.find(id="status").text.strip()
-    assert status == data_standard_case["case"]["data"]["status"]["value"]
+    table_text = html.find("table", {"class": "app-table"}).text.strip()
 
-    licensing_unit_case_officer = html.find(id="licensing_unit_case_officer").text.strip()
-    assert licensing_unit_case_officer == f'{gov_user["first_name"]} {gov_user["last_name"]}'
+    expected_case_status = data_standard_case["case"]["data"]["status"]["value"]
+    assert expected_case_status in table_text
 
-    case_adviser = html.find(id="case_adviser").text.strip()
-    assert case_adviser == f'{gov_user["first_name"]} {gov_user["last_name"]}'
+    expected_lu_case_officer = f'{gov_user["first_name"]} {gov_user["last_name"]}'
+    assert expected_lu_case_officer in table_text
 
-    temporary_or_permanent = html.find(id="temporary_or_permanent").text.strip()
-    assert temporary_or_permanent == data_standard_case["case"]["data"]["export_type"]["key"]
+    expected_case_adviser = f'{gov_user["first_name"]} {gov_user["last_name"]}'
+    assert expected_lu_case_officer in table_text
 
-    assigned_queues = html.find(id="assigned_queues").text.strip()
-    assert assigned_queues == data_standard_case["case"]["queue_details"][0]["name"]
+    expected_temporary_or_permanent = data_standard_case["case"]["data"]["export_type"]["key"]
+    assert expected_temporary_or_permanent in table_text
 
-    flags = html.find(id="flags").text.strip()
-    assert flags == data_standard_case["case"]["all_flags"][0]["name"]
+    expected_assigned_queues = data_standard_case["case"]["queue_details"][0]["name"]
+    assert expected_assigned_queues in table_text
 
-    open_query = html.find(id="open_query").text.strip()
-    assert open_query == None if data_standard_case["case"]["has_open_queries"] else "Outstanding queries"
+    expected_flags = data_standard_case["case"]["all_flags"][0]["name"]
+    assert expected_flags in table_text
 
-    latest_action = html.find(id="latest_action").text.strip()
-    assert data_standard_case["case"]["latest_activity"]["text"] in latest_action
+    expected_open_query = "Outstanding queries" if data_standard_case["case"]["has_open_queries"] else "None"
+    assert expected_open_query in table_text
 
-    days_on_queue_elapsed = html.find(id="days_on_queue_elapsed").text.strip()
-    for queue_detail in data_standard_case["case"]["queue_details"]:
-        assert str((timezone.now() - parse(queue_detail["joined_queue_at"])).days) in days_on_queue_elapsed
+    expected_latest_action = data_standard_case["case"]["latest_activity"]["text"]
+    assert expected_latest_action in table_text
 
-    total_days_elapsed = html.find(id="total_days_elapsed").text.strip()
-    assert total_days_elapsed == str((timezone.now() - parse(data_standard_case["case"]["submitted_at"])).days)
+    expected_total_days_elapsed = str((timezone.now() - parse(data_standard_case["case"]["submitted_at"])).days)
+    assert expected_total_days_elapsed in table_text
 
-    product_names = html.find(id="product_names").text.strip()
     for good in data_standard_case["case"]["data"]["goods"]:
-        assert good["good"]["name"] in product_names
+        expected_product_name = good["good"]["name"]
+        assert expected_product_name in table_text
 
-    total_value = html.find(id="total_value").text.strip()
     val = Decimal()
+    expected_total_value = Decimal(0)
     for good in data_standard_case["case"]["data"]["goods"]:
-        val += Decimal(good["value"])
-    assert total_value == f"£{val}"
+        expected_total_value += Decimal(good["value"])
+    assert f"£{expected_total_value}" in table_text
 
-    control_list_entry = html.find(id="control_list_entry").text.strip()
     for good in data_standard_case["case"]["data"]["goods"]:
         for cle in good["control_list_entries"]:
-            assert cle["rating"] in control_list_entry
+            assert cle["rating"] in table_text
 
-    regime = html.find(id="regime").text.strip()
     for good in data_standard_case["case"]["data"]["goods"]:
         for regime_entry in good["regime_entries"]:
-            assert regime_entry["name"] in regime
+            assert regime_entry["name"] in table_text
 
-    report_summary = html.find(id="report_summary").text.strip()
     for good in data_standard_case["case"]["data"]["goods"]:
-        assert f'{good["report_summary_prefix"]["name"]} {good["report_summary_subject"]["name"]}' in report_summary
+        assert f'{good["report_summary_prefix"]["name"]} {good["report_summary_subject"]["name"]}' in table_text
 
-    security_graded = html.find(id="security_graded").text.strip()
-    assert security_graded.lower() == data_standard_case["case"]["data"]["goods"][0]["good"]["is_pv_graded"]
+    assert data_standard_case["case"]["data"]["goods"][0]["good"]["is_pv_graded"] in table_text.lower()
 
-    security_approvals = html.find(id="security_approvals").text.strip()
     for approval in data_standard_case["case"]["data"]["security_approvals"]:
-        assert approval in security_approvals
+        assert approval in table_text
 
-    applicant = html.find(id="applicant").text.strip()
-    assert applicant == data_standard_case["case"]["data"]["submitted_by"]
+    expected_applicant = data_standard_case["case"]["data"]["submitted_by"]
+    assert expected_applicant in table_text
 
-    destination = html.find(id="destination").text.strip()
     all_parties = (
         data_standard_case["case"]["data"]["ultimate_end_users"] + data_standard_case["case"]["data"]["third_parties"]
     )
     all_parties.append(data_standard_case["case"]["data"]["consignee"])
     all_parties.append(data_standard_case["case"]["data"]["end_user"])
     for party in all_parties:
-        assert party["country"]["name"] in destination
+        assert party["country"]["name"] in table_text
 
-    denial_matches = html.find(id="denial_matches").text.strip()
     for denial_match in data_standard_case["case"]["data"]["denial_matches"]:
-        assert denial_match["denial"]["reference"] in denial_matches
+        assert denial_match["denial"]["reference"] in table_text
 
-    sanction_matches = html.find(id="sanction_matches").text.strip()
     for sanction_match in data_standard_case["case"]["data"]["sanction_matches"]:
-        assert sanction_match["list_name"] in sanction_matches
+        assert sanction_match["list_name"] in table_text
 
-    end_use = html.find(id="end_use").text.strip()
-    assert end_use == data_standard_case["case"]["data"]["intended_end_use"]
-
-    end_user_document = html.find(id="end_user_document")
-    assert end_user_document == None
-
-
-@override_settings(FEATURE_QUICK_SUMMARY=True)
-def test_case_summary_missing_data(authorized_client, data_queue, data_standard_case):
-    data_standard_case["case"]["queue_details"] = []
-    data_standard_case["case"]["flags"] = []
-    data_standard_case["case"]["goods_flags"] = []
-    data_standard_case["case"]["destinations_flags"] = []
-    data_standard_case["case"]["has_open_queries"] = False
-    data_standard_case["case"]["latest_activity"] = None
-    data_standard_case["case"]["data"]["security_approvals"] = []
-    data_standard_case["case"]["data"]["denial_matches"] = []
-    data_standard_case["case"]["data"]["sanction_matches"] = []
-    for good in data_standard_case["case"]["data"]["goods"]:
-        good["control_list_entries"] = []
-        good["regime_entries"] = []
-        good["report_summary_prefix"] = None
-        good["report_summary_subject"] = None
-        good["good"]["is_pv_graded"] = "no"
-
-    url = reverse("cases:case", kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"]})
-    response = authorized_client.get(url)
-    html = BeautifulSoup(response.content, "html.parser")
-
-    licensing_unit_case_officer = html.find(id="licensing_unit_case_officer").text.strip()
-    assert licensing_unit_case_officer == "Not assigned"
-
-    case_adviser = html.find(id="case_adviser").text.strip()
-    assert case_adviser == "Not assigned"
-
-    assigned_queues = html.find(id="assigned_queues").text.strip()
-    assert assigned_queues == "Not assigned"
-
-    flags = html.find(id="flags").text.strip()
-    assert flags == "None"
-
-    open_query = html.find(id="open_query").text.strip()
-    assert open_query == "None"
-
-    latest_action = html.find(id="latest_action").text.strip()
-    assert latest_action == "None"
-
-    days_on_queue_elapsed = html.find(id="days_on_queue_elapsed").text.strip()
-    assert days_on_queue_elapsed == "N/A"
-
-    control_list_entry = html.find(id="control_list_entry").text.strip()
-    assert control_list_entry == "None"
-
-    regime = html.find(id="regime").text.strip()
-    assert regime == "None"
-
-    report_summary = html.find(id="report_summary").text.strip()
-    assert report_summary == "None"
-
-    security_graded = html.find(id="security_graded").text.strip()
-    assert security_graded == "No"
-
-    denial_matches = html.find(id="denial_matches").text.strip()
-    assert denial_matches == "None"
-
-    sanction_matches = html.find(id="sanction_matches").text.strip()
-    assert sanction_matches == "None"
-
-    data_standard_case["case"]["data"]["goods"][0]["report_summary_subject"] = {"name": "guns"}
-    url = reverse("cases:case", kwargs={"queue_pk": data_queue["id"], "pk": data_standard_case["case"]["id"]})
-    response = authorized_client.get(url)
-    html = BeautifulSoup(response.content, "html.parser")
-
-    report_summary = html.find(id="report_summary").text.strip()
-    assert report_summary == "guns"
+    expected_end_use = data_standard_case["case"]["data"]["intended_end_use"]
+    assert expected_end_use in table_text
