d49e9b25905cff723b938b83fc58711dd4ac5a06
Tomos Williams <tllewwilliams@gmail.com>
2023-06-28 14:15:40 +0100

    refactored and split out

--- caseworker/assets/javascripts/case-notes.js
+++ caseworker/assets/javascripts/case-notes.js
@@ -1,4 +1,4 @@
-import { progressivelyEnhanceMultipleSelectField } from "core/multi-select";
+import { progressivelyEnhanceMultipleSelectField } from "core/multi-select-starts-with";
 class CaseNote {
   TEXTAREA_FOCUSED_CLASS = "case-note__textarea--focused";
   constructor($el) {
@@ -80,64 +80,13 @@ const initCaseNotes = () => {
     .forEach(($el) => new CaseNote($el).init());
 };
 
-const getItem = (option) => {
-  return { id: option.value, name: option.text, classes: [] };
-};
-
-const getItems = (element) => {
-  var items = [];
-  for (var i = 0; i < element.options.length; i++) {
-    var option = element.options.item(i);
-    var item = getItem(option);
-    items.push(item);
-  }
-  return items;
-};
-
-const search = (terms, filterTerm) => {
-  filterTerm = filterTerm.toLowerCase();
-  return terms
-    .filter((v) => v.name.toLowerCase().includes(filterTerm))
-    .sort((a, b) => {
-      const aName = a.name;
-      const bName = b.name;
-      const aStarts = aName.toLowerCase().startsWith(filterTerm);
-      const bStarts = bName.toLowerCase().startsWith(filterTerm);
-      if (aStarts && bStarts) return aName.localeCompare(bName);
-      if (aStarts && !bStarts) return -1;
-      if (!aStarts && bStarts) return 1;
-      return aName.localeCompare(bName);
-    });
-};
-
-const itemList = getItems(document.getElementById("id_mentions"));
-let fieldInput = "";
-
 export default function initMentionUsers() {
   const mentionUserField = document.getElementById("id_mentions");
 
   if (!mentionUserField) return;
 
-  const mentionUserTokenField = progressivelyEnhanceMultipleSelectField(
-    mentionUserField,
-    (option) => {
-      return { id: option.value, name: option.label, classes: [] };
-    }
-  );
-
-  mentionUserTokenField.onInput = (input, event) => {
-    fieldInput = input;
-    return input;
-  };
-
-  const onSuggestions = (event) => {
-    sortedItems = search(itemList, fieldInput);
-    mentionUserTokenField.setSuggestedItems(sortedItems);
-  };
-
-  mentionUserTokenField.on("showSuggestions", (data, event) => {
-    onSuggestions();
-  });
+  const mentionUserTokenField =
+    progressivelyEnhanceMultipleSelectField(mentionUserField);
 }
 
 export { CaseNote, initCaseNotes, initMentionUsers };
