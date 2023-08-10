2bfd66b11587f35be8a91041ffea43761acd5219
Kevin Carrogan <kevin@carrogan.com>
2023-08-10 16:25:22 +0100

    Renaming select all component to better reflect its usage as just to do with checkboxes

--- caseworker/assets/javascripts/tau.js
+++ caseworker/assets/javascripts/tau.js
@@ -1,4 +1,4 @@
-import SelectAll from "core/select-all";
+import SelectAllCheckboxes from "core/select-all-checkboxes";
 import ExpandAll, { SHOW_ALL_BUTTON_TEXT } from "core/expand-all";
 import CheckboxClassToggler from "core/checkbox-class-toggler";
 import DisablingButton from "core/disabling-button";
@@ -24,23 +24,26 @@ const initSelectAll = (goods) => {
   let isAllSelected = null;
 
   const checkboxes = goods.querySelectorAll("[name=goods]");
-  const selectAll = new SelectAll(checkboxes, (_isAllSelected) => {
-    isAllSelected = _isAllSelected;
-    if (isAllSelected) {
-      selectAllButton.innerText = DESELECT_ALL_BUTTON_TEXT;
-    } else {
-      selectAllButton.innerText = SELECT_ALL_BUTTON_TEXT;
+  const selectAllCheckboxes = new SelectAllCheckboxes(
+    checkboxes,
+    (_isAllSelected) => {
+      isAllSelected = _isAllSelected;
+      if (isAllSelected) {
+        selectAllButton.innerText = DESELECT_ALL_BUTTON_TEXT;
+      } else {
+        selectAllButton.innerText = SELECT_ALL_BUTTON_TEXT;
+      }
     }
-  });
-  selectAll.init();
+  );
+  selectAllCheckboxes.init();
 
   selectAllButton.addEventListener("click", (evt) => {
     evt.preventDefault();
     evt.stopPropagation();
     if (isAllSelected) {
-      selectAll.deselectAll();
+      selectAllCheckboxes.deselectAll();
     } else {
-      selectAll.selectAll();
+      selectAllCheckboxes.selectAll();
     }
   });
 
