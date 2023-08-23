81e632630cfb4e75bf70f75db8e21b2f01351b66
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2023-08-23 17:15:48 +0100

    Extend Appeal rule to cover all cases
    Besides checking for feature flag below conditions are also checked,
    
     - if there is a valid licence then user cannot appeal
     - if there is no deadline set then user cannot appeal
     - if current date is past the deadline then user cannot appeal
    
    Update unit tests

--- exporter/applications/rules.py
+++ exporter/applications/rules.py
@@ -1,11 +1,22 @@
 import rules
 
+from datetime import datetime
+
 from django.conf import settings
 
 
 @rules.predicate
-def is_appeal_feature_flag_set(request):
-    return settings.FEATURE_FLAG_APPEALS
+def is_appeal_feature_flag_set(request, application):
+    if not settings.FEATURE_FLAG_APPEALS:
+        return False
+
+    if application and (application.licence or not application.appeal_deadline):
+        return False
+
+    appeal_deadline_date_str = application.appeal_deadline.split("T")[0]
+    appeal_deadline = datetime.strptime(appeal_deadline_date_str, "%Y-%m-%d")
+
+    return datetime.today().date() <= appeal_deadline.date()
 
 
 rules.add_rule("can_user_appeal_case", is_appeal_feature_flag_set)
--- unit_tests/exporter/applications/test_rules.py
+++ unit_tests/exporter/applications/test_rules.py
@@ -1,15 +1,35 @@
 import pytest
 import rules
 
+from datetime import datetime, timedelta
+
+from django.utils import timezone
+
+from exporter.core.objects import Application
+
 
 @pytest.mark.parametrize(
-    "flag_value, expected",
+    "flag_value, licence, days_until_deadline, expected",
     (
-        (True, True),
-        (False, False),
+        (False, None, None, False),
+        (True, {"reference_code": "GBSIEL/2023/0000001/P"}, None, False),
+        (True, None, -1, False),  # deadline is 1 day before today
+        (True, None, 0, True),  # today is the deadline
+        (True, None, 5, True),  # deadline is 5 days from today
     ),
 )
-def test_can_user_appeal_case_based_on_feature_flag(settings, flag_value, expected, rf):
+def test_can_user_appeal_case_based_on_feature_flag(
+    settings, rf, data_standard_case, flag_value, licence, days_until_deadline, expected
+):
     settings.FEATURE_FLAG_APPEALS = flag_value
+    data_standard_case["licence"] = licence
+    data_standard_case["appeal_deadline"] = ""
+
+    if days_until_deadline is not None:
+        appeal_deadline = timezone.localtime() + timedelta(days_until_deadline)
+        data_standard_case["appeal_deadline"] = appeal_deadline.isoformat()
+
+    application = Application(data_standard_case)
+
     request = rf.get("/")
-    assert rules.test_rule("can_user_appeal_case", request) is expected
+    assert rules.test_rule("can_user_appeal_case", request, application) is expected
