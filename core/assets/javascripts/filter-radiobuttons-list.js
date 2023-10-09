a6bbb3a76b9f768bbd6f6e07f4024abafdaf2641
Kevin Carrogan <kevin@carrogan.com>
2023-10-09 10:51:31 +0100

    Swap from var to let

--- core/assets/javascripts/filter-radiobuttons-list.js
+++ core/assets/javascripts/filter-radiobuttons-list.js
@@ -1,11 +1,11 @@
 $(".lite-search__container").show();
 
 $("#filter-box").on("input", function () {
-  var value = $(this).val().toLowerCase();
+  let value = $(this).val().toLowerCase();
 
   $(".govuk-radios__item").each(function (i, obj) {
-    var checkboxText = $(obj).find(".govuk-radios__label").text().toLowerCase();
-    var checkboxDescription = $(obj)
+    let checkboxText = $(obj).find(".govuk-radios__label").text().toLowerCase();
+    let checkboxDescription = $(obj)
       .find(".govuk-radios__hint")
       .text()
       .toLowerCase();
@@ -22,7 +22,7 @@ $("#filter-box").on("input", function () {
 });
 
 $("input[type='checkbox']").change(function () {
-  var checkboxText = $(this).parent().find(".govuk-radios__label").text();
+  let checkboxText = $(this).parent().find(".govuk-radios__label").text();
   addCheckedCheckboxesToList();
 });
 
@@ -40,7 +40,7 @@ function addCheckedCheckboxesToList() {
     $("input[type='checkbox']:checked").length + " Selected"
   );
   $("input[type='checkbox']:checked").each(function () {
-    var checkboxText = $(this).parent().find(".govuk-radios__label").text();
+    let checkboxText = $(this).parent().find(".govuk-radios__label").text();
     $("#checkbox-list").append(
       "<div><a class='govuk-link govuk-link--no-visited-state lite-checkbox-filter-link' href='#" +
         checkboxText.trim() +
@@ -62,7 +62,7 @@ function addCheckedCheckboxesToList() {
       event.preventDefault();
 
       // Store hash
-      var hash = this.hash.substr(1);
+      let hash = this.hash.substr(1);
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
@@ -82,8 +82,8 @@ function addCheckedCheckboxesToList() {
 
 addCheckedCheckboxesToList();
 
-var mark = function () {
-  var keyword = $("#filter-box").val();
+let mark = function () {
+  let keyword = $("#filter-box").val();
 
   // Remove previous marked elements and mark
   // the new keyword inside the context
