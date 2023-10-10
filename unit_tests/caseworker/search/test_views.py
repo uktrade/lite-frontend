4c891cc9d67955903d26bc7437b9e69acb4a6036
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2023-10-10 17:27:07 +0100

    Refactor existing product search view for the new Product search interface
    Replace it with a skeleton page which will be added with more options
    as we implement more features

--- unit_tests/caseworker/search/test_views.py
+++ unit_tests/caseworker/search/test_views.py
@@ -23,16 +23,6 @@ def mock_product_search(requests_mock, data_search):
     return requests_mock.get(url=url, json=data_search)
 
 
-def test_product_search_disabled_feature_flag(
-    authorized_client,
-    product_search_url,
-    settings,
-):
-    settings.FEATURE_FLAG_PRODUCT_SEARCH = False
-    response = authorized_client.get(product_search_url)
-    assert response.status_code == 404
-
-
 def test_product_search_enabled_feature_flag(authorized_client, product_search_url):
     response = authorized_client.get(product_search_url)
     assert response.status_code == 200
@@ -42,33 +32,3 @@ def test_product_search_renders_template(authorized_client, product_search_url):
     response = authorized_client.get(product_search_url)
     assertTemplateUsed(response, "search/products.html")
     assertTemplateUsed(response, "layouts/base.html")
-
-
-def test_product_search_search_results_no_querystring(
-    authorized_client,
-    product_search_url,
-    mock_product_search,
-    data_search,
-):
-    response = authorized_client.get(product_search_url)
-    assert mock_product_search.called_once
-    assert mock_product_search.last_request.qs == {}
-
-    search_results = response.context["search_results"]
-    assert search_results == data_search
-
-
-def test_product_search_search_results_querystring(
-    authorized_client,
-    product_search_url,
-    mock_product_search,
-    data_search,
-):
-    response = authorized_client.get(f"{product_search_url}?q=test")
-    assert mock_product_search.called_once
-    assert mock_product_search.last_request.qs == {
-        "q": ["test"],
-    }
-
-    search_results = response.context["search_results"]
-    assert search_results == data_search
