a78d2b3b55b72bf07e532d03927db3fe2a365a43
Tomos Williams <tllewwilliams@gmail.com>
2023-11-03 11:09:29 +0000

    update test

--- unit_tests/core/middleware/test_http_error.py
+++ unit_tests/core/middleware/test_http_error.py
@@ -1,6 +1,6 @@
 from requests import HTTPError
 from requests.models import Response
-
+from unittest import mock
 from core.middleware import HttpErrorHandlerMiddleware
 
 
@@ -14,9 +14,12 @@ def test_http_error_handler_non_http_error_exception(rf, mocker, caplog):
     assert not caplog.records
 
 
-def test_http_error_handler_with_http_error(rf, mocker, caplog):
+@mock.patch("core.middleware.error_page")
+def test_http_error_handler_with_http_error(mock_error_page, rf, mocker, caplog):
     get_response = mocker.MagicMock()
     http_error_handler = HttpErrorHandlerMiddleware(get_response)
+
+    mock_error_page.return_value = "Error Page"
 
     response = Response()
     response.status_code = 400
@@ -24,5 +27,5 @@ def test_http_error_handler_with_http_error(rf, mocker, caplog):
 
     http_error = HTTPError(response=response)
     request = rf.get("/")
-    assert http_error_handler.process_exception(request, http_error) is None
+    assert http_error_handler.process_exception(request, http_error) is "Error Page"
     assert ("INFO", '{"error": "this is an error"}') in [(r.levelname, r.msg) for r in caplog.records]
