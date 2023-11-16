f7883ae1e47d045eaac25b324b339a1c152d975f
Le H Ngo <hgle.ngo@gmail.com>
2023-11-16 11:27:11 +0000

    changes comment

--- unit_tests/caseworker/tau/test_previous_assessments.py
+++ unit_tests/caseworker/tau/test_previous_assessments.py
@@ -67,7 +67,7 @@ def data_good_precedent(data_standard_case, data_queue):
         "regime_entries": [{"pk": "regime-0001", "name": "some regime"}],
         "report_summary_prefix": {"id": "0001", "name": "some prefix"},
         "report_summary_subject": {"id": "0002", "name": "some subject"},
-        "comment": "woop!",
+        "comment": "some assessment note",
         "is_ncsc_military_information_security": False,
     }
 
@@ -165,7 +165,7 @@ def test_previous_assessments_GET(
         "some regime",
         "some prefix some subject",
         "No",
-        "woop!",
+        "some assessment note",
     ]
     assert get_td_text(table_rows[1]) == [
         "2.",
@@ -176,7 +176,7 @@ def test_previous_assessments_GET(
         "some regime",
         "some prefix some subject",
         "No",
-        "woop!",
+        "some assessment note",
     ]
 
     notification_banner = soup.find("p", class_="govuk-notification-banner__heading")
@@ -216,7 +216,7 @@ def test_previous_assessments_GET_single_precedent_and_single_new_product(
         "some regime",
         "some prefix some subject",
         "No",
-        "woop!",
+        "some assessment note",
     ]
     assert get_td_text(table_rows[1]) == ["2.", "p2 44  NOT YET ASSESSED", "", "", "", "", "", "", ""]
 
