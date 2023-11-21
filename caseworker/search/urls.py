778ceafbb70ef3e31d989e823c479dddb4ae9ae0
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2023-11-21 09:48:51 +0000

    Add endpoint for product search suggestions

--- caseworker/search/urls.py
+++ caseworker/search/urls.py
@@ -6,4 +6,5 @@ app_name = "search"
 
 urlpatterns = [
     path("products/", views.ProductSearchView.as_view(), name="products"),
+    path("products/suggest/", views.ProductSearchSuggestView.as_view(), name="api-search-suggest-product"),
 ]
