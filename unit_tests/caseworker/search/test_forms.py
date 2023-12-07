d7a1cf5619b510387c066b5743b1306fc29cde74
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2023-12-07 10:38:37 +0000

    Add test for ProductSearchSuggestForm

--- unit_tests/caseworker/search/test_forms.py
+++ unit_tests/caseworker/search/test_forms.py
@@ -1,6 +1,6 @@
 import pytest
 
-from caseworker.search.forms import ProductSearchForm
+from caseworker.search.forms import ProductSearchForm, ProductSearchSuggestForm
 
 
 @pytest.mark.parametrize(
@@ -20,5 +20,26 @@ from caseworker.search.forms import ProductSearchForm
 )
 def test_product_search_form(data, is_valid, errors):
     form = ProductSearchForm(data=data)
+    assert form.is_valid() is is_valid
+    assert form.errors == errors
+
+
+@pytest.mark.parametrize(
+    "data, is_valid, errors",
+    (
+        (
+            {},
+            False,
+            {"q": ["This field is required."]},
+        ),
+        (
+            {"q": 'end_user_country:"France"'},
+            True,
+            {},
+        ),
+    ),
+)
+def test_product_search_suggest_form(data, is_valid, errors):
+    form = ProductSearchSuggestForm(data=data)
     assert form.is_valid() is is_valid
     assert form.errors == errors
