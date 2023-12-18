8441a9dca45278eec80db73c7825d7dea122f1b2
Kevin Carrogan <kevin@carrogan.com>
2023-12-18 15:44:38 +0000

    Remove tab collections

--- caseworker/core/objects.py
+++ caseworker/core/objects.py
@@ -5,11 +5,3 @@ class Tab:
         self.url = url
         self.count = count
         self.has_template = has_template
-
-
-class TabCollection:
-    def __init__(self, id, name, children, count=0):
-        self.id = "tab-collection-" + id
-        self.name = name
-        self.children = children if children else []
-        self.count = count
