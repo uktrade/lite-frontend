3108750860c47eeb5a9842a3ded78b333e6f9563
Gurdeep Atwal <depsi.a@gmail.com>
2023-12-28 17:57:22 +0000

    m2

--- caseworker/mock_sso/urls.py
+++ caseworker/mock_sso/urls.py
@@ -7,6 +7,6 @@ app_name = "mock_sso"
 
 urlpatterns = [
     path("o/authorize/", views.Authorize.as_view(), name="authorize"),
-    path("o/token/", views.Token.as_view(), name="token"),
+    # path("o/token/", views.Token.as_view(), name="token"),
     path("api/v1/user/me/", views.APIUserMe.as_view(), name="api_user_me"),
 ]
--- exporter/mock_sso/urls.py
+++ exporter/mock_sso/urls.py
@@ -8,7 +8,7 @@ app_name = "mock_sso"
 
 urlpatterns = [
     path("authorize", views.Authorize.as_view(), name="authorize"),
-    path("token", views.Token.as_view(), name="token"),
+    # path("token", views.Token.as_view(), name="token"),
     path("userinfo", views.APIUserMe.as_view(), name="api_user_me"),
     path("logout", Logout.as_view(), name="logout"),
 ]
