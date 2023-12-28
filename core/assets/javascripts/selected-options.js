bbcb44ce3f10b01e30dd36360ee900b2144ab726
Kevin Carrogan <kevin@carrogan.com>
2023-12-28 11:27:34 +0000

    Style mutli select choices

--- core/assets/javascripts/selected-options.js
+++ core/assets/javascripts/selected-options.js
@@ -5,10 +5,16 @@ class SelectedOptions {
     this.multiSelectObjectsAsPlural = multiSelectObjectsAsPlural;
   }
 
-  init() {
-    this.$container = document.createElement("div");
-    this.$container.ariaLive = "polite";
+  createContainer() {
+    const $container = document.createElement("div");
+    $container.ariaLive = "polite";
+    $container.classList.add("selected-options");
 
+    return $container;
+  }
+
+  init() {
+    this.$container = this.createContainer();
     this.$el.appendChild(this.$container);
 
     this.render();
@@ -23,34 +29,68 @@ class SelectedOptions {
     this.$multiSelect.addEventListener("change", () => this.handleChange());
   }
 
-  render() {
-    this.$container.innerHTML = "";
+  createRemoveButton($option) {
+    const $button = document.createElement("button");
+    $button.textContent = "Remove";
+    $button.classList.add("selected-options__option-remove");
+    $button.addEventListener("click", () => {
+      $option.selected = false;
+      $option.dispatchEvent(new Event("change", { bubbles: true }));
+    });
 
+    return $button;
+  }
+
+  createListItem($option) {
+    const $li = document.createElement("li");
+    $li.classList.add("selected-options__option");
+
+    const $span = document.createElement("span");
+    $span.textContent = $option.textContent;
+    $span.classList.add("selected-options__option-text");
+    $li.appendChild($span);
+
+    const $button = this.createRemoveButton($option);
+    $li.appendChild($button);
+
+    return $li;
+  }
+
+  createSelectedParagraph() {
     const $p = document.createElement("p");
     $p.textContent = `Selected ${this.multiSelectObjectsAsPlural}`;
     $p.classList.add("govuk-visually-hidden");
+
+    return $p;
+  }
+
+  createList() {
+    const $ul = document.createElement("ul");
+    $ul.classList.add("selected-options__options");
+    for (const option of this.$multiSelect.selectedOptions) {
+      const $li = this.createListItem(option);
+      $ul.appendChild($li);
+    }
+
+    return $ul;
+  }
+
+  hasSelectedItems() {
+    return this.$multiSelect.selectedOptions.length > 0;
+  }
+
+  render() {
+    this.$container.innerHTML = "";
+    this.$container.classList.toggle(
+      "selected-options--empty",
+      !this.hasSelectedItems()
+    );
+
+    const $p = this.createSelectedParagraph();
     this.$container.appendChild($p);
 
-    const $ul = document.createElement("ul");
+    const $ul = this.createList();
     this.$container.appendChild($ul);
-
-    for (const option of this.$multiSelect.selectedOptions) {
-      const li = document.createElement("li");
-
-      const span = document.createElement("span");
-      span.textContent = option.textContent;
-      li.appendChild(span);
-
-      const button = document.createElement("button");
-      button.textContent = "Remove";
-      li.appendChild(button);
-      button.addEventListener("click", () => {
-        option.selected = false;
-        option.dispatchEvent(new Event("change", { bubbles: true }));
-      });
-
-      $ul.appendChild(li);
-    }
   }
 }
 
