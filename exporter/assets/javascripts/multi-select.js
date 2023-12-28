4052c7e346e7fdf39e317f8fc40cb1c494e629aa
Kevin Carrogan <kevin@carrogan.com>
2023-12-28 12:04:28 +0000

    Use jest mocking for multi select tests

--- exporter/assets/javascripts/multi-select.js
+++ exporter/assets/javascripts/multi-select.js
@@ -1,14 +1,9 @@
-import accessibleAutocomplete from "accessible-autocomplete";
-
 import MultiSelector from "core/multi-selector";
-import SelectedOptions from "core/selected-options";
 
 const initMultiSelects = () => {
   document
     .querySelectorAll("[data-module=multi-select]")
-    .forEach(($el) =>
-      new MultiSelector(accessibleAutocomplete, SelectedOptions, $el).init()
-    );
+    .forEach(($el) => new MultiSelector($el).init());
 };
 
 export default initMultiSelects;
