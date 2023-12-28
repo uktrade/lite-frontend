a62e3d0b84a8a67b14292ff540f0d8195a4dfa9e
Kevin Carrogan <kevin@carrogan.com>
2023-12-28 13:50:24 +0000

    Remove functionality of CLE suggestions for TAU

--- caseworker/assets/javascripts/tau.js
+++ caseworker/assets/javascripts/tau.js
@@ -3,14 +3,8 @@ import SelectAllButton from "core/select-all-button";
 import ExpandAll, { SHOW_ALL_BUTTON_TEXT } from "core/expand-all";
 import CheckboxClassToggler from "core/checkbox-class-toggler";
 import DisablingButton from "core/disabling-button";
-import Headline from "./assessment-form/headline";
-import SelectProducts from "./assessment-form/select-products";
-import CLESuggestions from "./tau/cle-suggestions";
-import SuggestionsTokenField from "./tau/suggestions-token-field";
-import NoSuggestionsTokenField from "./tau/no-suggestions-token-field";
 import initARS from "./tau/ars";
 import initRegimes from "./tau/regimes";
-import ShowHideNcscField from "./tau/show-hide-ncsc-field";
 
 const initSelectAll = (goods) => {
   const checkboxes = goods.querySelectorAll("[name=goods]");
@@ -86,64 +80,7 @@ const initCheckboxClassToggler = () => {
   ).init();
 };
 
-const initAssessmentForm = () => {
-  const headlineEl = document.querySelector(".assessment-form__headline");
-  const headline = new Headline(headlineEl);
-
-  const noControlListCheckboxEl = document.querySelector(
-    "[name=does_not_have_control_list_entries]"
-  );
-  const noSuggestionsTokenField = new NoSuggestionsTokenField(
-    "#control_list_entries",
-    noControlListCheckboxEl
-  );
-  noSuggestionsTokenField.init();
-
-  const suggestionsTokenField = new SuggestionsTokenField(
-    "#control_list_entries"
-  );
-
-  const suggestionsEl = document.createElement("div");
-  suggestionsEl.classList.add("tau-assessment-form__cle-suggestions");
-  const controlListEntriesLabel = document.querySelector(
-    "[for=control_list_entries]"
-  );
-  controlListEntriesLabel.parentNode.insertBefore(
-    suggestionsEl,
-    controlListEntriesLabel.nextSibling
-  );
-
-  const cleSuggestions = new CLESuggestions(
-    suggestionsEl,
-    (selectedSuggestions) => {
-      suggestionsTokenField.setSuggestions(selectedSuggestions);
-      noSuggestionsTokenField.reset();
-    }
-  );
-
-  const goods = document.querySelector("#div_id_goods");
-  const checkboxes = goods.querySelectorAll("[name=goods]");
-  const products = JSON.parse(
-    document.querySelector("#cle-suggestions-json").textContent
-  );
-  const ncscBox = document.querySelector(
-    "#div_id_is_ncsc_military_information_security"
-  );
-  if (ncscBox) {
-    const ncscFormField = new ShowHideNcscField(
-      "#control_list_entries",
-      ncscBox
-    );
-
-    ncscFormField.toggleField();
-    ncscFormField.setOnChangeListener();
-  }
-
-  new SelectProducts(checkboxes, products, (selectedProducts) => {
-    headline.setProducts(selectedProducts);
-    cleSuggestions.setProducts(selectedProducts);
-  }).init();
-};
+const initAssessmentForm = () => {};
 
 const initSaveAndContinueButton = () => {
   const button = document.querySelector("#submit-id-submit");
