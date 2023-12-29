4c8f4e1148662ed9011c34afd5315d32004ccc8f
Kevin Carrogan <kevin@carrogan.com>
2023-12-29 15:20:35 +0000

    Declaratively declare progressively enhanceable selects on case filters

--- caseworker/assets/javascripts/case-filters.js
+++ caseworker/assets/javascripts/case-filters.js
@@ -1,53 +1,20 @@
-import "fetch-polyfill";
 import accessibleAutocomplete from "accessible-autocomplete";
 import MultiSelector from "core/multi-selector";
 
-function initFlagsFiltersField() {
-  const flagsField = document.getElementById("flags");
-  if (!flagsField) return;
-  new MultiSelector(flagsField).init();
-}
-
-function initAssignedQueuesFiltersField() {
-  const assignedQueuesField = document.getElementById("assigned-queues");
-  if (!assignedQueuesField) return;
-  new MultiSelector(assignedQueuesField).init();
-}
-
-function initCLEFiltersField() {
-  const clesField = document.getElementById("control_list_entry");
-  if (!clesField) return;
-  new MultiSelector(clesField).init();
-}
-
-function initRegimeFiltersField() {
-  const regimeField = document.getElementById("regime_entry");
-  if (!regimeField) return;
-  new MultiSelector(regimeField).init();
-}
-
-const initCountryAutocompleteField = () => {
-  const countriesField = document.getElementById("countries");
-  if (!countriesField) return;
-  new MultiSelector(countriesField).init();
-};
-
 const initCaseFilters = () => {
-  initCountryAutocompleteField();
-  accessibleAutocomplete.enhanceSelectElement({
-    defaultValue: "",
-    preserveNullOptions: true,
-    selectElement: document.querySelector("#case_officer"),
-  });
-  accessibleAutocomplete.enhanceSelectElement({
-    defaultValue: "",
-    preserveNullOptions: true,
-    selectElement: document.querySelector("#case_adviser"),
-  });
-  initFlagsFiltersField();
-  initAssignedQueuesFiltersField();
-  initCLEFiltersField();
-  initRegimeFiltersField();
+  const $singleSelects = document.querySelectorAll(".single-select-filter");
+  for (const $singleSelect of $singleSelects) {
+    accessibleAutocomplete.enhanceSelectElement({
+      defaultValue: "",
+      preserveNullOptions: true,
+      selectElement: $singleSelect,
+    });
+  }
+
+  const $multiSelects = document.querySelectorAll(".multi-select-filter");
+  for (const $multiSelect of $multiSelects) {
+    new MultiSelector($multiSelect).init();
+  }
 };
 
 initCaseFilters();
