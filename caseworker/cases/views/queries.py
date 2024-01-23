bc40bcdab17c8cfb8630bb3781c078a4c7bc9943
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2024-01-23 15:00:47 +0000

    Refactor use of lite_user id

--- caseworker/cases/views/queries.py
+++ caseworker/cases/views/queries.py
@@ -11,7 +11,7 @@ from core.exceptions import ServiceError
 
 class CloseQueryView(LoginRequiredMixin, FormView):
     def dispatch(self, request, *args, **kwargs):
-        self.lite_user = request.lite_user if request.lite_user else None
+        self.lite_user = request.lite_user
         return super().dispatch(request, *args, **kwargs)
 
     def get_form_class(self):
@@ -24,7 +24,7 @@ class CloseQueryView(LoginRequiredMixin, FormView):
     def form_valid(self, form):
         data = {
             "response": form.cleaned_data["reason_for_closing_query"],
-            "responded_by_user": self.lite_user.get("id") if self.lite_user else None,
+            "responded_by_user": self.lite_user["id"],
         }
         response, status_code = put_ecju_query(
             request=self.request, pk=self.kwargs["pk"], query_pk=self.kwargs["query_pk"], json=data
