73a865b04e3c361d91d1025c1247de9e8304c128
Kevin Carrogan <kevin@carrogan.com>
2024-01-24 10:43:20 +0000

    Remove views to allow editing of routing rules
    Routing rules are now defined in code itself so we no longer need these views

--- caseworker/routing_rules/urls.py
+++ caseworker/routing_rules/urls.py
@@ -6,7 +6,4 @@ app_name = "routing_rules"
 
 urlpatterns = [
     path("", views.RoutingRulesList.as_view(), name="list"),
-    path("create/", views.CreateRoutingRule.as_view(), name="create"),
-    path("<uuid:pk>/edit/", views.EditRoutingRules.as_view(), name="edit"),
-    path("<uuid:pk>/<str:status>/", views.ChangeRoutingRuleActiveStatus.as_view(), name="change_status"),
 ]
