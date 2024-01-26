24f3376cb93a7aa42436afb54abd42f108cfba03
Gurdeep Atwal <depsi.a@gmail.com>
2024-01-26 17:12:02 +0000

    new form for exporter to respond to query

---
+++ exporter/ecju_queries/new_forms/ecju_forms.py
@@ -0,0 +1,89 @@
+from core.common.forms import BaseForm
+from django import forms
+from django.template.loader import render_to_string
+
+from crispy_forms_gds.layout import Field, HTML, Div, Button
+
+
+class ECJUQueryRespondForm(BaseForm):
+    class Layout:
+        TITLE = "Respond to query"
+
+    response = forms.CharField(
+        label="Send a message to the exporter (optional)",
+        widget=forms.Textarea(attrs={"rows": 7, "class": "govuk-!-margin-top-4"}),
+        required=False,
+    )
+
+    def __init__(
+        self,
+        ecju_query,
+        documents,
+        case_id,
+        ecju_response,
+        *args,
+        **kwargs,
+    ):
+
+        self.ecju_query = ecju_query
+        self.case_id = case_id
+        self.ecju_response = ecju_response
+        self.documents = documents
+
+        self.declared_fields["response"] = forms.CharField(
+            initial=self.ecju_response,
+            label="Send a message to the exporter (optional)",
+            required=False,
+            widget=forms.Textarea(attrs={"rows": 7, "class": "govuk-!-margin-top-4"}),
+        )
+
+        super().__init__(*args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+
+        return response
+
+    def get_layout_fields(self):
+        return [
+            HTML.p(f'<div class="app-ecju-query__text">{self.ecju_query["question"]}</div>'),
+            HTML.p("Your application will be paused until the caseworker receives a response. To reply: "),
+            HTML.p("1. Edit and submit your application as required or send a written message below."),
+            HTML.p("2. Then select 'Continue' to notify the case worker that the query has been answered."),
+            "response",
+            HTML.p("<div class=" "govuk-hint" ">You can enter upto 2200 characters</div>"),
+            HTML.h2("Documents"),
+            Button.secondary("add_document", "Attach a document"),
+            HTML(
+                render_to_string(
+                    "ecju-queries/ecju-documents.html",
+                    {"documents": self.documents, "ecju_query": self.ecju_query, "case_id": self.case_id},
+                )
+            ),
+        ]
+
+    def get_layout_actions(self):
+        layout_actions = super().get_layout_actions()
+
+        layout_actions.append(
+            Button.secondary("cancel", "Cancel"),
+        )
+
+        return layout_actions
+
+
+class ECJUQueryRespondConfirmForm(BaseForm):
+    class Layout:
+        TITLE = "Confirm you have responsed to this query"
+
+    def get_layout_fields(self):
+        return []
+
+    def get_layout_actions(self):
+        layout_actions = super().get_layout_actions()
+
+        layout_actions.append(
+            Button.secondary("cancel", "Cancel"),
+        )
+
+        return layout_actions
--- exporter/ecju_queries/urls.py
+++ exporter/ecju_queries/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
 
 from exporter.ecju_queries import views
+from exporter.ecju_queries.new_views.ecju_views import ECJURespondQueryView, ECJURespondQueryConfirmView
 
 app_name = "ecju_queries"
 
 urlpatterns = [
+    # This superceeds the view respond_to_query where object type is application for other object types we will need to revert to the old view
+    path(
+        "<uuid:query_pk>/application/<uuid:case_pk>/",
+        ECJURespondQueryView.as_view(),
+        name="respond_to_application_query",
+    ),
     path("<uuid:query_pk>/<str:object_type>/<uuid:case_pk>/", views.RespondToQuery.as_view(), name="respond_to_query"),
+    path(
+        "<uuid:query_pk>/<uuid:case_pk>/confirm/",
+        ECJURespondQueryConfirmView.as_view(),
+        name="respond_to_application_query_confirm",
+    ),
     path(
         "<uuid:query_pk>/<str:object_type>/<uuid:extra_pk>/case/<uuid:case_pk>/",
         views.RespondToQuery.as_view(),
