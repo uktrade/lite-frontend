a4fa8f33ae0618badfb671286bc491a07b0cdd53
Stuart Axon <stuart.axon@digital.trade.gov.uk>
2024-01-31 16:30:52 +0000

    Set aria-labelledby on the ecju query close textarea to reference the hint widget.

--- caseworker/cases/forms/queries.py
+++ caseworker/cases/forms/queries.py
@@ -12,7 +12,18 @@ class CloseQueryForm(forms.Form):
         required=False,
     )
 
+    def use_hint_as_aria_labelledby(self, field_name):
+        """
+        :param field_name: Name of the field to add an aria-labelledby attribute to.
+
+        Add an aria-labelledby attribute to a field's widget referencing the field's hint widget.
+        """
+        field = self.fields[field_name]
+        label_id = (self.auto_id % self.prefix) + f"-{field_name}_hint"
+        field.widget.attrs["aria-labelledby"] = label_id
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout("reason_for_closing_query", Button("submit", "Submit"))
+        self.use_hint_as_aria_labelledby("reason_for_closing_query")
