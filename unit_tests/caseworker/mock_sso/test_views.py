3b4d0e5196122438936af74ca4e1fc619742671b
Kevin Carrogan <kevin@carrogan.com>
2024-02-26 12:48:08 +0000

    Rename fixtures for mock sso

--- unit_tests/caseworker/mock_sso/test_views.py
+++ unit_tests/caseworker/mock_sso/test_views.py
@@ -21,7 +21,7 @@ def configure_settings(settings):
 
 
 @pytest.fixture
-def state():
+def authorize_state():
     return "dummystate"
 
 
@@ -31,13 +31,13 @@ def redirect_uri():
 
 
 @pytest.fixture
-def get_params(redirect_uri, state):
+def authorize_url_params(redirect_uri, authorize_state):
     return {
         "response_type": "code",
         "client_id": "dummyid",
         "redirect_uri": redirect_uri,
         "scope": "read+write",
-        "state": state,
+        "state": authorize_state,
     }
 
 
@@ -52,7 +52,7 @@ def token_url():
 
 
 @pytest.fixture
-def email():
+def login_prompt_email():
     return "explicit-test-email@example.com"  # /PS-IGNORE
 
 
@@ -64,51 +64,57 @@ def settings_email(settings):
 
 
 @patch("core.mock_sso.views.uuid")
-def test_mock_authorize(mock_uuid, client, redirect_uri, state, authorize_url, get_params, email):
+def test_mock_authorize(
+    mock_uuid, client, redirect_uri, authorize_state, authorize_url, authorize_url_params, login_prompt_email
+):
     code = str(uuid.uuid4())
     mock_uuid.uuid4.return_value = code
     assert parse.urljoin(settings.AUTHBROKER_URL, authorize_url) == settings.AUTHBROKER_AUTHORIZATION_URL
 
-    response = client.get(authorize_url, get_params)
+    response = client.get(authorize_url, authorize_url_params)
     assert response.status_code == 200
     soup = BeautifulSoup(response.content, "html.parser")
     assert soup.find("input", {"name": "email"})
     assert soup.find("form").attrs["action"] == ""
 
-    response = client.post(f"{authorize_url}?{parse.urlencode(get_params)}", data={"email": email})
+    response = client.post(
+        f"{authorize_url}?{parse.urlencode(authorize_url_params)}", data={"email": login_prompt_email}
+    )
     assert response.status_code == 302
-    assert response["Location"] == redirect_uri + f"?code={code}&state={state}"
+    assert response["Location"] == redirect_uri + f"?code={code}&state={authorize_state}"
 
 
 @patch("core.mock_sso.views.uuid")
 def test_mock_authorize_default_email(
-    mock_uuid, settings_email, client, redirect_uri, state, authorize_url, get_params
+    mock_uuid, settings_email, client, redirect_uri, authorize_state, authorize_url, authorize_url_params
 ):
     code = str(uuid.uuid4())
     mock_uuid.uuid4.return_value = code
-    response = client.get(authorize_url, get_params)
+    response = client.get(authorize_url, authorize_url_params)
     assert response.status_code == 302
-    assert response["Location"] == redirect_uri + f"?code={code}&state={state}"
+    assert response["Location"] == redirect_uri + f"?code={code}&state={authorize_state}"
 
 
 @patch("core.mock_sso.views.uuid")
 def test_mock_authorize_pulls_from_session_on_multiple_requests(
-    mock_uuid, client, redirect_uri, state, authorize_url, get_params, email
+    mock_uuid, client, redirect_uri, authorize_state, authorize_url, authorize_url_params, login_prompt_email
 ):
     code = str(uuid.uuid4())
     mock_uuid.uuid4.return_value = code
     assert parse.urljoin(settings.AUTHBROKER_URL, authorize_url) == settings.AUTHBROKER_AUTHORIZATION_URL
 
-    response = client.get(authorize_url, get_params)
+    response = client.get(authorize_url, authorize_url_params)
     assert response.status_code == 200
 
-    response = client.post(f"{authorize_url}?{parse.urlencode(get_params)}", data={"email": email})
+    response = client.post(
+        f"{authorize_url}?{parse.urlencode(authorize_url_params)}", data={"email": login_prompt_email}
+    )
     assert response.status_code == 302
-    assert response["Location"] == redirect_uri + f"?code={code}&state={state}"
+    assert response["Location"] == redirect_uri + f"?code={code}&state={authorize_state}"
 
-    response = client.get(authorize_url, get_params)
+    response = client.get(authorize_url, authorize_url_params)
     assert response.status_code == 302
-    assert response["Location"] == redirect_uri + f"?code={code}&state={state}"
+    assert response["Location"] == redirect_uri + f"?code={code}&state={authorize_state}"
 
 
 def test_mock_authorize_bad_request(client, authorize_url):
@@ -118,10 +124,10 @@ def test_mock_authorize_bad_request(client, authorize_url):
 
 
 @patch("core.mock_sso.views.uuid")
-def test_mock_token(mock_uuid, client, authorize_url, get_params, token_url, email):
+def test_mock_token(mock_uuid, client, authorize_url, authorize_url_params, token_url, login_prompt_email):
     code = str(uuid.uuid4())
     mock_uuid.uuid4.return_value = code
-    client.post(f"{authorize_url}?{parse.urlencode(get_params)}", data={"email": email})
+    client.post(f"{authorize_url}?{parse.urlencode(authorize_url_params)}", data={"email": login_prompt_email})
 
     assert parse.urljoin(settings.AUTHBROKER_URL, token_url) == settings.AUTHBROKER_TOKEN_URL
 
@@ -134,10 +140,10 @@ def test_mock_token(mock_uuid, client, authorize_url, get_params, token_url, ema
 
 
 @patch("core.mock_sso.views.uuid")
-def test_mock_api_user_me(mock_uuid, client, get_params, authorize_url, token_url, email):
+def test_mock_api_user_me(mock_uuid, client, authorize_url_params, authorize_url, token_url, login_prompt_email):
     code = str(uuid.uuid4())
     mock_uuid.uuid4.return_value = code
-    client.post(f"{authorize_url}?{parse.urlencode(get_params)}", data={"email": email})
+    client.post(f"{authorize_url}?{parse.urlencode(authorize_url_params)}", data={"email": login_prompt_email})
 
     access_token = str(uuid.uuid4())
     mock_uuid.uuid4.return_value = access_token
@@ -150,9 +156,9 @@ def test_mock_api_user_me(mock_uuid, client, get_params, authorize_url, token_ur
 
     assert response.status_code == 200
     assert response.json() == {
-        "email": email,
-        "contact_email": email,
-        "email_user_id": email,
+        "email": login_prompt_email,
+        "contact_email": login_prompt_email,
+        "email_user_id": login_prompt_email,
         "user_id": "20a0353f-a7d1-4851-9af8-1bcaff152b60",
         "first_name": settings.MOCK_SSO_USER_FIRST_NAME,
         "last_name": settings.MOCK_SSO_USER_LAST_NAME,
@@ -164,10 +170,12 @@ def test_mock_api_user_me(mock_uuid, client, get_params, authorize_url, token_ur
 
 
 @patch("core.mock_sso.views.uuid")
-def test_mock_api_user_me_default_email(mock_uuid, settings_email, client, get_params, authorize_url, token_url):
+def test_mock_api_user_me_default_email(
+    mock_uuid, settings_email, client, authorize_url_params, authorize_url, token_url
+):
     code = str(uuid.uuid4())
     mock_uuid.uuid4.return_value = code
-    client.get(f"{authorize_url}?{parse.urlencode(get_params)}")
+    client.get(f"{authorize_url}?{parse.urlencode(authorize_url_params)}")
 
     access_token = str(uuid.uuid4())
     mock_uuid.uuid4.return_value = access_token
