97df117268e6ddfac020904f2d6d303b36ab6330
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2024-03-25 11:45:53 +0000

    Fix ultimate end user missing from exporter application summary

--- exporter/applications/helpers/check_your_answers.py
+++ exporter/applications/helpers/check_your_answers.py
@@ -128,7 +128,7 @@ def _convert_standard_application(application, editable=False, is_summary=False)
         product_location = {"Product location and journey": _get_product_location_and_journey(application)}
         converted = {**product_location, **converted}
 
-    if has_incorporated_goods(application):
+    if has_ultimate_end_users(application):
         ultimate_end_users = [convert_party(item, application, editable) for item in application["ultimate_end_users"]]
         converted[strings.ULTIMATE_END_USERS] = ultimate_end_users
 
@@ -704,6 +704,10 @@ def _is_application_export_type_temporary(application):
 
 def is_application_export_type_permanent(application):
     return False if not application.get("export_type") else (application.get("export_type").get("key") == PERMANENT)
+
+
+def has_ultimate_end_users(application):
+    return bool(application["ultimate_end_users"])
 
 
 def has_incorporated_goods(application):
