22451211a38f08e085fb90aa4d14d6701cefe72e
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-04-05 13:53:05 +0100

    Add product details to F680 application

---
+++ exporter/applications/summaries/f680.py
@@ -0,0 +1,31 @@
+from django.urls import reverse
+
+from core.summaries.formatters import (
+    add_edit_links,
+)
+from core.summaries.summaries import (
+    f680_goods_summary,
+    f680_good_details_on_application_summary as core_f680_good_details_on_application_summary,
+)
+
+
+def get_complete_item_summary_edit_link_factory(application, good):
+    def get_edit_link(name):
+        return reverse(
+            f"applications:complete_item_edit_{name}",
+            kwargs={
+                "pk": application["id"],
+                "good_pk": good["id"],
+            },
+        )
+
+    return get_edit_link
+
+
+def f680_good_details_summary(good, *args, **kwargs):
+
+    return f680_goods_summary(good)
+
+
+def f680_good_details_on_application_summary(good_on_application, *args, **kwargs):
+    return core_f680_good_details_on_application_summary(good_on_application)
