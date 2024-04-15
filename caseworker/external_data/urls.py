eba7386e418f1297f51f75e8230194884d4a3389
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-04-15 11:54:08 +0100

    amended urls.py and tests

--- caseworker/external_data/urls.py
+++ caseworker/external_data/urls.py
@@ -5,7 +5,7 @@ from caseworker.external_data import views
 app_name = "external_data"
 
 urlpatterns = [
-    path("denials/upload/", views.DenialUploadView.as_view(), name="denials-upload"),
+    path("denials/upload/", views.DenialUploadView.as_view(), name="denials-add-by-csv"),
     path("denials/<uuid:pk>/", views.DenialDetailView.as_view(), name="denial-detail"),
     path("denials/<uuid:pk>/revoke/", views.DenialRevokeView.as_view(), name="denial-revoke"),
 ]
