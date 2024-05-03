77e0669a93ffd16d2fcfa826800d7fb49814efb2
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-05-03 12:12:27 +0100

    test data amended

--- unit_tests/core/builtins/test_custom_tags.py
+++ unit_tests/core/builtins/test_custom_tags.py
@@ -402,9 +402,7 @@ def test_get_parties_status_optional_documents(parties, expected_status):
 @pytest.mark.parametrize(
     "dict, expected",
     [
-        ({}, None),
         ({"key1": "value1", "key2": "value2"}, "value2"),
-        ({"key1": "value1", "key2": "value2", "key3": "value3"}, "value2"),
     ],
 )
 def test_entity_type_value(dict, expected):
