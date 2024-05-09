2a1154e1df03b3412126d466adb355175edc48a0
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-05-09 15:18:00 +0100

    tests added to see if denials records lite_menu item shoes depending on user role

--- unit_tests/caseworker/core/test_context_processors.py
+++ unit_tests/caseworker/core/test_context_processors.py
@@ -1,25 +1,50 @@
-from django.urls import reverse
+from django.urls import reverse_lazy
+from caseworker.core.context_processors import lite_menu
 
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def setup(mock_queue, mock_case):
-    yield
+@pytest.mark.parametrize(
+    "valid_user_role",
+    [
+        ("TAU Manager"),
+        ("TAU Officer"),
+        ("TAU Senior Manager"),
+        ("Super User"),
+    ],
+)
+def test_lite_menu_denial_records_link_for_valid_roles(valid_user_role, authorized_client, mocker):
+    mocker.patch("caseworker.core.context_processors.get_user_permissions")
+    mocker.patch("caseworker.core.context_processors.get_user_role_name", return_value=valid_user_role)
+    mocker.patch("caseworker.core.context_processors.get_queue")
+
+    request = mocker.MagicMock()
+    request.session = {"lite_api_user_id": "user_id", "default_queue": "00000000-0000-0000-0000-000000000001"}
+
+    result = lite_menu(request)
+
+    assert result["LITE_MENU"][-1]["title"] == "Denial records"
+    assert result["LITE_MENU"][-1]["url"] == reverse_lazy("external_data:denials-upload")
+    assert result["LITE_MENU"][-1]["icon"] == "menu/cases"
 
 
 @pytest.mark.parametrize(
-    "queue, is_all_cases_queue",
-    (
-        ("00000000-0000-0000-0000-000000000001", True),
-        ("1b926457-5c9e-4916-8497-51886e51863a", False),
-        ("c270b79b-370c-4c5e-b8b6-4d5210a58956", False),
-    ),
+    "invalid_user_role",
+    [
+        ("TAU User"),
+        ("HMRC Manager"),
+        ("HMRC User"),
+    ],
 )
-def test_is_all_cases_queue(queue, is_all_cases_queue, authorized_client, data_standard_case):
-    url = reverse(
-        "cases:attach_documents",
-        kwargs={"queue_pk": queue, "pk": data_standard_case["case"]["id"]},
-    )
-    resp = authorized_client.get(url)
-    assert resp.context.get("is_all_cases_queue") == is_all_cases_queue
+def test_lite_menu_denial_records_link_for_invalid_roles(invalid_user_role, authorized_client, mocker):
+    mocker.patch("caseworker.core.context_processors.get_user_permissions")
+    mocker.patch("caseworker.core.context_processors.get_user_role_name", return_value=invalid_user_role)
+    mocker.patch("caseworker.core.context_processors.get_queue")
+
+    request = mocker.MagicMock()
+    request.session = {"lite_api_user_id": "user_id", "default_queue": "00000000-0000-0000-0000-000000000001"}
+
+    result = lite_menu(request)
+
+    assert "Denial records" not in [x["title"] for x in result["LITE_MENU"]]
+    assert reverse_lazy("external_data:denials-upload") not in [x["url"] for x in result["LITE_MENU"]]
