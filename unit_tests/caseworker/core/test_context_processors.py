135d639930152af506615b34f28434cb24c461e2
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-05-16 16:59:24 +0100

    test refactor

--- unit_tests/caseworker/core/test_context_processors.py
+++ unit_tests/caseworker/core/test_context_processors.py
@@ -1,4 +1,3 @@
-from django.urls import reverse_lazy
 from caseworker.core.context_processors import lite_menu
 from caseworker.core.constants import Role
 
@@ -6,35 +5,29 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "valid_user_role",
+    "valid_role",
     Role.tau_roles.value,
 )
 @pytest.mark.parametrize(
-    "invalid_user_role",
+    "invalid_role, status",
     [
-        ("TAU User"),
-        ("HMRC Manager"),
-        ("HMRC User"),
+        ("TAU User", False),
+        ("HMRC Manager", False),
+        ("HMRC User", False),
     ],
 )
-def test_lite_menu_denial_records_link_for_valid_and_invalid_roles(
-    valid_user_role, invalid_user_role, authorized_client, mocker
-):
+def test_lite_menu_user_can_access_denial_records(mocker, invalid_role, status, valid_role, authorized_client):
     mocker.patch("caseworker.core.context_processors.get_user_permissions")
-    mocker.patch(
-        "caseworker.core.context_processors.get_user_role_name", side_effect=[valid_user_role, invalid_user_role]
-    )
+    mocker.patch("caseworker.core.context_processors.get_user_role_name", side_effect=[valid_role, invalid_role])
     mocker.patch("caseworker.core.context_processors.get_queue")
 
     request = mocker.MagicMock()
     request.session = {"lite_api_user_id": "user_id", "default_queue": "00000000-0000-0000-0000-000000000001"}
 
-    result_for_side_effect_1 = lite_menu(request)
+    valid_status_result = lite_menu(request)
+    valid_menu_options = [item["title"] for item in valid_status_result["LITE_MENU"]]
+    assert "Denial records" in valid_menu_options
 
-    for item in result_for_side_effect_1["LITE_MENU"]:
-        if item["title"] == "Denial records":
-            assert item["url"] == reverse_lazy("external_data:denials-upload")
-
-    result_for_side_effect_2 = lite_menu(request)
-
-    assert "Denial records" not in [x["title"] for x in result_for_side_effect_2["LITE_MENU"]]
+    invalid_status_result = lite_menu(request)
+    invalid_menu_options = [item["title"] for item in invalid_status_result["LITE_MENU"]]
+    assert ("Denial records" in invalid_menu_options) == status
