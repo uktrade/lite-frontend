85a67f1b99b60ec999a48450c3419dee61f27765
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-05-16 16:12:36 +0100

    combined invalid and valid user test into 1 test

--- unit_tests/caseworker/core/test_context_processors.py
+++ unit_tests/caseworker/core/test_context_processors.py
@@ -9,21 +9,6 @@ import pytest
     "valid_user_role",
     Role.tau_roles.value,
 )
-def test_lite_menu_denial_records_link_for_valid_roles(valid_user_role, authorized_client, mocker):
-    mocker.patch("caseworker.core.context_processors.get_user_permissions")
-    mocker.patch("caseworker.core.context_processors.get_user_role_name", return_value=valid_user_role)
-    mocker.patch("caseworker.core.context_processors.get_queue")
-
-    request = mocker.MagicMock()
-    request.session = {"lite_api_user_id": "user_id", "default_queue": "00000000-0000-0000-0000-000000000001"}
-
-    result = lite_menu(request)
-
-    for item in result["LITE_MENU"]:
-        if item["title"] == "Denial records":
-            assert item["url"] == reverse_lazy("external_data:denials-upload")
-
-
 @pytest.mark.parametrize(
     "invalid_user_role",
     [
@@ -32,14 +17,24 @@ def test_lite_menu_denial_records_link_for_valid_roles(valid_user_role, authoriz
         ("HMRC User"),
     ],
 )
-def test_lite_menu_denial_records_link_for_invalid_roles(invalid_user_role, authorized_client, mocker):
+def test_lite_menu_denial_records_link_for_valid_and_invalid_roles(
+    valid_user_role, invalid_user_role, authorized_client, mocker
+):
     mocker.patch("caseworker.core.context_processors.get_user_permissions")
-    mocker.patch("caseworker.core.context_processors.get_user_role_name", return_value=invalid_user_role)
+    mocker.patch(
+        "caseworker.core.context_processors.get_user_role_name", side_effect=[valid_user_role, invalid_user_role]
+    )
     mocker.patch("caseworker.core.context_processors.get_queue")
 
     request = mocker.MagicMock()
     request.session = {"lite_api_user_id": "user_id", "default_queue": "00000000-0000-0000-0000-000000000001"}
 
-    result = lite_menu(request)
-    assert "Denial records" not in [x["title"] for x in result["LITE_MENU"]]
-    assert reverse_lazy("external_data:denials-upload") not in [x["url"] for x in result["LITE_MENU"]]
+    result_for_side_effect_1 = lite_menu(request)
+
+    for item in result_for_side_effect_1["LITE_MENU"]:
+        if item["title"] == "Denial records":
+            assert item["url"] == reverse_lazy("external_data:denials-upload")
+
+    result_for_side_effect_2 = lite_menu(request)
+
+    assert "Denial records" not in [x["title"] for x in result_for_side_effect_2["LITE_MENU"]]
