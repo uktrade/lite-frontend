ba26cc7569a379d35551ead5e0f60b0888624941
Gurdeep Atwal <depsi.a@gmail.com>
2024-05-24 14:32:47 +0100

    add help test

--- caseworker/cases/forms/denial_forms.py
+++ caseworker/cases/forms/denial_forms.py
@@ -4,16 +4,18 @@ from crispy_forms_gds.layout import (
     Field,
     Layout,
     Button,
+    HTML,
 )
 
 from django import forms
+from django.template.loader import render_to_string
 
 
 class DenialSearchForm(forms.Form):
 
     search_string = forms.CharField(
         widget=forms.Textarea(attrs={"rows": "2"}),
-        label="Or manually edit the query.",
+        label="",
         required=False,
     )
 
@@ -21,6 +23,11 @@ class DenialSearchForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
+            HTML.p("Or manually edit the query."),
+            HTML.details(
+                "To build your query",
+                render_to_string("external_data/help-build-query.html"),
+            ),
             Field("search_string"),
             Div(
                 Button.secondary("submit", "Search"),
