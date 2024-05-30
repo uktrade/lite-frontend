e3e545f8d6a91897a98b4cc1c511f25dafedf064
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-05-30 11:19:48 +0100

    Update amendments form to use BaseForm

--- exporter/applications/forms/amendments.py
+++ exporter/applications/forms/amendments.py
@@ -1,31 +1,26 @@
-from crispy_forms_gds.helper import FormHelper
-from crispy_forms_gds.layout import Layout, Submit
-
 from django import forms
 
+from core.common.forms import BaseForm
 from core.forms.utils import coerce_str_to_bool
 
 
-class ApplicationCopyConfirmationForm(forms.Form):
-    DOCUMENT_TITLE = "Review and countersign this case"
+class ApplicationCopyConfirmationForm(BaseForm):
     CONFIRMATION_CHOICES = [(True, "Yes"), (False, "No")]
     help_text = """Selecting Yes creates an exact copy of the current application which can be used
-    to make amendments. You can still view current application but cannot modify it.
-    The new application will need to be submitted again after all amendments."""
+    to make amendments. The new application will need to be submitted again after all amendments.
+     You can still view current application but cannot modify it."""
+
+    class Layout:
+        TITLE = "Confirm to create a copy of this application for making amendments"
 
     confirm_copy = forms.TypedChoiceField(
         choices=CONFIRMATION_CHOICES,
         widget=forms.RadioSelect,
         coerce=coerce_str_to_bool,
         help_text=help_text,
-        label="Confirm to create a copy of this application for amending it",
+        label="",
         error_messages={"required": "Confirmation required to copy application or not"},
     )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            "confirm_copy",
-            Submit("submit", "Submit"),
-        )
+    def get_layout_fields(self):
+        return ("confirm_copy",)
