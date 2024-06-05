3873ff8aa120ec8ca3037686aa882610cb87b60e
Gurdeep Atwal <depsi.a@gmail.com>
2024-06-05 14:53:50 +0100

    add country filter

--- caseworker/cases/forms/denial_forms.py
+++ caseworker/cases/forms/denial_forms.py
@@ -1,3 +1,4 @@
+from core.forms.widgets import GridmultipleSelect
 from crispy_forms_gds.helper import FormHelper
 from crispy_forms_gds.layout import (
     Div,
@@ -16,18 +17,29 @@ from core.forms.layouts import RenderTemplate
 class DenialSearchForm(forms.Form):
 
     search_string = forms.CharField(
-        widget=forms.Textarea(attrs={"rows": "1"}),
+        widget=forms.Textarea(attrs={"rows": "2"}),
         label="",
         required=False,
     )
 
-    def __init__(self, *args, **kwargs):
+    country_filter = forms.MultipleChoiceField(
+        choices=(),
+        widget=forms.CheckboxSelectMultiple,
+        label="Select countries",
+        required=False,
+    )
+
+    def __init__(self, countries, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.fields["country_filter"].choices = [(c, c) for c in countries]
+
         self.helper = FormHelper()
 
         self.helper.layout = Layout(
             HTML.p("Or manually edit the query."),
             Field("search_string"),
+            Field("country_filter"),
             Div(
                 Button("submit", "Search"),
             ),
