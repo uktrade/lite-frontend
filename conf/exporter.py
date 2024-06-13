9a5105077888bf6c9d58b38ca1fc810f8d07a417
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-06-13 15:07:17 +0100

    Added ASIM logging format via django-log-formatter-asim

--- conf/exporter.py
+++ conf/exporter.py
@@ -1,3 +1,6 @@
+import logging
+from django_log_formatter_asim import ASIMFormatter
+
 import os
 from urllib.parse import urljoin
 
@@ -23,6 +26,7 @@ if MOCK_SSO_ACTIVATE_ENDPOINTS:
     INSTALLED_APPS += [
         "exporter.mock_sso",
     ]
+
 
 TEMPLATES = [
     {
@@ -127,3 +131,24 @@ MIDDLEWARE += [
 ]
 
 SURVEY_URL = env.str("SURVEY_URL", "")
+
+# Logging formatting
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "asim_formatter": {
+            "()": ASIMFormatter,
+        },
+    },
+    "handlers": {
+        "asim": {"class": "logging.StreamHandler", "formatter": "asim_formatter"},
+    },
+    "root": {
+        "handlers": ["asim"],
+    },
+    "loggers": {
+        "django": {"handlers": ["asim"], "propagate": False},
+    },
+}
