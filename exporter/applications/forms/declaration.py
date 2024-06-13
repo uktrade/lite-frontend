41651a0169ae2f1f9b475615a7358cf8177ecd3e
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2024-06-13 13:38:16 +0100

    Refactor to use template includes

--- exporter/applications/forms/declaration.py
+++ exporter/applications/forms/declaration.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.template.loader import render_to_string
 
 from crispy_forms_gds.layout import HTML
 
@@ -69,31 +70,17 @@ class ApplicationDeclarationForm(BaseForm):
 
     def get_layout_fields(self):
         return [
-            HTML(f'<h2 class="govuk-heading-m">{self.Layout.HEADING_FOI_DISCLOSURE}</h2>'),
-            HTML.p(
-                "Any information you provide in this application may be made public under the Freedom of Information Act 2000."
+            HTML(
+                render_to_string(
+                    "applications/forms/declaration/declaration-heading-question.html",
+                    context={"HEADING_FOI_DISCLOSURE": self.Layout.HEADING_FOI_DISCLOSURE},
+                )
             ),
-            HTML.p("Do you agree to make the application details publicly available?"),
             ConditionalRadios("agreed_to_foi", "Yes", ConditionalRadiosQuestion("No", "foi_reason")),
             HTML(
-                '<p class="govuk-body"><a class="govuk-link" href="https://ico.org.uk/for-organisations/guide-to-freedom-of-information/">'
-                "Find out more about the Freedom of Information Act 2000 and exemptions at the Information Commissioner's Office</a>.</p>"
-            ),
-            HTML(f'<h2 class="govuk-heading-m">{self.Layout.HEADING_DECLARATION}</h2>'),
-            HTML(
-                '<p class="govuk-body">Making a misleading application is an offence as set out in the '
-                '<a class="govuk-link" href="https://www.legislation.gov.uk/uksi/2008/3231/contents/made">Export Control Order 2008</a>.</p>'
-            ),
-            HTML.p(
-                "The licensee must comply with the licence conditions even, where relevant, after completing the activity authorised by the licence. "
-                "Failure to do so is an offence."
-            ),
-            HTML.p(
-                "Information provided in this application may be passed to international organisations or other governments in accordance with commitments "
-                "entered into by His Majesty's Government."
-            ),
-            HTML.p(
-                "If ECJU staff have completed this application on your behalf, they based it on the information you provided. You as the exporter are "
-                "responsible for the accuracy of the information."
+                render_to_string(
+                    "applications/forms/declaration/declaration-heading-declaration.html",
+                    context={"HEADING_DECLARATION": self.Layout.HEADING_DECLARATION},
+                )
             ),
         ]
