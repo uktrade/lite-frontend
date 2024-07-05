72719efd00d6ebafbb030daa7c4fb6b8793c6501
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-05 13:58:57 +0100

    Fix syntax error with health check view

--- core/health_check/views.py
+++ core/health_check/views.py
@@ -1,4 +1,4 @@
-from hfrom django.http import HttpResponse
+from health_check.views import MainView
 from django.http import HttpResponse
 
 class HealthCheckPingdomView(MainView):
@@ -6,7 +6,6 @@ class HealthCheckPingdomView(MainView):
 
     def render_to_response(self, context, status):
         return HttpResponse('')
-
         #context["errored_plugins"] = [plugin for plugin in context["plugins"] if plugin.errors]
         #context["total_response_time"] = sum([plugin.time_taken for plugin in context["plugins"]])
         #return super().render_to_response(context=context, status=status, content_type="text/xml")
