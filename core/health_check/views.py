0a67882cd6cb49f7dd21a4215669459cd490e90a
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-05 12:28:59 +0100

    Test returning success for health check

--- core/health_check/views.py
+++ core/health_check/views.py
@@ -1,10 +1,12 @@
-from health_check.views import MainView
-
+from hfrom django.http import HttpResponse
+from django.http import HttpResponse
 
 class HealthCheckPingdomView(MainView):
     template_name = "healthcheck/pingdom.xml"
 
     def render_to_response(self, context, status):
-        context["errored_plugins"] = [plugin for plugin in context["plugins"] if plugin.errors]
-        context["total_response_time"] = sum([plugin.time_taken for plugin in context["plugins"]])
-        return super().render_to_response(context=context, status=status, content_type="text/xml")
+        return HttpResponse('')
+
+        #context["errored_plugins"] = [plugin for plugin in context["plugins"] if plugin.errors]
+        #context["total_response_time"] = sum([plugin.time_taken for plugin in context["plugins"]])
+        #return super().render_to_response(context=context, status=status, content_type="text/xml")
