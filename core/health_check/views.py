17913beb8b057e2bd57343e7c02994ff15be0acf
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-05 15:13:51 +0100

    restore health check file

--- core/health_check/views.py
+++ core/health_check/views.py
@@ -1,11 +1,10 @@
 from health_check.views import MainView
-from django.http import HttpResponse
+
 
 class HealthCheckPingdomView(MainView):
     template_name = "healthcheck/pingdom.xml"
 
     def render_to_response(self, context, status):
-        return HttpResponse('')
-        #context["errored_plugins"] = [plugin for plugin in context["plugins"] if plugin.errors]
-        #context["total_response_time"] = sum([plugin.time_taken for plugin in context["plugins"]])
-        #return super().render_to_response(context=context, status=status, content_type="text/xml")
+        context["errored_plugins"] = [plugin for plugin in context["plugins"] if plugin.errors]
+        context["total_response_time"] = sum([plugin.time_taken for plugin in context["plugins"]])
+        return super().render_to_response(context=context, status=status, content_type="text/xml")
