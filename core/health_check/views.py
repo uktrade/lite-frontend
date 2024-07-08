fef5823d8ed006a9337b6494c73381af71671fc5
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-08 11:02:07 +0100

    Remove debugging information

--- core/health_check/views.py
+++ core/health_check/views.py
@@ -5,10 +5,6 @@ class HealthCheckPingdomView(MainView):
     template_name = "healthcheck/pingdom.xml"
 
     def render_to_response(self, context, status):
-        print(context["plugins"])
-
         context["errored_plugins"] = [plugin for plugin in context["plugins"] if plugin.errors]
-        print("------------------------")
-        print(context["errored_plugins"])
         context["total_response_time"] = sum([plugin.time_taken for plugin in context["plugins"]])
         return super().render_to_response(context=context, status=status, content_type="text/xml")
