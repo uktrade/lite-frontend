1abde910252b9b5579f32ed9cf8bd06b2b7763ec
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-07-22 12:49:43 +0100

    add new rule and licensing unit senior manager constant

--- caseworker/core/rules.py
+++ caseworker/core/rules.py
@@ -5,7 +5,7 @@ from caseworker.advice.services import (
     GOODS_NOT_LISTED_ID,
     OGD_TEAMS,
 )
-from caseworker.core.constants import ADMIN_TEAM_ID, TAU_TEAM_ID
+from caseworker.core.constants import ADMIN_TEAM_ID, TAU_TEAM_ID, LICENSING_UNIT_SENIOR_MANAGER_ROLE_ID
 from caseworker.cases.services import get_case_sub_statuses
 from caseworker.flags.helpers import has_flag
 
@@ -70,6 +70,12 @@ def case_is_nlr(request, case):
     return has_flag(case, GOODS_NOT_LISTED_ID)
 
 
+@rules.predicate
+def is_user_licencing_unit_senior_manager(request):
+    user = getattr(request, "lite_user", None)
+    return user and user.get("role", {}).get("id") == LICENSING_UNIT_SENIOR_MANAGER_ROLE_ID
+
+
 rules.add_rule("can_user_change_case", is_user_allocated)
 rules.add_rule("can_user_move_case_forward", is_user_allocated)
 rules.add_rule("can_user_review_and_countersign", is_user_allocated)
@@ -82,3 +88,6 @@ rules.add_rule("can_user_add_contact", is_user_allocated)
 rules.add_rule("can_user_change_sub_status", is_user_allocated & has_available_sub_statuses)
 rules.add_rule("can_user_search_products", is_user_in_admin_team | is_user_in_tau_team)  # noqa
 rules.add_rule("can_user_rerun_routing_rules", rules.always_deny)
+
+
+rules.add_rule("can_user_change_licence_status", is_user_licencing_unit_senior_manager)
