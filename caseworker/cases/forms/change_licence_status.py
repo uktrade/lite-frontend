1467082f5bb363610becd10b62edd532c2f728bf
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-07-26 15:03:03 +0100

    add in licence status change form

---
+++ caseworker/cases/forms/change_licence_status.py
@@ -0,0 +1,52 @@
+from django import forms
+
+from core.common.forms import BaseForm
+from crispy_forms_gds.layout import HTML
+
+
+class ChangeLicenseStatusForm(BaseForm):
+    class Layout:
+        TITLE = "Change license status"
+        SUBMIT_BUTTON_TEXT = "Continue"
+
+    status = forms.ChoiceField(
+        choices=[],
+        widget=forms.RadioSelect,
+        label="Select a license status to change",
+        error_messages={"required": "Select a status to change the license to"},
+    )
+
+    def get_layout_fields(self):
+        self.fields["status"].choices += self.statuses
+
+        p_text = HTML.p(
+            f"You should only alter the license status of  {self.reference_code} "
+            "if you have documented the reasons why in the case notes and timeline."
+        )
+
+        return (
+            p_text,
+            "status",
+        )
+
+    def __init__(self, *args, statuses, license, cancel_url, **kwargs):
+
+        self.cancel_url = cancel_url
+        self.statuses = statuses
+
+        self.license = license
+
+        self.reference_code = license["reference_code"]
+
+        super().__init__(*args, **kwargs)
+
+    def get_layout_actions(self):
+        layout_actions = super().get_layout_actions()
+
+        layout_actions.append(
+            HTML(
+                f'<a class="govuk-button govuk-button--secondary" href="{self.cancel_url}" id="cancel-id-cancel">Cancel</a>'
+            ),
+        )
+
+        return layout_actions
