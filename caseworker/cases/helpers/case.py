31430a905c945091c71983bdf870a5d690e78514
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-07-29 16:35:30 +0100

    add testing into front end to show implementation

--- caseworker/cases/helpers/case.py
+++ caseworker/cases/helpers/case.py
@@ -1,3 +1,4 @@
+import rules
 from dateutil.parser import parse
 from decimal import Decimal
 
@@ -172,6 +173,12 @@ class CaseView(CaseworkerMixin, TemplateView):
             open_ecju_queries_with_forms.append((open_query, CloseQueryForm(prefix=str(open_query["id"]))))
         return open_ecju_queries_with_forms
 
+    def get_licence_dict(self):
+        return {
+            "case": {"status": self.case.data["status"]["key"]},
+            "licence": {"status": self.case.data["licence"]["status"]["key"]},
+        }
+
     def get_context(self):
         if not self.tabs:
             self.tabs = []
@@ -190,6 +197,10 @@ class CaseView(CaseworkerMixin, TemplateView):
         context = super().get_context_data()
         default_tab = "quick-summary"
         current_tab = default_tab if self.kwargs["tab"] == "default" else self.kwargs["tab"]
+        licence_dict = {
+            "case": {"status": self.case.data["status"]["key"]},
+            "licence": {"status": self.case.data["licence"]["status"]["key"]},
+        }
 
         return {
             **context,
@@ -218,6 +229,7 @@ class CaseView(CaseworkerMixin, TemplateView):
             "is_terminal": status_props["is_terminal"],
             "security_classified_approvals_types": SecurityClassifiedApprovalsType,
             "user": self.caseworker,
+            "can_change_licence_status": rules.test_rule("can_licence_status_be_changed", self.request, licence_dict),
             **self.additional_context,
         }
 
--- caseworker/core/rules.py
+++ caseworker/core/rules.py
@@ -5,7 +5,12 @@ from caseworker.advice.services import (
     GOODS_NOT_LISTED_ID,
     OGD_TEAMS,
 )
-from caseworker.core.constants import ADMIN_TEAM_ID, TAU_TEAM_ID, LICENSING_UNIT_SENIOR_MANAGER_ROLE_ID
+from caseworker.core.constants import (
+    ADMIN_TEAM_ID,
+    TAU_TEAM_ID,
+    LICENSING_UNIT_SENIOR_MANAGER_ROLE_ID,
+    SUPER_USER_ROLE_ID,
+)
 from caseworker.cases.services import get_case_sub_statuses
 from caseworker.flags.helpers import has_flag
 from core.constants import CaseStatusEnum, LicenceStatusEnum
@@ -74,7 +79,7 @@ def case_is_nlr(request, case):
 @rules.predicate
 def is_user_licencing_unit_senior_manager(request):
     user = getattr(request, "lite_user", None)
-    return user and user.get("role", {}).get("id") == LICENSING_UNIT_SENIOR_MANAGER_ROLE_ID
+    return user and user.get("role", {}).get("id") == LICENSING_UNIT_SENIOR_MANAGER_ROLE_ID or SUPER_USER_ROLE_ID
 
 
 @rules.predicate
