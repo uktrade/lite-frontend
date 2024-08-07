4a5b9319cb907e1a300eab8ae0711d60548a428a
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-08-07 10:12:55 +0100

    test amendments

--- unit_tests/caseworker/cases/views/test_case_details.py
+++ unit_tests/caseworker/cases/views/test_case_details.py
@@ -305,6 +305,46 @@ def test_case_details_licence_change_status(
             "3ae08e0c-47b3-47ba-965f-48318129c147",
             True,
         ),
+        (
+            {
+                "id": str(uuid.uuid4()),
+                "status": "suspended",
+                "case_status": "finalised",
+                "reference_code": "12345AB",
+            },
+            "3ae08e0c-47b3-47ba-965f-48318129c147",
+            True,
+        ),
+        (
+            {
+                "id": str(uuid.uuid4()),
+                "status": "issued",
+                "case_status": "submitted",
+                "reference_code": "12345AB",
+            },
+            "3ae08e0c-47b3-47ba-965f-48318129c147",
+            False,
+        ),
+        (
+            {
+                "id": str(uuid.uuid4()),
+                "status": "revoked",
+                "case_status": "finalised",
+                "reference_code": "12345AB",
+            },
+            "3ae08e0c-47b3-47ba-965f-48318129c147",
+            False,
+        ),
+        (
+            {
+                "id": str(uuid.uuid4()),
+                "status": "issued",
+                "case_status": "finalised",
+                "reference_code": "12345AB",
+            },
+            str(uuid.uuid4()),
+            False,
+        ),
     ],
 )
 def test_licence_details_actions_column_and_licence_status_change_link_display(
@@ -329,7 +369,8 @@ def test_licence_details_actions_column_and_licence_status_change_link_display(
     show_actions_column = bool(html.find(id="actions_column_header"))
     show_licence_status_change_link = bool(html.find(id="licence_status_change_link"))
 
-    assert show_actions_column and show_licence_status_change_link == expected
+    assert show_actions_column is expected
+    assert show_licence_status_change_link is expected
 
 
 @pytest.mark.parametrize(
