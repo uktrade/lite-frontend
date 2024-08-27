b1c6050a25c33075a2e4a21444d6946aa17c539b
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-08-27 10:00:05 +0100

    remove unused code

--- ui_tests/caseworker/step_defs/test_give_advice/test_lu_change_licence_status.py
+++ ui_tests/caseworker/step_defs/test_give_advice/test_lu_change_licence_status.py
@@ -1,20 +1,4 @@
-from pytest_bdd import when, scenarios
-from selenium.webdriver.common.by import By
+from pytest_bdd import scenarios
 
 
 scenarios("../../features/give_advice/lu_licence_change_status.feature", strict_gherkin=False)
-
-
-@when("I log out as current user")
-def click_the_logout_link(driver):
-    driver.find_element(by=By.LINK_TEXT, value="Sign out").click()
-
-
-# Log out as current user
-# Log in as a user with the role "Licensing Unit Senior Manager"
-# Go to the "Cases" page
-# Filter for the case with licence status "finalised"
-# Click on the case
-# Click on the "Change licence status" button
-# Select the new licence status "suspended"
-# Assert that it's visible in the UI
