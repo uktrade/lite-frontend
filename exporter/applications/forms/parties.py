2dc4869679a0753aef14b385e936c2a2c8f1c7dd
Tomos Williams <tllewwilliams@gmail.com>
2024-09-17 14:19:59 +0100

    added validation to party name

--- exporter/applications/forms/parties.py
+++ exporter/applications/forms/parties.py
@@ -11,7 +11,7 @@ from core.forms.layouts import ConditionalRadios, ConditionalRadiosQuestion
 from core.forms.widgets import Autocomplete
 from exporter.core.constants import CaseTypes, FileUploadFileTypes
 from exporter.core.services import get_countries
-from exporter.core.validators import PartyAddressValidator
+from exporter.core.validators import PartyAddressValidator, PartyNameValidator
 from lite_content.lite_exporter_frontend import strings
 from lite_content.lite_exporter_frontend.applications import PartyForm, PartyTypeForm
 from lite_forms.common import country_question
@@ -216,7 +216,8 @@ class PartyNameForm(BaseForm):
             MaxLengthValidator(
                 80,
                 f"End user name should be 80 characters or less",
-            )
+            ),
+            PartyNameValidator(),
         ],
     )
 
