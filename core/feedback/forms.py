7c1edaf18fb4f60d452f193e815e214a30fd4693
Gurdeep Atwal <depsi.a@gmail.com>
2024-09-24 18:45:57 +0100

    add exporter feedback link

--- core/feedback/forms.py
+++ core/feedback/forms.py
@@ -1,5 +1,19 @@
 from django import forms
+from crispy_forms_gds.layout import HTML
+
+from core.common.forms import BaseForm
 
 
-class FeedbackForm(forms.Form):
-    feedback = forms.CharField(label="Please type your feedback here: ", widget=forms.Textarea)
+class FeedbackForm(BaseForm):
+    class Layout:
+        TITLE = "Leave Feedback"
+        SUBMIT_BUTTON_TEXT = "Submit"
+
+    feedback = forms.CharField(label="", widget=forms.Textarea)
+
+    text_p1 = """
+                Use this form to give general feedback about your experience. We do not reply to feedback comments.
+             """
+
+    def get_layout_fields(self):
+        return (HTML.p(self.text_p1), "feedback")
