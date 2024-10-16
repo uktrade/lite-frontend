4b7ee7a43c7ea9f8a254623cd108dff2ba16070f
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-10-16 18:52:23 +0100

    update to iterate over dictionaries

--- exporter/core/organisation/validators.py
+++ exporter/core/organisation/validators.py
@@ -1,4 +1,3 @@
-import re
 import phonenumbers
 
 from django.core.exceptions import ValidationError
@@ -9,16 +8,7 @@ from .constants import Validation
 def validate_vat(value):
     errors = []
     if value:
-        if not re.match(Validation.LETTERS_AND_NUMBERS_ONLY, value):
-            errors.append("UK VAT number can only include numbers and letters")
-        stripped_vat = re.sub(r"[^A-Z0-9]", "", value)
-        if len(stripped_vat) < Validation.UK_VAT_MIN_LENGTH:
-            errors.append("UK VAT number is too short")
-        if len(stripped_vat) > Validation.UK_VAT_MAX_LENGTH:
-            errors.append("UK VAT number is too long")
-        if not re.match(Validation.UK_VAT_VALIDATION_REGEX, stripped_vat):
-            errors.append("Enter a UK VAT number in the correct format")
-
+        errors.extend(key for key, func in Validation.validate_vat_number_functions.items() if func(value))
         if errors:
             raise ValidationError(errors)
 
@@ -26,18 +16,7 @@ def validate_vat(value):
 def validate_eori(value):
     errors = []
     if value:
-        if not re.match(Validation.LETTERS_AND_NUMBERS_ONLY, value):
-            errors.append("EORI number can only include numbers and letters")
-        stripped_eori = re.sub(r"[^A-Z0-9]", "", value)
-        if len(stripped_eori) > Validation.UK_EORI_MAX_LENGTH:
-            errors.append("EORI number is too long")
-        if len(stripped_eori) < Validation.UK_EORI_MIN_LENGTH:
-            errors.append("EORI number is too short")
-        if not (stripped_eori.startswith("GB") or stripped_eori.startswith("XI")):
-            errors.append("Country code can only be GB or XI")
-        if not re.match(Validation.UK_EORI_VALIDATION_REGEX, stripped_eori):
-            errors.append("Enter an EORI number in the correct format")
-
+        errors.extend(key for key, func in Validation.validate_eori_number_functions.items() if func(value))
         if errors:
             raise ValidationError(errors)
 
