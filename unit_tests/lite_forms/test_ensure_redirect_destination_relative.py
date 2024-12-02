a9255bd80bb20893c35585faedc5767034e1d69c
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-12-02 17:09:23 +0000

    test amendment

--- unit_tests/lite_forms/test_ensure_redirect_destination_relative.py
+++ unit_tests/lite_forms/test_ensure_redirect_destination_relative.py
@@ -1,28 +1,27 @@
 import pytest
 
-from django.core.exceptions import SuspiciousOperation
-from lite_forms.views import ensure_redirect_destination_relative
+from lite_forms.views import ensure_redirect_destination_relative, UnsafeRedirectDestination
 
 
 def test_ensure_redirect_destination_relative_valid():
     try:
         ensure_redirect_destination_relative("/valid/path")
-    except SuspiciousOperation:
-        pytest.fail("SuspiciousOperation was raised unexpectedly!")
+    except UnsafeRedirectDestination:
+        pytest.fail("UnsafeRedirectDestination was raised unexpectedly!")
 
 
 def test_ensure_redirect_destination_relative_backslashes_valid():
     try:
         ensure_redirect_destination_relative("\\valid\path")
-    except SuspiciousOperation:
-        pytest.fail("SuspiciousOperation was raised unexpectedly!")
+    except UnsafeRedirectDestination:
+        pytest.fail("UnsafeRedirectDestination was raised unexpectedly!")
 
 
 def test_ensure_redirect_destination_relative_invalid():
-    with pytest.raises(SuspiciousOperation):
+    with pytest.raises(UnsafeRedirectDestination):
         ensure_redirect_destination_relative("https://malicious.com/invalid/path")
 
 
 def test_ensure_redirect_destination_relative_backslashes_invalid():
-    with pytest.raises(SuspiciousOperation):
+    with pytest.raises(UnsafeRedirectDestination):
         ensure_redirect_destination_relative("https:/malicious.com")
