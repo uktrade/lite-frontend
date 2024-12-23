61aae0ab2cb110e6fb6174f01ad6c93ce7d6b6bb
Gurdeep Atwal <depsi.a@gmail.com>
2024-12-23 15:52:55 +0000

    remove gov pass specific

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-sed -i '15 s/build_exporter/build_exporter_dbt/' package.json
-sed -i '15 s/build_caseworker/build_caseworker_dbt/' package.json
+sed -i '15 s/build_exporter/build_exporter/' package.json
+sed -i '15 s/build_caseworker/build_caseworker/' package.json
