556bd8814c66ee1d556626a3f992190914d20d31
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-01-29 10:28:48 +0000

    more view testing added

--- exporter/f680/services.py
+++ exporter/f680/services.py
@@ -2,10 +2,10 @@ from core import client
 
 
 def post_f680_application(request, json):
-    data = client.post(request, "/exporter/f680/", json)
+    data = client.post(request, "/exporter/f680/application/", json)
     return data.json(), data.status_code
 
 
 def get_f680_application(request, application_id):
-    data = client.get(request, f"/exporter/f680/{application_id}/")
+    data = client.get(request, f"/exporter/f680/application/{application_id}/")
     return data.json()
--- unit_tests/exporter/applications/views/test_f680s.py
+++ unit_tests/exporter/applications/views/test_f680s.py
@@ -3,10 +3,19 @@ import pytest
 from bs4 import BeautifulSoup
 from django.urls import reverse
 
+from core import client
+
 
 @pytest.fixture
 def authorized_client(authorized_client_factory, mock_exporter_user):
     return authorized_client_factory(mock_exporter_user["user"])
+
+
+@pytest.fixture
+def mock_f680_application_get(requests_mock, data_f680_case):
+    application = data_f680_case["id"]
+    url = client._build_absolute_uri(f"/exporter/f680/application/{application}/")
+    return requests_mock.get(url=url, json=application)
 
 
 def test_apply_f680_view(authorized_client):
@@ -17,3 +26,10 @@ def test_apply_f680_view(authorized_client):
 
     soup = BeautifulSoup(response.content, "html.parser")
     assert "Name of the application" in soup.find("h1").text
+
+
+def test_f680_summary_view(authorized_client, data_f680_case, mock_f680_application_get):
+    url = reverse("f680:summary", kwargs={"pk": data_f680_case["id"]})
+    response = authorized_client.get(url)
+    breakpoint()
+    assert response.status_code == 200
