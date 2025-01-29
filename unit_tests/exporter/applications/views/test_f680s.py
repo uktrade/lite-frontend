aad09c1289c5f1d5787a9f659cbacb55f1cf0c56
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-01-29 14:20:44 +0000

    Update test_f680s.py

--- unit_tests/exporter/applications/views/test_f680s.py
+++ unit_tests/exporter/applications/views/test_f680s.py
@@ -12,6 +12,11 @@ def authorized_client(authorized_client_factory, mock_exporter_user):
 
 
 @pytest.fixture
+def f680_apply_url(data_f680_case):
+    return reverse("f680:apply", kwargs={"pk": data_f680_case["id"]})
+
+
+@pytest.fixture
 def mock_f680_application_get(requests_mock, data_f680_case):  # PS-IGNORE
     application_id = data_f680_case["id"]  # PS-IGNORE
     url = client._build_absolute_uri(f"/exporter/f680/application/{application_id}/")  # PS-IGNORE
@@ -23,12 +28,15 @@ def set_f680_fetaure_flag(settings):  # PS-IGNORE
     settings.FEATURE_FLAG_ALLOW_F680 = True  # PS-IGNORE
 
 
-def test_apply_f680_view(authorized_client):  # PS-IGNORE
-    url = reverse("f680:apply")  # PS-IGNORE
-    response = authorized_client.get(url)
+def test_apply_f680_view(authorized_client, f680_apply_url):  # PS-IGNORE
+    response = authorized_client.get(f680_apply_url)
     assert response.status_code == 200
     soup = BeautifulSoup(response.content, "html.parser")
     assert "Name of the application" in soup.find("h1").text
+
+
+# def test_apply_flow(authorized_client, f680_apply_url):
+#     url =
 
 
 def test_f680_summary_view(
