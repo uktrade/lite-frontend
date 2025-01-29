8a6c83130742a7d1ca6047b33a8c8261b7a3df79
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-01-29 10:59:52 +0000

    add ps ignore

--- unit_tests/exporter/applications/views/test_f680s.py
+++ unit_tests/exporter/applications/views/test_f680s.py
@@ -12,28 +12,28 @@ def authorized_client(authorized_client_factory, mock_exporter_user):
 
 
 @pytest.fixture
-def mock_f680_application_get(requests_mock, data_f680_case):
-    application = data_f680_case["id"]
-    url = client._build_absolute_uri(f"/exporter/f680/application/{application}/")
+def mock_f680_application_get(requests_mock, data_f680_case):  # PS-IGNORE
+    application = data_f680_case["id"]  # PS-IGNORE
+    url = client._build_absolute_uri(f"/exporter/f680/application/{application}/")  # PS-IGNORE
     return requests_mock.get(url=url, json=application)
 
 
 @pytest.fixture(autouse=True)
-def set_f680_fetaure_flag(settings):
-    settings.FEATURE_FLAG_ALLOW_F680 = True
+def set_f680_fetaure_flag(settings):  # PS-IGNORE
+    settings.FEATURE_FLAG_ALLOW_F680 = True  # PS-IGNORE
 
 
-def test_apply_f680_view(authorized_client, settings):
-    url = reverse("f680:apply")
-
+def test_apply_f680_view(authorized_client, settings):  # PS-IGNORE
+    url = reverse("f680:apply")  # PS-IGNORE
     response = authorized_client.get(url)
     assert response.status_code == 200
-
     soup = BeautifulSoup(response.content, "html.parser")
     assert "Name of the application" in soup.find("h1").text
 
 
-def test_f680_summary_view(authorized_client, data_f680_case, set_f680_fetaure_flag, mock_f680_application_get):
-    url = reverse("f680:summary", kwargs={"pk": data_f680_case["id"]})
+def test_f680_summary_view(
+    authorized_client, data_f680_case, set_f680_fetaure_flag, mock_f680_application_get
+):  # PS-IGNORE
+    url = reverse("f680:summary", kwargs={"pk": data_f680_case["id"]})  # PS-IGNORE
     response = authorized_client.get(url)
     assert response.status_code == 200
