71afe9f0023da03abcc191ff7a5197064a0a8759
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2025-01-30 15:58:37 +0000

    Add stub F680 case detail page

---
+++ caseworker/f680/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import TemplateView
+
+from caseworker.cases.services import get_case
+from caseworker.cases.helpers.case import CaseworkerMixin
+from caseworker.queues.services import get_queue
+
+
+class CaseDetailView(CaseworkerMixin, TemplateView):
+    template_name = "f680/case/detail.html"
+
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+
+        self.case_id = str(kwargs["pk"])
+        self.case = get_case(request, self.case_id)
+        self.queue_id = kwargs["queue_pk"]
+        self.queue = get_queue(request, self.queue_id)
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["case"] = self.case
+        return context_data
