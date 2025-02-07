00418fa5ec95d2e39fccb4d69b5625b8e1277000
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2025-02-07 12:27:36 +0000

    Fix lint complaints

--- exporter/f680/payloads.py
+++ exporter/f680/payloads.py
@@ -18,7 +18,7 @@ class F680PatchPayloadBuilder:
     def build(self, section, application_data, form_dict):
         answer_payload = {}
         question_payload = {}
-        for step_name, form in form_dict.items():
+        for form in form_dict.values():
             if form:
                 always_merger.merge(answer_payload, get_cleaned_data(form))
                 always_merger.merge(question_payload, get_questions_data(form))
