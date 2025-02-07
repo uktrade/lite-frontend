97f372063f7375b86ef57ae355b484024838ccae
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-02-07 16:47:56 +0000

    form update

--- exporter/f680/forms.py
+++ exporter/f680/forms.py
@@ -1,10 +1,8 @@
 from django import forms
-from crispy_forms_gds.layout import HTML
+from crispy_forms_gds.choices import Choice
 
-from core.common.forms import BaseForm, FieldsetForm, TextChoice
-from core.forms.layouts import ConditionalCheckboxes, ConditionalCheckboxesQuestion
-from django.db.models import TextChoices
-from django.template.loader import render_to_string
+from core.common.forms import BaseForm, FieldsetForm
+from core.forms.layouts import ConditionalCheckboxes
 
 
 class ApplicationNameForm(BaseForm):
@@ -37,62 +35,35 @@ class ApprovalTypeForm(FieldsetForm):
         TITLE_AS_LABEL_FOR = "approval_choices"
         SUBMIT_BUTTON_TEXT = "Save and continue"
 
-    class ApprovalTypeChoices(TextChoices):
-        INITIAL_DISCUSSIONS_OR_PROMOTING = (
-            "INITIAL_DISCUSSIONS_OR_PROMOTING",
-            "Initial discussions or promoting products",
-        )
-        DEMO_IN_UK_TO_OVERSEAS = "DEMO_IN_UK_TO_OVERSEAS", "Demonstration in the United Kingdom to overseas customers"
-        DEMO_OVERSEAS = "DEMO_OVERSEAS", "Demonstration overseas"
-        TRAINING = "TRAINING", "Training"
-        THROUGH_LIFE_SUPPORT = "THROUGH_LIFE_SUPPORT", "Through life support"
-        SUPPLY = "SUPPLY", "Supply"
+    choice_list = [
+        "initial discussions or promoting products",
+        "demonstration in the United Kingdom to overseas customers",
+        "demonstration overseas",
+        "training",
+        "through life support",
+        "supply",
+    ]
 
-    ApprovalTypeChoices = (
-        TextChoice(ApprovalTypeChoices.INITIAL_DISCUSSIONS_OR_PROMOTING),
-        TextChoice(ApprovalTypeChoices.DEMO_IN_UK_TO_OVERSEAS),
-        TextChoice(ApprovalTypeChoices.DEMO_OVERSEAS),
-        TextChoice(ApprovalTypeChoices.TRAINING),
-        TextChoice(ApprovalTypeChoices.THROUGH_LIFE_SUPPORT),
-        TextChoice(ApprovalTypeChoices.SUPPLY),
-    )
+    def _get_choices(self, choice_list):
+        approval_choices = []
+        approval_text = {}
+
+        approval_list = choice_list
+        for result in approval_list:
+            key = "_".join(result.lower().split())
+            choice = Choice(key, result)
+            if result == approval_list[-1]:
+                choice = Choice(key, result, divider="or")
+            approval_choices.append(choice)
+            approval_text[key] = result.capitalize()
+        return approval_choices, approval_text
 
     approval_choices = forms.MultipleChoiceField(
-        choices=ApprovalTypeChoices,
-        error_messages={
-            "required": 'Select an approval choice"',
-        },
-        widget=forms.CheckboxSelectMultiple(),
-    )
-
-    demonstration_text = forms.MultipleChoiceField(
-        label="Explain what you are demonstrating and why",
-        choices=(),  # set in __init__
+        label="",
         required=False,
-        # setting id for javascript to use
-        widget=forms.SelectMultiple(
-            attrs={
-                "id": "demonstration_text",
-                "data-module": "multi-select",
-            }
-        ),
+        widget=forms.CheckboxSelectMultiple,
+        choices=(),
     )
 
     def get_layout_fields(self):
-        return (
-            ConditionalCheckboxes(
-                "approval_choices",
-                ConditionalCheckboxesQuestion(
-                    "Demonstration in the United Kingdom to overseas customers",
-                    "demonstration_text",
-                ),
-                ConditionalCheckboxesQuestion(
-                    "Demonstration overseas",
-                    "demonstration_text",
-                ),
-            ),
-            HTML.details(
-                "Help with exceptional circumstances",
-                render_to_string("applications/forms/help_with_approval_type.html"),
-            ),
-        )
+        return (ConditionalCheckboxes("approval_choices", *self.conditional_checkbox_choices),)
