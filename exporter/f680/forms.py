3b825d5b168f7cea68e0d46b538373d9de4d75ab
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-02-10 14:40:24 +0000

    textboxes working dynamically

--- exporter/f680/forms.py
+++ exporter/f680/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from crispy_forms_gds.layout import HTML
+from crispy_forms_gds.choices import Choice
 
-from core.common.forms import BaseForm, FieldsetForm, TextChoice
+from core.common.forms import BaseForm, TextChoice
 from core.forms.layouts import ConditionalCheckboxes, ConditionalCheckboxesQuestion
 from django.db.models import TextChoices
 from django.template.loader import render_to_string
@@ -42,64 +43,52 @@ class ApprovalTypeForm(BaseForm):
             "INITIAL_DISCUSSIONS_OR_PROMOTING",
             "Initial discussions or promoting products",
         )
-        DEMO_IN_UK_TO_OVERSEAS = "DEMO_IN_UK_TO_OVERSEAS", "Demonstration in the United Kingdom to overseas customers"
-        DEMO_OVERSEAS = "DEMO_OVERSEAS", "Demonstration overseas"
+        demonstration_in_uk = (
+            "demonstration_in_uk",
+            "Demonstration in the United Kingdom to overseas customers",
+        )
+        demonstration_overseas = "demonstration_overseas", "Demonstration overseas"
         TRAINING = "TRAINING", "Training"
         THROUGH_LIFE_SUPPORT = "THROUGH_LIFE_SUPPORT", "Through life support"
         SUPPLY = "SUPPLY", "Supply"
 
     ApprovalTypeChoices = (
         TextChoice(ApprovalTypeChoices.INITIAL_DISCUSSIONS_OR_PROMOTING),
-        TextChoice(ApprovalTypeChoices.DEMO_IN_UK_TO_OVERSEAS),
-        TextChoice(ApprovalTypeChoices.DEMO_OVERSEAS),
+        TextChoice(ApprovalTypeChoices.demonstration_in_uk),
+        TextChoice(ApprovalTypeChoices.demonstration_overseas),
         TextChoice(ApprovalTypeChoices.TRAINING),
         TextChoice(ApprovalTypeChoices.THROUGH_LIFE_SUPPORT),
         TextChoice(ApprovalTypeChoices.SUPPLY),
     )
 
     approval_choices = forms.MultipleChoiceField(
-        choices=ApprovalTypeChoices,
+        choices=(),
         error_messages={
             "required": 'Select an approval choice"',
         },
         widget=forms.CheckboxSelectMultiple(),
     )
 
-    demonstration_in_uk_text = forms.CharField(
+    demonstration_in_uk = forms.CharField(
         label="Explain what you are demonstrating and why",
         widget=forms.Textarea(attrs={"rows": 5}),
     )
 
-    demonstration_overseas_text = forms.CharField(
+    demonstration_overseas = forms.CharField(
         label="Explain what you are demonstrating and why",
         widget=forms.Textarea(attrs={"rows": 5}),
     )
+
+    def __init__(self, *args, **kwargs):
+        self.conditional_checkbox_choices = (
+            ConditionalCheckboxesQuestion(choices.label, choices.value) for choices in self.ApprovalTypeChoices
+        )
+        super().__init__(*args, **kwargs)
+        self.fields["approval_choices"].choices = self.ApprovalTypeChoices
 
     def get_layout_fields(self):
         return (
-            ConditionalCheckboxes(
-                "approval_choices",
-                ConditionalCheckboxesQuestion(
-                    "Initial discussions or promoting products",
-                ),
-                ConditionalCheckboxesQuestion(
-                    "Demonstration in the United Kingdom to overseas customers",
-                    "demonstration_in_uk_text",
-                ),
-                ConditionalCheckboxesQuestion(
-                    "Demonstration overseas",
-                    "demonstration_overseas_text",
-                ),
-                ConditionalCheckboxesQuestion(
-                    "Training",
-                ),
-                ConditionalCheckboxesQuestion(
-                    "Through life support",
-                ),
-                ConditionalCheckboxesQuestion(
-                    "Supply",
-                ),
-            ),
+            ConditionalCheckboxes("approval_choices", *self.conditional_checkbox_choices),
             HTML.details(
                 "Help with exceptional circumstances",
                 render_to_string("applications/forms/help_with_approval_type.html"),
