7abf7a811998957440dadbf11305a3fa9ed66957
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-02-12 14:53:23 +0000

    display form field

--- exporter/f680/application_sections/additional_information/forms.py
+++ exporter/f680/application_sections/additional_information/forms.py
@@ -1,10 +1,20 @@
+from django import forms
+
 from core.common.forms import BaseForm
 
 
 class NotesForCaseOfficerForm(BaseForm):
     class Layout:
-        TITLE = ""
-        SUBMIT_BUTTON_TEXT = "Submit"
+        TITLE = "Notes"
+        SUBMIT_BUTTON_TEXT = "Save and continue"
+
+    note = forms.CharField(
+        label="Add note",
+        widget=forms.Textarea(attrs={"cols": "80"}),
+    )
+
+    def get_context(self):
+        return super().get_context()
 
     def get_layout_fields(self):
-        return []
+        return ("note",)
