cbfeaf5b738567666bd722ea7f1073265299889d
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-02-14 16:05:38 +0000

    first 2 screens added

--- exporter/f680/application_sections/approval_details/views.py
+++ exporter/f680/application_sections/approval_details/views.py
@@ -1,11 +1,12 @@
 from exporter.f680.application_sections.views import F680ApplicationSectionWizard
 
 from .constants import FormSteps
-from .forms import ProductNameForm
+from .forms import ProductNameForm, ProductDescription
 
 
 class ProductInformationView(F680ApplicationSectionWizard):
     form_list = [
         (FormSteps.PRODUCT_NAME, ProductNameForm),
+        (FormSteps.PRODUCT_DESCRIPTION, ProductDescription),
     ]
     section = "approval_details"
