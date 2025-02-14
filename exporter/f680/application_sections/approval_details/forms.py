e198335edc6d728685e25df8ee08f2dfbb3340c1
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-02-14 16:57:44 +0000

    add product classification confirmation screen

--- exporter/f680/application_sections/approval_details/forms.py
+++ exporter/f680/application_sections/approval_details/forms.py
@@ -4,12 +4,13 @@ from django.template.loader import render_to_string
 from crispy_forms_gds.layout.content import HTML
 
 from core.common.forms import BaseForm
+from core.forms.utils import coerce_str_to_bool
 
 
 class ProductNameForm(BaseForm):
     class Layout:
         TITLE = "Give the item a descriptive name"
-        # TITLE_AS_LABEL_FOR = "product_name"
+        TITLE_AS_LABEL_FOR = "product_name"
         SUBTITLE = (
             '<p class="govuk-body">We need to understand what you will be sharing with non-uk entities.  '
             'This includes: <br><ul class="govuk-body"><li>physical products</li>'
@@ -30,7 +31,7 @@ class ProductNameForm(BaseForm):
 class ProductDescription(BaseForm):
     class Layout:
         TITLE = "Describe the item"
-        # TITLE_AS_LABEL_FOR = "product_description"
+        TITLE_AS_LABEL_FOR = "product_description"
         SUBTITLE = render_to_string("f680/forms/subtitle_product_description.html")
         SUBMIT_BUTTON_TEXT = "Save and continue"
 
@@ -43,7 +44,33 @@ class ProductDescription(BaseForm):
         return (
             "product_description",
             HTML.details(
-                "Help with exceptional circumstances",
+                "Help with incorporating an item",
                 render_to_string("f680/forms/help_product_description.html"),
+            ),
+        )
+
+
+class ProductClassification(BaseForm):
+    class Layout:
+        TITLE = "Does the item have a UK government security grading or classification"
+        ITLE_AS_LABEL_FOR = "product_classification"
+
+    product_classification = forms.TypedChoiceField(
+        choices=(
+            (True, "Yes"),
+            (False, "No"),
+        ),
+        help_text="If the item has multiple security gradings, we need to know the highest one.",
+        label=Layout.TITLE,
+        widget=forms.RadioSelect,
+        coerce=coerce_str_to_bool,
+    )
+
+    def get_layout_fields(self):
+        return (
+            "product_classification",
+            HTML.details(
+                "Help with security grading",
+                render_to_string("f680/forms/help_security_grading.html"),
             ),
         )
--- exporter/f680/application_sections/approval_details/views.py
+++ exporter/f680/application_sections/approval_details/views.py
@@ -1,12 +1,13 @@
 from exporter.f680.application_sections.views import F680ApplicationSectionWizard
 
 from .constants import FormSteps
-from .forms import ProductNameForm, ProductDescription
+from .forms import ProductNameForm, ProductDescription, ProductClassification
 
 
 class ProductInformationView(F680ApplicationSectionWizard):
     form_list = [
         (FormSteps.PRODUCT_NAME, ProductNameForm),
         (FormSteps.PRODUCT_DESCRIPTION, ProductDescription),
+        (FormSteps.SECURITY_GRADING_OR_CLASSIFICATION, ProductClassification),
     ]
     section = "approval_details"
