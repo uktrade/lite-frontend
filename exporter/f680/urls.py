94fd54ab15f6b8883b32cf131e6dd999bf91bcd4
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2025-02-17 13:59:46 +0000

    Fixup: Fix bad conflict resolution

--- exporter/f680/urls.py
+++ exporter/f680/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path(
         "<uuid:pk>/additional-information/",
         include("exporter.f680.application_sections.additional_information.urls"),
-    )
+    ),
     path(
         "<uuid:pk>/user-information/",
         include("exporter.f680.application_sections.user_information.urls"),
