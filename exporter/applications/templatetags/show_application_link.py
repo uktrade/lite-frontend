ac1e23547a05964fbeec2b45678ccd3051b23685
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-02-20 15:55:29 +0000

    add template

--- exporter/applications/templatetags/show_application_link.py
+++ exporter/applications/templatetags/show_application_link.py
@@ -1,13 +1,14 @@
 from django import template
 from django.utils.safestring import mark_safe
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 
 register = template.Library()
 
 
 @register.simple_tag
-def show_application_link(application, template="application_link.html"):
+def show_application_link(application, template="show_application_link.html"):
     is_f680_application = application["case_type"]["sub_type"]["key"] == "f680_clearance"
     is_draft_or_editiable = (
         application["status"]["key"] == "draft" or application["status"]["key"] == "applicant_editing"
@@ -23,6 +24,6 @@ def show_application_link(application, template="application_link.html"):
     else:
         link_target = reverse("applications:application", kwargs={"pk": application["id"]})
 
-    return mark_safe( # /PS-IGNORE
-        f'<a class="govuk-link govuk-link--no-visited-state app-icon-label" href="{link_target}">{application_display_text}</a>'
+    return render_to_string(
+        template, {"link_target": link_target, "application_display_text": application_display_text}
     )
