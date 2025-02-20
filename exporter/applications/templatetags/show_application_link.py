2c4c35a2b3f92a50f519c54b2e78cb045b0b7211
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-02-20 18:05:03 +0000

    refactored to enable easy adding of new application types

--- exporter/applications/templatetags/show_application_link.py
+++ exporter/applications/templatetags/show_application_link.py
@@ -8,18 +8,18 @@ register = template.Library()
 
 @register.simple_tag
 def show_application_link(application, template="show_application_link.html"):
-    is_f680_application = application["case_type"]["sub_type"]["key"] == "f680_clearance"
+    application_type = application["case_type"]["sub_type"]["key"]
     is_draft_or_editiable = (
         application["status"]["key"] == "draft" or application["status"]["key"] == "applicant_editing"
     )
 
     link_mapping = {
-        (True, True): "f680:summary",
-        (False, True): "applications:task_list",
-        (False, False): "applications:application",
+        ("f680_clearance", True): "f680:summary",
+        ("standard", True): "applications:task_list",
+        ("standard", False): "applications:application",
     }
 
-    link_url = link_mapping.get((is_f680_application, is_draft_or_editiable), None)
+    link_url = link_mapping.get((application_type, is_draft_or_editiable), None)
 
     return render_to_string(
         template,
