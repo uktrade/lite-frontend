42b58561bf8b220189be17bdeb91e58cd061850c
Kevin Carrogan <kevin@carrogan.com>
2025-03-10 12:24:15 +0000

    Switch licence type question to a Django form

--- unit_tests/exporter/apply_for_a_licence/forms/test_triage_questions.py
+++ unit_tests/exporter/apply_for_a_licence/forms/test_triage_questions.py
@@ -1,39 +1,80 @@
 import pytest
 
-from exporter.apply_for_a_licence.forms import triage_questions
-from exporter.core.constants import CaseTypes
-from unit_tests.helpers import reload_urlconf
+from exporter.apply_for_a_licence.forms.triage_questions import LicenceTypeForm
 
 
 @pytest.mark.parametrize(
-    "siel_only_allowed, f680_allowed, expect_enabled, expect_disabled",
+    "f680_allowed, expect_enabled, expect_disabled",
     (
-        (True, True, ["export_licence", "f680"], ["transhipment", "trade_control_licence"]),
-        (True, False, ["export_licence"], ["f680", "transhipment", "trade_control_licence"]),
-        (False, True, ["export_licence", "f680", "transhipment", "trade_control_licence"], []),
-        (False, False, ["export_licence", "transhipment", "trade_control_licence"], ["f680"]),
+        (True, ["export_licence", "f680"], ["transhipment", "trade_control_licence"]),
+        (False, ["export_licence"], ["f680", "transhipment", "trade_control_licence"]),
     ),
 )
-def test_opening_question_feature_flags(
-    rf, client, settings, siel_only_allowed, f680_allowed, expect_enabled, expect_disabled
+def test_licence_type_form_feature_flags_disabled_values(
+    render_form,
+    rf,
+    client,
+    beautiful_soup,
+    settings,
+    f680_allowed,
+    expect_enabled,
+    expect_disabled,
 ):
     request = rf.get("/")
     request.session = client.session
     session = request.session
-    session["organisation"] = "123"
+    session["organisation"] = "12345"
     session.save()
 
-    # given the flag is set or unset
-    settings.FEATURE_FLAG_ONLY_ALLOW_SIEL = siel_only_allowed
     settings.FEATURE_FLAG_ALLOW_F680 = f680_allowed
-    reload_urlconf(["exporter.apply_for_a_licence.urls", settings.ROOT_URLCONF])
 
     # when the form is created
-    form = triage_questions.opening_question(request)
+    form = LicenceTypeForm(request=request)
 
-    # then the disabled options reflect the feature flag
-    assert [item.key for item in form.questions[0].options if not item.disabled] == expect_enabled
-    assert [item.key for item in form.questions[0].options if item.disabled] == expect_disabled
+    form_html = render_form(form, request)
+    soup = beautiful_soup(form_html)
+
+    enabled_options = soup.select("input[name=licence_type]:not([disabled])")
+    assert [option["value"] for option in enabled_options] == expect_enabled
+
+    disabled_options = soup.select("input[name=licence_type][disabled]")
+    assert [option["value"] for option in disabled_options] == expect_disabled
+
+
+@pytest.mark.parametrize(
+    "f680_allowed, expect_enabled, expect_disabled",
+    (
+        (True, ["export_licence", "f680"], ["transhipment", "trade_control_licence"]),
+        (False, ["export_licence"], ["f680", "transhipment", "trade_control_licence"]),
+    ),
+)
+def test_licence_type_form_feature_flags_errors(
+    rf,
+    client,
+    settings,
+    f680_allowed,
+    expect_enabled,
+    expect_disabled,
+):
+    request = rf.get("/")
+    request.session = client.session
+    session = request.session
+    session["organisation"] = "12345"
+    session.save()
+
+    settings.FEATURE_FLAG_ALLOW_F680 = f680_allowed
+
+    # when the form is created
+    for value_to_submit in expect_enabled:
+        form = LicenceTypeForm({"licence_type": value_to_submit}, request=request)
+        assert form.is_valid()
+
+    for value_to_submit in expect_disabled:
+        form = LicenceTypeForm({"licence_type": value_to_submit}, request=request)
+        assert not form.is_valid()
+        assert form.errors["licence_type"] == [
+            f"Select a valid choice. {value_to_submit} is not one of the available choices."
+        ]
 
 
 @pytest.mark.parametrize(
@@ -45,8 +86,16 @@ def test_opening_question_feature_flags(
         (False, ["1234"], "12345", ["export_licence"]),
     ),
 )
-def test_opening_question_f680_feature_flags(
-    rf, client, settings, f680_allowed, f680_orgs_allowed, user_org, expect_enabled
+def test_licence_type_f680_feature_flags_disabled_values(
+    rf,
+    client,
+    settings,
+    render_form,
+    beautiful_soup,
+    f680_allowed,
+    f680_orgs_allowed,
+    user_org,
+    expect_enabled,
 ):
     request = rf.get("/")
     request.session = client.session
@@ -57,30 +106,47 @@ def test_opening_question_f680_feature_flags(
     # given the flag is set
     settings.FEATURE_FLAG_ALLOW_F680 = f680_allowed
     settings.FEATURE_FLAG_F680_ALLOWED_ORGANISATIONS = f680_orgs_allowed
-    reload_urlconf(["exporter.apply_for_a_licence.urls", settings.ROOT_URLCONF])
 
     # when the form is created
-    form = triage_questions.opening_question(request)
+    form = LicenceTypeForm(request=request)
 
-    # then the disabled options reflect the feature flag
-    assert [item.key for item in form.questions[0].options if not item.disabled] == expect_enabled
+    form_html = render_form(form, request)
+    soup = beautiful_soup(form_html)
+
+    enabled_options = soup.select("input[name=licence_type]:not([disabled])")
+    assert [option["value"] for option in enabled_options] == expect_enabled
 
 
 @pytest.mark.parametrize(
-    "value, expect_enabled, expect_disabled",
+    "f680_allowed, f680_orgs_allowed, user_org, expect_enabled",
     (
-        (True, [CaseTypes.SIEL], [CaseTypes.OGEL, CaseTypes.OIEL]),
-        (False, [CaseTypes.SIEL, CaseTypes.OGEL, CaseTypes.OIEL], []),
+        (True, [], "1234", True),
+        (False, [], "1234", False),
+        (False, ["1234"], "1234", True),
+        (False, ["1234"], "12345", False),
     ),
 )
-def test_export_type_form_feature_flag(settings, value, expect_enabled, expect_disabled):
-    # given the flag is set or unset
-    settings.FEATURE_FLAG_ONLY_ALLOW_SIEL = value
-    reload_urlconf(["exporter.apply_for_a_licence.urls", settings.ROOT_URLCONF])
+def test_licence_type_f680_feature_flags_disabled_values(
+    rf,
+    client,
+    settings,
+    f680_allowed,
+    f680_orgs_allowed,
+    user_org,
+    expect_enabled,
+):
+    request = rf.get("/")
+    request.session = client.session
+    session = request.session
+    session["organisation"] = user_org
+    session.save()
+
+    # given the flag is set
+    settings.FEATURE_FLAG_ALLOW_F680 = f680_allowed
+    settings.FEATURE_FLAG_F680_ALLOWED_ORGANISATIONS = f680_orgs_allowed
 
     # when the form is created
-    form = triage_questions.export_type_form()
-
-    # then the disabled options reflect the feature flag
-    assert [item.key for item in form.questions[0].options if item.disabled] == expect_disabled
-    assert [item.key for item in form.questions[0].options if not item.disabled] == expect_enabled
+    form = LicenceTypeForm({"licence_type": "f680"}, request=request)
+    assert form.is_valid() is expect_enabled
+    if not expect_enabled:
+        assert form.errors["licence_type"] == ["Select a valid choice. f680 is not one of the available choices."]
