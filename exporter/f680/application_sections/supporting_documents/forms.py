36f625a6da9c43fc3e5d8322ec1e50e55584bd76
Gurdeep Atwal <depsi.a@gmail.com>
2025-03-13 08:21:38 +0000

    add file upload content

--- exporter/f680/application_sections/supporting_documents/forms.py
+++ exporter/f680/application_sections/supporting_documents/forms.py
@@ -5,6 +5,8 @@ from core.file_handler import validate_mime_type
 from exporter.core.constants import FileUploadFileTypes
 from exporter.core.forms import PotentiallyUnsafeClearableFileInput
 
+from crispy_forms_gds.layout import HTML
+
 
 class F680AttachSupportingDocument(BaseForm):
     class Layout:
@@ -28,7 +30,14 @@ class F680AttachSupportingDocument(BaseForm):
     )
 
     def get_layout_fields(self):
+        content = HTML(
+            "You should upload a technical specification for the products in your application."
+            "<p>  If it applies to your application, make sure you also upload:</p>"
+            "<ul><li>the EW release capture form with part A </a> completed</li>"
+            + "<li>a written agreement or clearance to release security classified foriegn technology or information.</li></ul>"
+        )
         return (
+            content,
             "file",
             "description",
         )
