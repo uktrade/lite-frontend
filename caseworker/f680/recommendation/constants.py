df6fb41c9a752b425313b167a44709d55a9da331
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2025-03-18 16:40:36 +0000

    Add constants to define the steps in recommendation form wizard

---
+++ caseworker/f680/recommendation/constants.py
@@ -0,0 +1,8 @@
+class RecommendationType:
+    APPROVE = "approve"
+    REFUSE = "refuse"
+    NOT_APPLICABLE = "not_applicable"
+
+
+class RecommendationSteps:
+    RELEASE_REQUEST_PROVISOS = "release_request_provisos"
