812202fd28a0210e387e5974ad6415efcefe1ba4
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2025-03-18 16:35:53 +0000

    Define payload builder for the Recommendation form wizard

---
+++ caseworker/f680/recommendation/payloads.py
@@ -0,0 +1,17 @@
+from caseworker.f680.recommendation.constants import RecommendationSteps
+from core.wizard.payloads import MergingPayloadBuilder
+
+
+def get_cleaned_data(form):
+    return form.cleaned_data
+
+
+class RecommendationPayloadBuilder(MergingPayloadBuilder):
+
+    def build(self, form_dict):
+        payload = []
+        for _, form in form_dict.items():
+            if form:
+                payload.append(get_cleaned_data(form))
+
+        return payload
