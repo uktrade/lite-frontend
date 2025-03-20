db8e2b158f0fc44a0c8a9e7422bbc98959a81b9a
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-03-20 15:35:38 +0000

    remove required for notes and make sure it shows notes as optional if no response

--- exporter/f680/application_sections/additional_information/tests/test_views.py
+++ exporter/f680/application_sections/additional_information/tests/test_views.py
@@ -189,19 +189,28 @@ class TestAdditionalInformationView:
             }
         }
 
-    def test_POST_to_step_validation_error(
+    def test_POST_to_notes_no_validation_error(
         self,
         post_to_step,
         goto_step,
         mock_f680_application_get,
+        mock_patch_f680_application,
+        authorized_client,
+        beautiful_soup,
     ):
         goto_step(FormSteps.NOTES_FOR_CASEWORKER)
         response = post_to_step(
             FormSteps.NOTES_FOR_CASEWORKER,
             {},
         )
-        assert response.status_code == 200
-        assert response.context["form"]["note"].errors == ["This field is required."]
+        assert response.status_code == 302
+
+        response = authorized_client.get(response.url)
+        soup = beautiful_soup(response.content)
+        parent_div = soup.find(text="Notes for case officers").parent.parent
+        task_list_option_guidance = parent_div.find("div", {"class": "lite-tag"})
+
+        assert "Optional" in task_list_option_guidance.text
 
     def test_GET_with_existing_data_success(
         self,
