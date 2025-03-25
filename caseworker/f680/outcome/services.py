2203916338a3e6758814f5ffe5835662d6877499
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2025-03-25 14:28:48 +0000

    Make outcome view only operate on release requests where no outcome has been given

--- caseworker/f680/outcome/services.py
+++ caseworker/f680/outcome/services.py
@@ -4,3 +4,8 @@ from core import client
 def post_outcome(request, case_id, data):
     response = client.post(request, f"/caseworker/f680/{case_id}/outcome/", data)
     return response.json(), response.status_code
+
+
+def get_outcomes(request, case_id):
+    response = client.get(request, f"/caseworker/f680/{case_id}/outcome/")
+    return response.json(), response.status_code
