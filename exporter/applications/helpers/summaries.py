1dbb3ba172277db739bc228d67d8319f75ebf163
Kevin Carrogan <kevin@carrogan.com>
2025-05-08 14:14:49 +0100

    Fix withdraw view when application doesn't have a sub type

--- exporter/applications/helpers/summaries.py
+++ exporter/applications/helpers/summaries.py
@@ -24,7 +24,7 @@ def application_summary(application):
         values={
             strings.applications.ApplicationSummaryPage.REFERENCE_NAME: application["name"],
             strings.applications.ApplicationSummaryPage.REFERENCE_CODE: application["reference_code"],
-            strings.applications.ApplicationSummaryPage.TYPE: application.sub_type_value,
+            strings.applications.ApplicationSummaryPage.TYPE: application.type_reference_value,
             strings.applications.ApplicationSummaryPage.SUBMITTED_AT: application.submitted_at,
         },
         classes=["govuk-summary-list--no-border"],
